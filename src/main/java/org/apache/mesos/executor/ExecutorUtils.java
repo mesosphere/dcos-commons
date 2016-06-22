a787c19dfa3e1221a96a896d0aa7186754649bd4
Nick <nick@mesosphere.io>
2016-06-22 11:51:51 -0700

    Fix message in exception

--- src/main/java/org/apache/mesos/executor/ExecutorUtils.java
+++ src/main/java/org/apache/mesos/executor/ExecutorUtils.java
@@ -25,7 +25,7 @@ public class ExecutorUtils {
       if (underScoreIndex == -1) {
         throw new ExecutorTaskException(String.format(
                 "ExecutorID '%s' is malformed.  Expected '%s' to extract ExecutorName from ExecutorID.  "
-                + "ExecutorIDs should be generated with TaskUtils.toTaskId().", executorId, EXECUTOR_NAME_DELIM));
+                + "ExecutorIDs should be generated with ExecutorUtils.toExecutorId().", executorId, EXECUTOR_NAME_DELIM));
       }
 
       return executorId.getValue().substring(0, underScoreIndex);
