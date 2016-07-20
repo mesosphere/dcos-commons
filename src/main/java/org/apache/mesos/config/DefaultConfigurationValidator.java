2cd4ea09050eef435f029506ff28a3e00edb15da
Mohit Soni <mohitsoni1989@gmail.com>
2016-07-19 18:33:49 -0700

    Added DefaultConfigurationValidator

---
+++ src/main/java/org/apache/mesos/config/DefaultConfigurationValidator.java
@@ -0,0 +1,27 @@
+package org.apache.mesos.config;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Default Configuration validator, which validates a given new {@code Configuration} w.r.t.
+ * an old {@code Configuration}.
+ */
+public class DefaultConfigurationValidator {
+    private final Collection<ConfigurationValidation> validations = new ArrayList<>();
+
+    public DefaultConfigurationValidator(final Collection<ConfigurationValidation> validations) {
+        if (validations != null && !validations.isEmpty()) {
+            this.validations.addAll(validations);
+        }
+    }
+
+    public Collection<ConfigurationValidationError> validate(Configuration oldConfig, Configuration newConfig) {
+        final List<ConfigurationValidationError> errors = new ArrayList<>();
+        for (ConfigurationValidation validation : validations) {
+            errors.addAll(validation.validate(oldConfig, newConfig));
+        }
+        return errors;
+    }
+}
