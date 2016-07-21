74489d921a8af0aed53692cccad9b0205963ea8d
Nick <nick@mesosphere.io>
2016-07-21 11:09:55 -0700

    Bump to v25

--- src/main/java/org/apache/mesos/state/CuratorStateStore.java
+++ src/main/java/org/apache/mesos/state/CuratorStateStore.java
@@ -10,7 +10,6 @@ import org.apache.zookeeper.KeeperException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
