6e6948dadc7dff4a12b01f3abdc9defc23ff93c1
Anand Mazumdar <anand@apache.org>
2016-08-09 11:48:08 -0700

    Modified the `evolve()`/`devolve()` implementations to use generics.
    This changes the old implementation to use generics thereby
    avoiding the need for it to be duplicated in multiple overloads.

--- src/main/java/org/apache/mesos/protobuf/Devolver.java
+++ src/main/java/org/apache/mesos/protobuf/Devolver.java
@@ -1,19 +1,28 @@
 package org.apache.mesos.protobuf;
 
 import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.Message;
 
 import org.apache.mesos.v1.Protos.FrameworkInfo;
 
 /**
- * Utility class for devolving vX protos to v0 protos.
+ * Utility class for devolving vX protos to v0.
  */
 public class Devolver {
-    public static org.apache.mesos.Protos.FrameworkInfo devolve(FrameworkInfo frameworkInfo) {
-        byte[] data = frameworkInfo.toByteArray();
+    private static <T extends Message> T devolve(Message message, Message.Builder builder) {
+        byte[] data = message.toByteArray();
         try {
-            return org.apache.mesos.Protos.FrameworkInfo.parseFrom(data);
+            builder.mergeFrom(data);
+
+            // NOTE: We need to use `buildPartial()` instead of `build()` because some required
+            // fields might not be set and we don't want an exception to get thrown.
+            return (T) builder.buildPartial();
         } catch (InvalidProtocolBufferException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static org.apache.mesos.Protos.FrameworkInfo devolve(FrameworkInfo frameworkInfo) {
+        return devolve(frameworkInfo, org.apache.mesos.Protos.FrameworkInfo.newBuilder());
     }
 }
--- src/main/java/org/apache/mesos/protobuf/Evolver.java
+++ src/main/java/org/apache/mesos/protobuf/Evolver.java
@@ -1,19 +1,29 @@
 package org.apache.mesos.protobuf;
 
 import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.Message;
 
 import org.apache.mesos.Protos.FrameworkInfo;
 
+
 /**
- * Utility class for evolving v0 protos to vX protos.
+ * Utility class for evolving v0 protos to vX.
  */
 public class Evolver {
-    public static org.apache.mesos.v1.Protos.FrameworkInfo evolve(FrameworkInfo frameworkInfo) {
-        byte[] data = frameworkInfo.toByteArray();
+    private static <T extends Message> T evolve(Message message, Message.Builder builder) {
+        byte[] data = message.toByteArray();
         try {
-            return org.apache.mesos.v1.Protos.FrameworkInfo.parseFrom(data);
+            builder.mergeFrom(data);
+
+            // NOTE: We need to use `buildPartial()` instead of `build()` because some required
+            // fields might not be set and we don't want an exception to get thrown.
+            return (T) builder.buildPartial();
         } catch (InvalidProtocolBufferException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static org.apache.mesos.v1.Protos.FrameworkInfo evolve(FrameworkInfo frameworkInfo) {
+        return evolve(frameworkInfo, org.apache.mesos.v1.Protos.FrameworkInfo.newBuilder());
     }
 }
