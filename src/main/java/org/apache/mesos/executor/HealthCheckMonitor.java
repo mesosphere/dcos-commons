4c70f1570aa38f81599f0b35d35fbbc1a5acf6da
Gabriel Hartmann <gabriel@mesosphere.io>
2016-08-22 20:24:16 -0700

    Health checks (#96)
    * Add health check implementation to the custom executor
    
    It only supports Command health checks which are executed as sub
    processes.
    
    * Address PR comments around logging and commenting.

---
+++ src/main/java/org/apache/mesos/executor/HealthCheckMonitor.java
@@ -0,0 +1,47 @@
+package org.apache.mesos.executor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
+/**
+ * This class reacts to the failure of a health check by calling the stop() method of the ExecutorTask.
+ */
+public class HealthCheckMonitor implements Callable<Optional<HealthCheckStats>> {
+    private final HealthCheckHandler healthCheckHandler;
+    private final ExecutorTask executorTask;
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    public HealthCheckMonitor(
+            HealthCheckHandler healthCheckHandler,
+            ExecutorTask executorTask) {
+        this.healthCheckHandler = healthCheckHandler;
+        this.executorTask = executorTask;
+    }
+
+    @Override
+    public Optional<HealthCheckStats> call() throws Exception {
+        Future<?> healthCheck = healthCheckHandler.start();
+        Optional<HealthCheckStats> healthCheckStats = Optional.empty();
+
+        try {
+            healthCheck.get();
+        } catch (Throwable t) {
+            logger.error("Waiting for failed health check failed with exception: ", t);
+
+            if (t.getCause() instanceof HealthCheckHandler.HealthCheckRuntimeException) {
+                HealthCheckHandler.HealthCheckRuntimeException healthCheckRuntimeException =
+                        (HealthCheckHandler.HealthCheckRuntimeException) t.getCause();
+                healthCheckStats = Optional.of(healthCheckRuntimeException.getHealthCheckStats());
+            } else {
+                logger.error("Health check exited without statistics");
+            }
+        }
+
+        executorTask.stop();
+        return healthCheckStats;
+    }
+}
