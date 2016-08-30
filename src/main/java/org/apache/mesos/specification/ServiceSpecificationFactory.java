0911c705ada9d223e643cd033877af5d16e4005f
Gabriel Hartmann <gabriel@mesosphere.io>
2016-08-29 20:21:59 -0700

    Add Specification tests and remove unused Specification factories

--- src/main/java/org/apache/mesos/specification/ServiceSpecificationFactory.java
+++
@@ -1,8 +0,0 @@
-package org.apache.mesos.specification;
-
-/**
- * Created by gabriel on 8/25/16.
- */
-public interface ServiceSpecificationFactory {
-    ServiceSpecification getServiceSpecification();
-}
