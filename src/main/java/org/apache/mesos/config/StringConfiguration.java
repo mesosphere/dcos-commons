f15bdd8bff62a47bc5a84a323ca7cb43bb2d54c6
Gabriel Hartmann <gabriel@mesosphere.io>
2016-08-30 10:43:05 -0700

    Remove irrelevant configuration modifications.

--- src/main/java/org/apache/mesos/config/StringConfiguration.java
+++ src/main/java/org/apache/mesos/config/StringConfiguration.java
@@ -1,15 +1,13 @@
 package org.apache.mesos.config;
 
-import org.json.JSONObject;
+import java.nio.charset.StandardCharsets;
 
-import java.io.*;
-import java.util.Map;
-import java.util.Optional;
+import java.util.Objects;
 
 /**
  * This class implements the Configuration interface for configurations stored as Strings.
  */
-public class StringConfiguration implements Configuration<String, String> {
+public class StringConfiguration implements Configuration {
 
     /**
      * Factory which performs the inverse of {@link StringConfiguration#getBytes()}.
@@ -18,65 +16,39 @@ public class StringConfiguration implements Configuration<String, String> {
     public static class Factory implements ConfigurationFactory<StringConfiguration> {
         @Override
         public StringConfiguration parse(byte[] bytes) throws ConfigStoreException {
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
-            try {
-                ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
-                Map<String, String> map = (Map<String, String>) objectInputStream.readObject();
-                return new StringConfiguration(map);
-            } catch (ClassNotFoundException | IOException e) {
-                throw new ConfigStoreException(e);
-            }
+            return new StringConfiguration(new String(bytes, StandardCharsets.UTF_8));
         }
     }
 
-    private final Map<String, String> configuration;
+    private final String config;
 
-    public StringConfiguration(Map<String, String> configuration) {
-        this.configuration = configuration;
+    public StringConfiguration(String config) {
+        this.config = config;
     }
 
     @Override
     public byte[] getBytes() throws ConfigStoreException {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        ObjectOutputStream objectOutputStream = null;
-        try {
-            objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
-            objectOutputStream.writeObject(configuration);
-            return byteArrayOutputStream.toByteArray();
-        } catch (IOException e) {
-            throw new ConfigStoreException(e);
-        }
-    }
-
-    @Override
-    @SuppressWarnings("PMD.IfStmtsMustUseBraces")
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        StringConfiguration that = (StringConfiguration) o;
-
-        return configuration != null ? configuration.equals(that.configuration) : that.configuration == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        return configuration != null ? configuration.hashCode() : 0;
+        return config.getBytes(StandardCharsets.UTF_8);
     }
 
     @Override
     public String toJsonString() {
-        return new JSONObject(configuration).toString();
+        return String.format("{ \"string\": \"%s\" }", config.replace("\"", "\\\""));
     }
 
     @Override
-    public Optional<String> get(String key) {
-        String value = configuration.get(key);
-        if (value != null) {
-            return Optional.of(value);
-        } else {
-            return Optional.empty();
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
         }
+        if (!(o instanceof StringConfiguration)) {
+            return false;
+        }
+        return Objects.equals(config, ((StringConfiguration) o).config);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(config);
     }
 }
