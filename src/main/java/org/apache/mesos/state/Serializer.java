3e4665ea6847dd6b20a58ced0c0c35b0826561a1
Michael Gummelt <mgummelt@mesosphere.io>
2016-09-19 18:09:01 -0700

    improve comments

--- src/main/java/org/apache/mesos/state/Serializer.java
+++ src/main/java/org/apache/mesos/state/Serializer.java
@@ -2,7 +2,7 @@ package org.apache.mesos.state;
 
 import java.io.IOException;
 
-/** Serializer */
+/** Serialize and deserialize objects to byte arrays */
 public interface Serializer {
     public <T> byte[] serialize(T value) throws IOException;
     public <T> T deserialize(byte[] bytes, Class<T> clazz) throws IOException;
