9a110c56831c6ef3af0b12d570275ddcbca5c234
Gabriel Hartmann <gabriel@mesosphere.io>
2016-09-29 16:31:24 -0700

    Fix comment link

--- src/main/java/org/apache/mesos/specification/ServiceSpecification.java
+++ src/main/java/org/apache/mesos/specification/ServiceSpecification.java
@@ -14,7 +14,7 @@ public interface ServiceSpecification {
 
 
     /**
-     * Gets the list of TaskSets defining the types of Tasks of which the Service is made.
+     * Gets the list of {@link TaskSet}s defining the types of Tasks of which the Service is made.
      * @return
      */
     List<TaskSet> getTaskSets();
