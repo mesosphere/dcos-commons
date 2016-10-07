83c2ec6a4f3d0fbe120d0c348634fbdb0bdac6b3
Gabriel Hartmann <gabriel@mesosphere.io>
2016-10-07 16:46:28 -0700

    Add PlacementUtils for avoid and collocate agents (#195)

---
+++ src/main/java/org/apache/mesos/offer/constrain/PlacementUtils.java
@@ -0,0 +1,42 @@
+package org.apache.mesos.offer.constrain;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * This class provides Utilities for commonly needed Placement rule scenarios.
+ */
+public class PlacementUtils {
+
+    /**
+     * Given a list of agents to avoid and collocate with, the appropriate rule is generated.
+     * @param avoidAgents Agents which should not have Tasks placed on them.
+     * @param collocateAgents Agents which should have Tasks placed on them.
+     * @return The appropriate placement rule.
+     */
+    public static Optional<PlacementRuleGenerator> getAgentPlacementRule(
+            List<String> avoidAgents,
+            List<String> collocateAgents) {
+
+        Optional<PlacementRuleGenerator> placement;
+        if (!avoidAgents.isEmpty()) {
+            if (!collocateAgents.isEmpty()) {
+                // avoid and collocate enforcement
+                placement = Optional.of(new AndRule.Generator(
+                        new AgentRule.AvoidAgentsGenerator(avoidAgents),
+                        new AgentRule.ColocateAgentsGenerator(collocateAgents)));
+            } else {
+                // avoid enforcement only
+                placement = Optional.of(new AgentRule.AvoidAgentsGenerator(avoidAgents));
+            }
+        } else if (!collocateAgents.isEmpty()) {
+            // collocate enforcement only
+            placement = Optional.of(new AgentRule.ColocateAgentsGenerator(collocateAgents));
+        } else {
+            // no collocate/avoid enforcement
+            placement = Optional.empty();
+        }
+
+        return placement;
+    }
+}
