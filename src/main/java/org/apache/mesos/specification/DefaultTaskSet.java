b2a24a9e2324cdff1236bb554ed1c2c4052c17de
Gabriel Hartmann <gabriel@mesosphere.io>
2016-10-09 11:15:37 -0700

    Add DefaultTaskSet creation method which does not need a PlacementRule

--- src/main/java/org/apache/mesos/specification/DefaultTaskSet.java
+++ src/main/java/org/apache/mesos/specification/DefaultTaskSet.java
@@ -21,6 +21,15 @@ public class DefaultTaskSet implements TaskSet {
             String name,
             Protos.CommandInfo command,
             Collection<ResourceSpecification> resources,
+            Collection<VolumeSpecification> volumes) {
+        return create(count, name, command, resources, volumes, Optional.empty());
+    }
+
+    public static DefaultTaskSet create(
+            int count,
+            String name,
+            Protos.CommandInfo command,
+            Collection<ResourceSpecification> resources,
             Collection<VolumeSpecification> volumes,
             Optional<PlacementRuleGenerator> placementOptional) {
 
