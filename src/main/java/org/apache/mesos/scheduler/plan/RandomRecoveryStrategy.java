b7b809193177dbed36202bda23ce19d73be4276d
Mohit Soni <mohitsoni@users.noreply.github.com>
2016-10-11 15:07:30 -0700

    Ensuring that RandomRecoveryStrategy works well with mostly and all COMPLETED blocks (#205)

--- src/main/java/org/apache/mesos/scheduler/plan/RandomRecoveryStrategy.java
+++ src/main/java/org/apache/mesos/scheduler/plan/RandomRecoveryStrategy.java
@@ -1,10 +1,10 @@
 package org.apache.mesos.scheduler.plan;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.apache.commons.collections.CollectionUtils;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * {@code RandomRecoveryStrategy} extends {@link DefaultInstallStrategy}, by providing a random block selection
@@ -17,11 +17,22 @@ public class RandomRecoveryStrategy extends DefaultInstallStrategy {
 
     @Override
     public Optional<Block> getCurrentBlock() {
-        final List<? extends Block> blocks = getPhase().getBlocks();
+        final List<? extends Block> blocks = filterOutCompletedBlocks(getPhase().getBlocks());
         if (isInterrupted() || CollectionUtils.isEmpty(blocks)) {
             return Optional.empty();
         } else {
             return Optional.of(blocks.get(new Random().nextInt(blocks.size())));
         }
+    }
+
+    /**
+     * Filters blocks that are already COMPLETE.
+     */
+    @VisibleForTesting
+    protected static List<Block> filterOutCompletedBlocks(List<? extends Block> blocks) {
+        if (blocks == null) {
+            return Arrays.asList();
+        }
+        return blocks.stream().filter(block -> !block.isComplete()).collect(Collectors.toList());
     }
 }
