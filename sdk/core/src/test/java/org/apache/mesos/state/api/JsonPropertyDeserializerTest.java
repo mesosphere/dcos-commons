add6eb4b3bd93ded77b5c20e250ddba2fb28f999
Nicholas Parker <nick@nickbp.com>
2016-10-26 13:20:01 -0700

    Simplify test

--- sdk/core/src/test/java/org/apache/mesos/state/api/JsonPropertyDeserializerTest.java
+++ sdk/core/src/test/java/org/apache/mesos/state/api/JsonPropertyDeserializerTest.java
@@ -17,8 +17,7 @@ public class JsonPropertyDeserializerTest {
     @Test
     public void testToJsonString() throws Exception {
         boolean b = false;
-        String bstr = SerializationUtils.toJsonString(b);
-        byte[] barr = bstr.getBytes("UTF-8");
+        byte[] barr = SerializationUtils.toJsonString(b).getBytes(StandardCharsets.UTF_8);
         String bjson = deserializer.toJsonString("key", barr);
         assertEquals(bjson, "false");
     }
