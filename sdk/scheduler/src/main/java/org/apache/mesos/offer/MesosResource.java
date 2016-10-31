606ef8eff2027374128ee98232c576a734e36a1f
Nick <nick@mesosphere.io>
2016-10-31 13:48:27 -0700

    Reindent MesosResource, add toString(), mark members `final`.

--- sdk/scheduler/src/main/java/org/apache/mesos/offer/MesosResource.java
+++ sdk/scheduler/src/main/java/org/apache/mesos/offer/MesosResource.java
@@ -1,5 +1,6 @@
 package org.apache.mesos.offer;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.mesos.Protos.Label;
 import org.apache.mesos.Protos.Labels;
 import org.apache.mesos.Protos.Resource;
@@ -7,78 +8,83 @@ import org.apache.mesos.Protos.Resource.DiskInfo.Source;
 import org.apache.mesos.Protos.Value;
 
 /**
- * A representation of a Mesos Resources.
+ * Wrapper around a Mesos {@link Resource}, combined with a resource ID string.
  **/
 public class MesosResource {
-  public static final String RESOURCE_ID_KEY = "resource_id";
-  public static final String DYNAMIC_PORT_KEY = "dynamic_port";
+    public static final String RESOURCE_ID_KEY = "resource_id";
+    public static final String DYNAMIC_PORT_KEY = "dynamic_port";
 
-  private Resource resource;
-  private String resourceId;
+    private final Resource resource;
+    private final String resourceId;
 
-  public MesosResource(Resource resource) {
-    this.resource = resource;
-    this.resourceId = getResourceIdInternal();
-  }
-
-  public Resource getResource() {
-    return resource;
-  }
-
-  public boolean isAtomic() {
-    return resource.hasDisk()
-      && resource.getDisk().hasSource()
-      && resource.getDisk().getSource().getType().equals(Source.Type.MOUNT);
-  }
-
-  public String getName() {
-    return resource.getName();
-  }
-
-  public Value.Type getType() {
-    return resource.getType();
-  }
-
-  public boolean hasResourceId() {
-    return resourceId != null;
-  }
-
-  public String getResourceId() {
-    return resourceId;
-  }
-
-  public boolean hasReservation() {
-    return resource.hasReservation();
-  }
-
-  public Value getValue() {
-    return ValueUtils.getValue(resource);
-  }
-
-  public String getRole() {
-    return resource.getRole();
-  }
-
-  public String getPrincipal() {
-    if (hasReservation() &&
-        resource.getReservation().hasPrincipal()) {
-      return resource.getReservation().getPrincipal();
+    public MesosResource(Resource resource) {
+        this.resource = resource;
+        this.resourceId = getResourceIdInternal();
     }
 
-    return null;
-  }
+    public Resource getResource() {
+        return resource;
+    }
 
-  private String getResourceIdInternal() {
-    if (resource.hasReservation()) {
-      Labels labels = resource.getReservation().getLabels();
+    public boolean isAtomic() {
+        return resource.hasDisk()
+            && resource.getDisk().hasSource()
+            && resource.getDisk().getSource().getType().equals(Source.Type.MOUNT);
+    }
 
-      for (Label label : labels.getLabelsList()) {
-        if (label.getKey().equals(RESOURCE_ID_KEY)) {
-          return label.getValue();
+    public String getName() {
+        return resource.getName();
+    }
+
+    public Value.Type getType() {
+        return resource.getType();
+    }
+
+    public boolean hasResourceId() {
+        return resourceId != null;
+    }
+
+    public String getResourceId() {
+        return resourceId;
+    }
+
+    public boolean hasReservation() {
+        return resource.hasReservation();
+    }
+
+    public Value getValue() {
+        return ValueUtils.getValue(resource);
+    }
+
+    public String getRole() {
+        return resource.getRole();
+    }
+
+    public String getPrincipal() {
+        if (hasReservation() &&
+                resource.getReservation().hasPrincipal()) {
+            return resource.getReservation().getPrincipal();
         }
-      }
+
+        return null;
     }
 
-    return null;
-  }
+    private String getResourceIdInternal() {
+        if (resource.hasReservation()) {
+            Labels labels = resource.getReservation().getLabels();
+
+            for (Label label : labels.getLabelsList()) {
+                if (label.getKey().equals(RESOURCE_ID_KEY)) {
+                    return label.getValue();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
 }
