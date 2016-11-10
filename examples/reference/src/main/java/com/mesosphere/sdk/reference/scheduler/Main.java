a9d9c4c24ecda5b8ad0ce380b53774c430f7c30a
Gabriel Hartmann <gabriel@mesosphere.io>
2016-11-10 13:22:28 -0800

    Fix comment on Main

--- examples/reference/src/main/java/com/mesosphere/sdk/reference/scheduler/Main.java
+++ examples/reference/src/main/java/com/mesosphere/sdk/reference/scheduler/Main.java
@@ -4,6 +4,9 @@ import org.apache.mesos.specification.DefaultService;
 
 import java.io.File;
 
+/**
+ * Main.
+ */
 public class Main {
     public static void main(String[] args) throws Exception {
         new DefaultService(new File(args[0]));
