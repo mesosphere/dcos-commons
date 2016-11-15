e7c08d7e52e530fe0af06efe855a66d6345616f4
Loren Siebert <loren@siebert.org>
2016-11-15 13:37:33 -0800

    Add user-settable plan strategies PARALLEL and SERIAL

--- frameworks/elastic/src/main/java/com/mesosphere/sdk/elastic/scheduler/ElasticScheduler.java
+++ frameworks/elastic/src/main/java/com/mesosphere/sdk/elastic/scheduler/ElasticScheduler.java
@@ -5,6 +5,7 @@ import org.apache.mesos.config.validate.ConfigurationValidator;
 import org.apache.mesos.scheduler.DefaultScheduler;
 import org.apache.mesos.scheduler.plan.*;
 import org.apache.mesos.scheduler.plan.strategy.ParallelStrategy;
+import org.apache.mesos.scheduler.plan.strategy.SerialStrategy;
 import org.apache.mesos.specification.ServiceSpecification;
 import org.apache.mesos.state.StateStore;
 import org.slf4j.Logger;
@@ -17,6 +18,9 @@ import java.util.stream.Collectors;
 
 class ElasticScheduler extends DefaultScheduler {
     private static final Logger LOGGER = LoggerFactory.getLogger(ElasticScheduler.class);
+    private static final String SERIAL = "SERIAL";
+    private static final String PARALLEL = "PARALLEL";
+    private final String strategy = Optional.ofNullable(System.getenv("PLAN_STRATEGY")).orElse(SERIAL);
 
     ElasticScheduler(ServiceSpecification serviceSpecification,
                      StateStore stateStore,
@@ -25,21 +29,24 @@ class ElasticScheduler extends DefaultScheduler {
                      Integer permanentFailureTimeoutSec,
                      Integer destructiveRecoveryDelaySec) {
         super(serviceSpecification, stateStore, configStore, configValidators,
-            Optional.of(permanentFailureTimeoutSec), destructiveRecoveryDelaySec);
+                Optional.of(permanentFailureTimeoutSec), destructiveRecoveryDelaySec);
     }
 
     @Override
     protected void initializeDeploymentPlanManager() {
-        LOGGER.info("Initializing Elastic deployment plan...");
+        LOGGER.info(String.format("Initializing Elastic deployment plan with %s strategy...", strategy));
         DefaultStepFactory stepFactory = new DefaultStepFactory(
-            configStore,
-            stateStore,
-            offerRequirementProvider,
-            taskSpecificationProvider);
+                configStore,
+                stateStore,
+                offerRequirementProvider,
+                taskSpecificationProvider);
         DefaultPhaseFactory phaseFactory = new DefaultPhaseFactory(stepFactory);
-        List<Phase> phases = serviceSpecification.getTaskSets().stream().map(taskSet -> phaseFactory.getPhase(taskSet,
-            new ParallelStrategy<>())).collect(Collectors.toList());
-        Plan plan = new DefaultPlan("elastic_plan", phases, new ParallelStrategy<>());
+        List<Phase> phases = serviceSpecification.getTaskSets().stream().
+                map(taskSet -> phaseFactory.getPhase(taskSet,
+                        strategy.equals(PARALLEL) ? new ParallelStrategy<>() : new SerialStrategy<>())).
+                collect(Collectors.toList());
+        Plan plan = new DefaultPlan("elastic_plan", phases,
+                strategy.equals(PARALLEL) ? new ParallelStrategy<>() : new SerialStrategy<>());
         deploymentPlanManager = new DefaultPlanManager(plan);
     }
 
