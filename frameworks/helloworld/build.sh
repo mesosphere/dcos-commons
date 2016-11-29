046531982a71b404762e4890c27d6132453abedf
Gabriel Hartmann <gabriel@mesosphere.io>
2016-11-29 12:10:06 -0800

    Allow frameworks to reference the latest CustomExecutor build. (#326)
    	name is hello-world-scheduler
    	   scheduler/bin -> hello-world-scheduler/bin
    
    	update and fix problem in test cases - 	velocity was failing!
    
    	Added test cases for all those three different configuration!
    
    	update README.md
    
    	Three sample configuration files are given here (change the name of the input yml file at "universe/marathon.json.mustache"):
    	       - svc.yml  (only one pod)
    	       - svc_simple.yml (two pods, with disk)
    	       - svc_plan.yml (two pods, with healthcheck, with disk, with port, and with a plan structure)

--- frameworks/helloworld/build.sh
+++ frameworks/helloworld/build.sh
@@ -5,7 +5,11 @@ set -e
 curl https://mesosphere.com/wp-content/themes/mesosphere/library/images/assets/sdk/build-sh-start.png >/dev/null 2>&1
 
 FRAMEWORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-${FRAMEWORK_DIR}/../../tools/build_framework.sh hello-world $FRAMEWORK_DIR $1
+ROOT_DIR=$FRAMEWORK_DIR/../..
+BUILD_DIR=$ROOT_DIR/build/distributions
+PUBLISH_STEP=${1-none}
+${ROOT_DIR}/gradlew distZip -p ${ROOT_DIR}/sdk/executor
+${ROOT_DIR}/tools/build_framework.sh $PUBLISH_STEP hello-world $FRAMEWORK_DIR $BUILD_DIR/executor.zip $BUILD_DIR/hello-world-scheduler.zip
 
 # capture anonymous metrics for reporting
 curl https://mesosphere.com/wp-content/themes/mesosphere/library/images/assets/sdk/build-sh-finish.png >/dev/null 2>&1
