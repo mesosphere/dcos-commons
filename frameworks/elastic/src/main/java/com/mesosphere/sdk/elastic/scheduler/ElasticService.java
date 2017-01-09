e0de271d1a6def8e2580e0817d7d8218c25c640f
Loren Siebert <loren@siebert.org>
2017-01-09 11:16:37 -0800

    Re-introduce web ui URL

--- frameworks/elastic/src/main/java/com/mesosphere/sdk/elastic/scheduler/ElasticService.java
+++ frameworks/elastic/src/main/java/com/mesosphere/sdk/elastic/scheduler/ElasticService.java
@@ -1,6 +1,9 @@
 package com.mesosphere.sdk.elastic.scheduler;
 
 import com.mesosphere.sdk.specification.DefaultService;
+import org.apache.mesos.Protos;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
@@ -8,9 +11,24 @@ import java.io.File;
  * Service for the Elastic framework.
  */
 public class ElasticService extends DefaultService {
+    protected static final Logger LOGGER = LoggerFactory.getLogger(ElasticService.class);
 
     ElasticService(File pathToYamlSpecification) throws Exception {
         super(pathToYamlSpecification);
     }
+
+    protected static Protos.FrameworkInfo.Builder getFrameworkInfoBuilder(String serviceName) {
+        String webuiUrl = String.format("http://kibana-0-server.%s.mesos:%s", serviceName,
+                System.getenv("KIBANA_PORT"));
+        LOGGER.info("Setting web UI URL: " + webuiUrl);
+
+        return Protos.FrameworkInfo.newBuilder()
+                .setName(serviceName)
+                .setFailoverTimeout(TWO_WEEK_SEC)
+                .setUser(USER)
+                .setWebuiUrl(webuiUrl)
+                .setCheckpoint(true);
+    }
+
 
 }
