d8b3439e5c0b2094bf1a0b81d08354b257864504
Gabriel Hartmann <gabriel@mesosphere.io>
2017-01-13 15:09:27 -0800

    Update gradle and distribution builds.

--- frameworks/helloworld/build.sh
+++ frameworks/helloworld/build.sh
@@ -6,7 +6,7 @@ curl https://mesosphere.com/wp-content/themes/mesosphere/library/images/assets/s
 
 FRAMEWORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$FRAMEWORK_DIR/../..
-BUILD_DIR=$ROOT_DIR/build/distributions
+BUILD_DIR=$FRAMEWORK_DIR/build/distributions
 PUBLISH_STEP=${1-none}
 ${ROOT_DIR}/gradlew distZip -p ${ROOT_DIR}/sdk/executor
 ${ROOT_DIR}/tools/build_framework.sh $PUBLISH_STEP hello-world $FRAMEWORK_DIR $BUILD_DIR/executor.zip $BUILD_DIR/hello-world-scheduler.zip
