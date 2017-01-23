eb393c12790941eca1b61a78909d73f9710885d6
Mikhail Shirkov <shirkevich@gmail.com>
2017-01-23 12:30:51 +0200

    Fix paths to mimic upstream changes

--- frameworks/mongodb-sidecar/build.sh
+++ frameworks/mongodb-sidecar/build.sh
@@ -3,7 +3,7 @@ set -e
 
 FRAMEWORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$FRAMEWORK_DIR/../..
-BUILD_DIR=$ROOT_DIR/build/distributions
+BUILD_DIR=$FRAMEWORK_DIR/build/distributions
 PUBLISH_STEP=${1-none}
 ${ROOT_DIR}/gradlew distZip -p ${ROOT_DIR}/sdk/executor
-${ROOT_DIR}/tools/build_framework.sh $PUBLISH_STEP mongodb-sidecar $FRAMEWORK_DIR $BUILD_DIR/executor.zip $BUILD_DIR/scheduler.zip
+${ROOT_DIR}/tools/build_framework.sh $PUBLISH_STEP mongodb-sidecar $FRAMEWORK_DIR $BUILD_DIR/executor.zip $BUILD_DIR/mongodb-sidecar-scheduler.zip
