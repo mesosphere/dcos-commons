fb2cb8b2b8c4550c48701d2c9611503d7050fba1
Mohit Soni <mohitsoni1989@gmail.com>
2017-01-26 11:14:09 -0800

    Added new prototype framework

--- frameworks/polymorph/README.md
+++
@@ -1,3 +0,0 @@
-# Polymorph
-
-Polymorph is a general purpose framework that takes a DC/OS service specification YAML and runs it as a managed, fault-tolerant, configurable upgradable service.
--- frameworks/prototype/src/main/java/com/mesosphere/sdk/prototype/scheduler/Main.java
+++ frameworks/prototype/src/main/java/com/mesosphere/sdk/prototype/scheduler/Main.java
@@ -1,4 +1,4 @@
-package com.mesosphere.frameworks.polymorph.scheduler;
+package com.mesosphere.sdk.prototype.scheduler;
 
 import com.mesosphere.sdk.specification.DefaultService;
 import org.slf4j.Logger;
@@ -7,22 +7,23 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 
 /**
- * Polymorph Framework.
+ * Prototype Framework.
  */
 public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
         if (args.length == 0) {
-            LOGGER.error(
-                    "No service specification provided. Please provide a YAML file containing service specification.");
+            LOGGER.error("Please provide a YAML file containing service specification.");
         }
-        String pathToServiceSpecification = args[0];
+        final String pathToServiceSpecification = args[0];
         LOGGER.info("Reading service specification from: {}", pathToServiceSpecification);
-        File serviceSpeficiationFile = new File(args[0]);
-        if (!serviceSpeficiationFile.exists()) {
+        final File serviceSpeficiationFile = new File(args[0]);
+        if (serviceSpeficiationFile.exists()) {
+            new DefaultService(serviceSpeficiationFile);
+        } else {
             LOGGER.error("Service specification file doesn't exist at location: {}", pathToServiceSpecification);
+            System.exit(1);
         }
-        new DefaultService(serviceSpeficiationFile);
     }
 }
