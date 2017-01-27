e27a7b9d9e8ed2ea32ab03cd99a596fdadfefa08
Gabriel Hartmann <gabriel@mesosphere.io>
2017-01-27 14:04:31 -0800

    [HDFS] Update HDFS to CDH 5.9.1 and add some tests (#482)
    * [HDFS] Update to hadoop-2.6.0-cdh5.9.1
    
    * Fix prototype CI failures

--- frameworks/hdfs/tests/test_sanity.py
+++ frameworks/hdfs/tests/test_sanity.py
@@ -54,3 +54,79 @@ def test_bump_data_nodes():
 
     check_running(DEFAULT_TASK_COUNT + 1)
     tasks.check_tasks_not_updated(PACKAGE_NAME, 'data', data_ids)
+
+
+@pytest.mark.sanity
+def test_modify_app_config():
+    check_running()
+    app_config_field = 'TASKCFG_ALL_CLIENT_READ_SHORTCIRCUIT_STREAMS_CACHE_SIZE_EXPIRY_MS'
+
+    journal_ids = tasks.get_task_ids(PACKAGE_NAME, 'journal')
+    name_ids = tasks.get_task_ids(PACKAGE_NAME, 'name')
+    zkfc_ids = tasks.get_task_ids(PACKAGE_NAME, 'zkfc')
+    data_ids = tasks.get_task_ids(PACKAGE_NAME, 'data')
+    print('journal ids: ' + str(journal_ids))
+    print('name ids: ' + str(name_ids))
+    print('zkfc ids: ' + str(zkfc_ids))
+    print('data ids: ' + str(data_ids))
+
+    config = marathon.get_config(PACKAGE_NAME)
+    print('marathon config: ')
+    print(config)
+    expiry_ms = int(config['env'][app_config_field])
+    config['env'][app_config_field] = str(expiry_ms + 1)
+    r = cmd.request('put', marathon.api_url('apps/' + PACKAGE_NAME), json=config)
+
+    # All tasks should be updated because hdfs-site.xml has changed
+    tasks.check_tasks_updated(PACKAGE_NAME, 'journal', journal_ids)
+    tasks.check_tasks_updated(PACKAGE_NAME, 'name', name_ids)
+    tasks.check_tasks_updated(PACKAGE_NAME, 'zkfc', zkfc_ids)
+    tasks.check_tasks_updated(PACKAGE_NAME, 'data', journal_ids)
+
+    check_running()
+
+
+@pytest.mark.sanity
+def test_modify_app_config_rollback():
+    check_running()
+    app_config_field = 'TASKCFG_ALL_CLIENT_READ_SHORTCIRCUIT_STREAMS_CACHE_SIZE_EXPIRY_MS'
+
+    journal_ids = tasks.get_task_ids(PACKAGE_NAME, 'journal')
+    name_ids = tasks.get_task_ids(PACKAGE_NAME, 'name')
+    zkfc_ids = tasks.get_task_ids(PACKAGE_NAME, 'zkfc')
+    data_ids = tasks.get_task_ids(PACKAGE_NAME, 'data')
+    print('journal ids: ' + str(journal_ids))
+    print('name ids: ' + str(name_ids))
+    print('zkfc ids: ' + str(zkfc_ids))
+    print('data ids: ' + str(data_ids))
+
+    old_config = marathon.get_config(PACKAGE_NAME)
+    config = marathon.get_config(PACKAGE_NAME)
+    print('marathon config: ')
+    print(config)
+    expiry_ms = int(config['env'][app_config_field])
+    print('expiry ms: ' + str(expiry_ms))
+    config['env'][app_config_field] = str(expiry_ms + 1)
+    r = cmd.request('put', marathon.api_url('apps/' + PACKAGE_NAME), json=config)
+
+    # Wait for journal nodes to be affected by the change
+    tasks.check_tasks_updated(PACKAGE_NAME, 'journal', journal_ids)
+    journal_ids = tasks.get_task_ids(PACKAGE_NAME, 'journal')
+
+    print('old config: ')
+    print(old_config)
+    # Put the old config back (rollback)
+    r = cmd.request('put', marathon.api_url('apps/' + PACKAGE_NAME), json=old_config)
+
+    # Wait for the journal nodes to return to their old configuration
+    tasks.check_tasks_updated(PACKAGE_NAME, 'journal', journal_ids)
+    check_running()
+
+    config = marathon.get_config(PACKAGE_NAME)
+    assert int(config['env'][app_config_field]) == expiry_ms
+
+    # ZKFC and Data tasks should not have been affected
+    tasks.check_tasks_not_updated(PACKAGE_NAME, 'zkfc', zkfc_ids)
+    tasks.check_tasks_not_updated(PACKAGE_NAME, 'data', data_ids)
+
+
