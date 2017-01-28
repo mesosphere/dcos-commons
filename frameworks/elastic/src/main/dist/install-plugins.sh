9906a3494555f81ff1847912a7277f7ac2007a28
Loren Siebert <loren@siebert.org>
2017-01-28 10:02:17 -0800

    Enable statsd reporting for clusters that support it
    - Also add portDefinitions.labels, to make things work again

--- frameworks/elastic/src/main/dist/install-plugins.sh
+++ frameworks/elastic/src/main/dist/install-plugins.sh
@@ -7,6 +7,11 @@ if [ -n "$ELASTICSEARCH_PLUGINS" ]; then
     PLUGINS="$PLUGINS$IFS$ELASTICSEARCH_PLUGINS"
 fi
 
+if [ -n "$STATSD_UDP_HOST" ]; then
+    STATSD_PLUGIN="https://github.com/Automattic/elasticsearch-statsd-plugin/releases/download/$ELASTIC_VERSION.0/elasticsearch-statsd-$ELASTIC_VERSION.0.zip"
+    PLUGINS="$PLUGINS$IFS$STATSD_PLUGIN"
+fi
+
 for PLUGIN in ${PLUGINS}; do
     echo "Installing plugin: $PLUGIN"
     ./elasticsearch-$ELASTIC_VERSION/bin/elasticsearch-plugin install --batch ${PLUGIN}
