fdb825f3d2ce34f24d2a59bc99677f9247743e44
Mehmet <mbalman@mesosphere.com>
2017-01-31 22:15:09 -0800

    svc.yml is updated test is updated

--- frameworks/kafka/src/test/java/com/mesosphere/sdk/kafka/scheduler/ServiceSpecTest.java
+++ frameworks/kafka/src/test/java/com/mesosphere/sdk/kafka/scheduler/ServiceSpecTest.java
@@ -10,25 +10,26 @@ public class ServiceSpecTest extends BaseServiceSpecTest {
         ENV_VARS.set("FRAMEWORK_NAME", "kafka");
         ENV_VARS.set("FRAMEWORK_PRINCIPLE","kafka-principle");
         ENV_VARS.set("MESOS_ZOOKEEPER_URI","master.mesos:2811");
+        ENV_VARS.set("CONFIG_TEMPLATE_PATH","frameworks/kafka");
         ENV_VARS.set("KAFKA_URI", "some_uri");
         ENV_VARS.set("BOOTSTRAP_URI", "another_uri");
         ENV_VARS.set("PLACEMENT_CONSTRAINTS","");
-        ENV_VARS.set("CONFIG_TEMPLATE_PATH","kafka-scheduler");
         ENV_VARS.set("PHASE_STRATEGY","serial");
         ENV_VARS.set("KAFKA_VERSION_PATH","somepath");
-        ENV_VARS.set("LIBMESOS_URI", "");
         ENV_VARS.set("PORT_API", "8080");
         ENV_VARS.set("BROKER_COUNT", "2");
         ENV_VARS.set("BROKER_CPUS", "0.1");
         ENV_VARS.set("BROKER_MEM", "512");
-        ENV_VARS.set("BROKER_DISKSIZE", "5000");
-        ENV_VARS.set("BROKER_DISKTYPE", "ROOT");
+        ENV_VARS.set("BROKER_DISK_SIZE", "5000");
+        ENV_VARS.set("BROKER_DISK_TYPE", "ROOT");
+        ENV_VARS.set("BROKER_DATA_PATH", "ROOT");
         ENV_VARS.set("PORT_BROKER_PORT","9999");
         ENV_VARS.set("BROKER_PORT","0");
+
     }
 
     @Test
     public void testYaml() throws Exception {
-        super.testYaml("svc.yml");
+        super.testYaml("kafka_svc.yml");
     }
 }
