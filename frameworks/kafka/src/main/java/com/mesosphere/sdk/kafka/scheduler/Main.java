837156ce3eba72119a581d613d1df360b2e4fd34
Mehmet <mbalman@mesosphere.com>
2017-01-31 22:03:07 -0800

    marathon.json.mustache config.json resources.json package.json universe updated
    some part of  marathon.json.mustache is autogenerated (overrides in old kafka service)

--- frameworks/kafka/src/main/java/com/mesosphere/sdk/kafka/scheduler/Main.java
+++ frameworks/kafka/src/main/java/com/mesosphere/sdk/kafka/scheduler/Main.java
@@ -1,4 +1,4 @@
-package com.mesosphere.sdk.kafka.scheduler;
+package com.mesosphere.dcos.kafka.scheduler;
 
 import com.mesosphere.sdk.specification.DefaultService;
 
@@ -8,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Kafka service.
+ * Apache Kafka Service.
  */
 public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
