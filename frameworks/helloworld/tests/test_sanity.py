a798e119426a11f4385798431c26ec22b903554e
Gabriel Hartmann <gabriel@mesosphere.io>
2017-02-02 08:26:58 -0800

    [HELLO-WORLD] Add simple recovery test (#502)

--- frameworks/helloworld/tests/test_sanity.py
+++ frameworks/helloworld/tests/test_sanity.py
@@ -131,54 +131,6 @@ def test_pods_info():
 
 
 @pytest.mark.sanity
-def test_pods_restart():
-    hello_ids = tasks.get_task_ids(PACKAGE_NAME, 'hello-0')
-
-    # get current agent id:
-    stdout = cmd.run_cli('hello-world pods info hello-0')
-    old_agent = json.loads(stdout)[0]['info']['slaveId']['value']
-
-    stdout = cmd.run_cli('hello-world pods restart hello-0')
-    jsonobj = json.loads(stdout)
-    assert len(jsonobj) == 2
-    assert jsonobj['pod'] == 'hello-0'
-    assert len(jsonobj['tasks']) == 1
-    assert jsonobj['tasks'][0] == 'hello-0-server'
-
-    tasks.check_tasks_updated(PACKAGE_NAME, 'hello', hello_ids)
-    check_running()
-
-    # check agent didn't move:
-    stdout = cmd.run_cli('hello-world pods info hello-0')
-    new_agent = json.loads(stdout)[0]['info']['slaveId']['value']
-    assert old_agent == new_agent
-
-
-@pytest.mark.sanity
-def test_pods_replace():
-    world_ids = tasks.get_task_ids(PACKAGE_NAME, 'world-0')
-
-    # get current agent id:
-    stdout = cmd.run_cli('hello-world pods info world-0')
-    old_agent = json.loads(stdout)[0]['info']['slaveId']['value']
-
-    jsonobj = json.loads(cmd.run_cli('hello-world pods replace world-0'))
-    assert len(jsonobj) == 2
-    assert jsonobj['pod'] == 'world-0'
-    assert len(jsonobj['tasks']) == 1
-    assert jsonobj['tasks'][0] == 'world-0-server'
-
-    tasks.check_tasks_updated(PACKAGE_NAME, 'world-0', world_ids)
-    check_running()
-
-    # check agent moved:
-    stdout = cmd.run_cli('hello-world pods info world-0')
-    new_agent = json.loads(stdout)[0]['info']['slaveId']['value']
-    # TODO: enable assert if/when agent is guaranteed to change (may randomly move back to old agent)
-    #assert old_agent != new_agent
-
-
-@pytest.mark.sanity
 def test_lock():
     '''This test verifies that a second scheduler fails to startup when
     an existing scheduler is running.  Without locking, the scheduler
