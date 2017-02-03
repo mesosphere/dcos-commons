059f6d3c524cf7cdc764161537f1420cdbe7306a
Mehmet <mbalman@mesosphere.com>
2017-02-03 15:51:48 -0800

    apache-kafka prototype in dcos-commons/frameworks/kafka  svc_yml+universe+mustache+resources+config (#501)
    + svc.yml 
    
    + server.properties
    
    + universe:
       * marathon.json.mustache 
       * config.json 
       * resources.json 
       * package.json
    some part of  marathon.json.mustache is autogenerated (overrides in old kafka service)

--- frameworks/kafka/src/test/java/com/mesosphere/sdk/kafka/scheduler/ServiceSpecTest.java
+++ frameworks/kafka/src/test/java/com/mesosphere/sdk/kafka/scheduler/ServiceSpecTest.java
@@ -7,13 +7,25 @@ import org.junit.Test;
 public class ServiceSpecTest extends BaseServiceSpecTest {
     @BeforeClass
     public static void beforeAll() {
-        ENV_VARS.set("EXECUTOR_URI", "");
-        ENV_VARS.set("LIBMESOS_URI", "");
+        ENV_VARS.set("FRAMEWORK_NAME", "kafka");
+        ENV_VARS.set("FRAMEWORK_PRINCIPLE","kafka-principle");
+        ENV_VARS.set("MESOS_ZOOKEEPER_URI","master.mesos:2811");
+        ENV_VARS.set("CONFIG_TEMPLATE_PATH","frameworks/kafka");
+        ENV_VARS.set("KAFKA_URI", "some_uri");
+        ENV_VARS.set("BOOTSTRAP_URI", "another_uri");
+        ENV_VARS.set("PLACEMENT_CONSTRAINTS","");
+        ENV_VARS.set("PHASE_STRATEGY","serial");
+        ENV_VARS.set("KAFKA_VERSION_PATH","somepath");
         ENV_VARS.set("PORT_API", "8080");
         ENV_VARS.set("BROKER_COUNT", "2");
         ENV_VARS.set("BROKER_CPUS", "0.1");
         ENV_VARS.set("BROKER_MEM", "512");
-        ENV_VARS.set("BROKER_DISK", "5000");
+        ENV_VARS.set("BROKER_DISK_SIZE", "5000");
+        ENV_VARS.set("BROKER_DISK_TYPE", "ROOT");
+        ENV_VARS.set("BROKER_DATA_PATH", "ROOT");
+        ENV_VARS.set("PORT_BROKER_PORT","9999");
+        ENV_VARS.set("BROKER_PORT","0");
+
     }
 
     @Test
