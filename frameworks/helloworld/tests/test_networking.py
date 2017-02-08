689704c8a556faeaecf5a13701390c7fcbcf040a
Michael Browning <mbrowning@mesosphere.com>
2017-02-07 17:48:29 -0800

    Clarify purpose of individual networking tests

--- frameworks/helloworld/tests/test_networking.py
+++ frameworks/helloworld/tests/test_networking.py
@@ -18,11 +18,11 @@ def setup_module(module):
     }
 
     install.install(PACKAGE_NAME, 1, additional_options=options)
-    print('done installing...')
 
 
 @pytest.mark.sanity
 def test_deploy():
+    """Verify that the current deploy plan matches the expected plan from the spec."""
     deployment_plan = plan.get_deployment_plan(PACKAGE_NAME).json()
     print("deployment_plan: " + str(deployment_plan))
 
@@ -33,5 +33,9 @@ def test_deploy():
 
 @pytest.mark.sanity
 def test_joins_overlay_network():
+    """Verify that the container joined the dcos subnet at 9.0.0.0/24.
+    
+    The logic for this is in the task itself, which will check the container IP address
+    and fail if incorrect, thus preventing the plan from reaching the COMPLETE state."""
     spin.time_wait_noisy(lambda: (
         plan.get_deployment_plan(PACKAGE_NAME).json()['status'] == 'COMPLETE'))
