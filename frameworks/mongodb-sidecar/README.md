59922e6e7f2088c69e3fe0cc880a3ae729c3f416
Mikhail Shirkov <shirkevich@gmail.com>
2017-02-10 21:05:00 +0300

    Updated build section in Readme; bumped sidecer version to 0.0.23; simplify basic test

--- frameworks/mongodb-sidecar/README.md
+++ frameworks/mongodb-sidecar/README.md
@@ -2,20 +2,21 @@
 **Replica set functionality only**
 
 This is MongoDB PoC replica set sidecar for DC/OS. All mongo replica set init/add/removal logic is a duty of sidecar application.
-For lazy fellas who prefer to express dummy application logic with dumb
 
 *Warning! Experimental quality software.*
 
-
-
 ## Sidecar
-Uses mongo binary to execute init/add/remove commands against replica set. Current RS members and candidates for removal are stored in ZK and processed with each sidecar loop.
+We use sidecar application along with default java dcos scheduler to simplify framework operations agains mongo.
+Mongo binary is used for initialization of RS and [ruby](http://api.mongodb.com/ruby/current/) mongo driver to manage RS state and query for current status. RS members are stored in ZK at /dcos/frameworks/#{framework_name}/rs_members path.
+
+## UI
+Management interface is available at http://sidecar-0-companion.{{FRAMEWORK_NAME}}.mesos:4567
 
 ## YML specification
-Framework behaviour defined with [svc.yml](/frameworks/mongodb-sidecar/src/main/dist/svc.yml) specification file. It provides description for constructing pods, resources, ports, deploy/recovery plans, phases and so on.
+Framework is defined with [svc.yml](/frameworks/mongodb-sidecar/src/main/dist/svc.yml) specification. It defined tasks for mongo server nodes, sidecar companion, and deploy plan.
 
 ## Settings
-You can change defaults in  [config.json](/frameworks/mongodb-sidecar/universe/config.json) or provide overrides with json in command line or via UI installation.
+Defaults settings can be changed in [config.json](/frameworks/mongodb-sidecar/universe/config.json) or can be overridden with json in command line or via UI installation.
 
 {
   "mongodb": {
@@ -25,28 +26,33 @@ You can change defaults in  [config.json](/frameworks/mongodb-sidecar/universe/c
   }
 }
 
-## Default framework
-Default framework sends mongo agent permanent failures notifications to sidecar with
-[CustomHook](frameworks/mongodb-sidecar/src/main/java/fwd/cloud/frameworks/mongodbsidecar/scheduler/CustomHook.java). [PodSpecsCannotShrink](sdk/scheduler/src/main/java/com/mesosphere/sdk/config/validate/PodSpecsCannotShrink.java) validation were removed from default service specification to experiment with downscaling.
+## Build
 
-## Repository
-Testing builds distributed to S3 and can be listed with  
-`aws s3 ls khufu-dcos-stub-universe/autodelete7d/mongodb-replicaset/`
+## dcos-commons part:
+To build mognodb sidecar
+1. clone repo, checkout mongodb-sidecar branch
+2. navigate to frameworks/mongodb-sidecar folder
+3. AWS_PROFILE=profile S3_BUCKET=your-fancy-bucket TEMPLATE_PACKAGE_VERSION=x.y.z S3_DIR_PATH=frameworks ./build.sh aws
 
-You should provide full path to mongodb-sidecar zip package when adding to DC/OS.
-
-`https://khufu-dcos-stub-universe.s3.amazonaws.com/autodelete7d/mongodb-sidecar/20170119-152649-vt6fGhDm
-pm5Hz4il/stub-universe-mongodb-sidecar.zip`
+This will build mongodb-sidecar artifact and put it to provided s3 bucket. It will also display instruction how to add it to your DC/OS universe.
 
 
-## TEST ME!
+## ruby sidecar container
+1. navigate to frameworks/mongodb-sidecar/src/main/docker
+2. docker build -t dockerhub_account/mongodb-sidecar:x.y.z .
+3. docker push dockerhub_account/mongodb-sidecar:x.y.z
 
+You can override sidecar container version in svc.yml specification or in DC/OS ui.
+
+## Limitations
+* Downscaling is not possible at the moment.
+* Removal mongo node from RS on task replacement should be done by hand with UI.
+*
 
 ## TODO
+* CNI
 * Use ZK for CustomHook to store replace events.
 * Create recovery plan to recreate replica set from known nodes.  
-* Decide how to distribute artifacts https://serv.sh/ or company S3.
-* CNI
 * Shakedown tests
 
 
--- frameworks/mongodb-sidecar/src/test/java/fwd/cloud/frameworks/mongodbsidecar/scheduler/ServiceSpecTest.java
+++ frameworks/mongodb-sidecar/src/test/java/fwd/cloud/frameworks/mongodbsidecar/scheduler/ServiceSpecTest.java
@@ -1,97 +1,38 @@
 package fwd.cloud.frameworks.mongodbsidecar.scheduler;
 
-import com.mesosphere.sdk.dcos.Capabilities;
-import org.apache.curator.test.TestingServer;
-import org.apache.mesos.SchedulerDriver;
-import com.mesosphere.sdk.scheduler.DefaultScheduler;
-import com.mesosphere.sdk.specification.DefaultServiceSpec;
-import com.mesosphere.sdk.specification.yaml.YAMLServiceSpecFactory;
-import com.mesosphere.sdk.state.StateStoreCache;
-import org.junit.*;
-import org.junit.contrib.java.lang.system.EnvironmentVariables;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import com.mesosphere.sdk.testing.BaseServiceSpecTest;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-import java.io.File;
-import java.util.Collections;
-
-import static com.mesosphere.sdk.specification.yaml.YAMLServiceSpecFactory.generateRawSpecFromYAML;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-public class ServiceSpecTest {
-
-    @ClassRule
-    public static final EnvironmentVariables environmentVariables = new EnvironmentVariables();
-
-    @Mock
-    private SchedulerDriver mockSchedulerDriver;
+public class ServiceSpecTest extends BaseServiceSpecTest {
 
     @BeforeClass
     public static void beforeAll() {
-        environmentVariables.set("SERVICE_NAME", "mongodb-sidecar");
-        environmentVariables.set("SERVICE_PRINCIPAL", "mongodb-sidecar-principal");
-        environmentVariables.set("EXECUTOR_URI", "");
-        environmentVariables.set("PORT_API", "8080");
-        environmentVariables.set("MONGODB_PORT", "27017");
-        environmentVariables.set("MONGODB_REST_PORT", "28017");
-        environmentVariables.set("MONGODB_COUNT", "3");
-        environmentVariables.set("MONGODB_CPUS", "0.1");
-        environmentVariables.set("MONGODB_MEM", "1024");
-        environmentVariables.set("MONGODB_DISK", "1000");
-        environmentVariables.set("SIDECAR_COUNT", "1");
-        environmentVariables.set("SIDECAR_CPUS", "0.2");
-        environmentVariables.set("SIDECAR_MEM", "128");
-        environmentVariables.set("SIDECAR_DISK", "0");
-        environmentVariables.set("SIDECAR_APP_NAME", "blub");
-        environmentVariables.set("SIDECAR_ADD_DELAY", "10");
-        environmentVariables.set("SIDECAR_IMAGE", "alpine");
-        environmentVariables.set("SIDECAR_CMD", "sleep 100");
-        environmentVariables.set("SIDECAR_ZK_URL", "zk://zk-1.zk:2181");
-        environmentVariables.set("SIDECAR_MESOS_URL", "http://master.mezos/mesos");
-
-
-        // URL resource = ServiceSpecTest.class.getClassLoader().getResource("init.sh.mustache");
-        // environmentVariables.set("CONFIG_TEMPLATE_PATH", new File(resource.getPath()).getParent());
-        environmentVariables.set("LIBMESOS_URI", "");
-    }
-
-    @Before
-    public void beforeEach() {
-        MockitoAnnotations.initMocks(this);
+        ENV_VARS.set("EXECUTOR_URI", "");
+        ENV_VARS.set("LIBMESOS_URI", "");
+        ENV_VARS.set("PORT_API", "8080");
+        ENV_VARS.set("FRAMEWORK_NAME", "mongodb-sidecar");
+        ENV_VARS.set("SERVICE_PRINCIPAL", "mongodb-sidecar-principal");
+        ENV_VARS.set("MONGODB_PORT", "27017");
+        ENV_VARS.set("MONGODB_REST_PORT", "28017");
+        ENV_VARS.set("MONGODB_COUNT", "3");
+        ENV_VARS.set("MONGODB_CPUS", "0.1");
+        ENV_VARS.set("MONGODB_MEM", "1024");
+        ENV_VARS.set("MONGODB_DISK", "1000");
+        ENV_VARS.set("SIDECAR_COUNT", "1");
+        ENV_VARS.set("SIDECAR_CPUS", "0.2");
+        ENV_VARS.set("SIDECAR_MEM", "128");
+        ENV_VARS.set("SIDECAR_DISK", "0");
+        ENV_VARS.set("SIDECAR_APP_NAME", "blub");
+        ENV_VARS.set("SIDECAR_ADD_DELAY", "10");
+        ENV_VARS.set("SIDECAR_IMAGE", "alpine");
+        ENV_VARS.set("SIDECAR_CMD", "sleep 100");
+        ENV_VARS.set("SIDECAR_ZK_URL", "zk://zk-1.zk:2181");
+        ENV_VARS.set("SIDECAR_MESOS_URL", "http://master.mezos/mesos");
     }
 
     @Test
     public void testYmlBase() throws Exception {
-        deserializeServiceSpec("svc.yml");
-        validateServiceSpec("svc.yml");
-    }
-
-    private void deserializeServiceSpec(String fileName) throws Exception {
-        File file = new File(getClass().getClassLoader().getResource(fileName).getFile());
-        DefaultServiceSpec serviceSpec = YAMLServiceSpecFactory
-                .generateServiceSpec(generateRawSpecFromYAML(file));
-        Assert.assertNotNull(serviceSpec);
-        Assert.assertEquals(8080, serviceSpec.getApiPort());
-        DefaultServiceSpec.getFactory(serviceSpec, Collections.emptyList());
-    }
-
-    private void validateServiceSpec(String fileName) throws Exception {
-        File file = new File(getClass().getClassLoader().getResource(fileName).getFile());
-        DefaultServiceSpec serviceSpec = YAMLServiceSpecFactory.generateServiceSpec(generateRawSpecFromYAML(file));
-
-        TestingServer testingServer = new TestingServer();
-        StateStoreCache.resetInstanceForTests();
-
-        Capabilities capabilities = mock(Capabilities.class);
-        when(capabilities.supportsNamedVips()).thenReturn(true);
-        when(capabilities.supportsRLimits()).thenReturn(true);
-
-        DefaultScheduler.newBuilder(serviceSpec)
-                .setStateStore(DefaultScheduler.createStateStore(serviceSpec, testingServer.getConnectString()))
-                .setConfigStore(DefaultScheduler.createConfigStore(serviceSpec, testingServer.getConnectString()))
-                .setCapabilities(capabilities)
-                .build();
-        testingServer.close();
+        testYaml("svc.yml");
     }
 }
