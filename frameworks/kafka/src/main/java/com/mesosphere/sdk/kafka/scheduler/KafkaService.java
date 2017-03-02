87eaf579bf90c311a6b292ed0fab41d7387390b1
Mehmet <mbalman@mesosphere.com>
2017-03-01 17:06:36 -0800

    [KAFKA] Add TopicController endpoint API and Command Executor class (#610)
    * [KAFKA] Add TopicController endpoint API and Command Executor class
    
    * Address review comments

--- frameworks/kafka/src/main/java/com/mesosphere/sdk/kafka/scheduler/KafkaService.java
+++ frameworks/kafka/src/main/java/com/mesosphere/sdk/kafka/scheduler/KafkaService.java
@@ -1,22 +1,25 @@
 package com.mesosphere.sdk.kafka.scheduler;
 
-import com.mesosphere.sdk.kafka.api.BrokerController;
+import com.mesosphere.sdk.kafka.api.BrokerResource;
+import com.mesosphere.sdk.kafka.api.KafkaZKClient;
+import com.mesosphere.sdk.kafka.api.TopicResource;
+import com.mesosphere.sdk.kafka.cmd.CmdExecutor;
 import com.mesosphere.sdk.scheduler.DefaultScheduler;
 import com.mesosphere.sdk.specification.DefaultService;
 import com.mesosphere.sdk.specification.ServiceSpec;
-import org.apache.commons.collections.CollectionUtils;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
 /**
  * Kafka Service.
  */
 public class KafkaService extends DefaultService {
     protected static final Logger LOGGER = LoggerFactory.getLogger(KafkaService.class);
+
 
     public KafkaService(File pathToYamlSpecification) throws Exception {
         super(pathToYamlSpecification);
@@ -30,11 +33,14 @@ public class KafkaService extends DefaultService {
         final Collection<Object> apiResources = new ArrayList<>();
         final String zkUri = String.format("%s/dcos-service-%s",
                 serviceSpec.getZookeeperConnection(), serviceSpec.getName());
-        apiResources.add(new BrokerController(zkUri));
-        if (CollectionUtils.isNotEmpty(additionalResources)) {
-            apiResources.addAll(additionalResources);
-        }
-        LOGGER.info("Starting API server with resources: {}", apiResources);
+        final KafkaZKClient kafkaZKClient = new KafkaZKClient(zkUri);
+
+        apiResources.add(new BrokerResource(kafkaZKClient));
+        apiResources.add(new TopicResource(new CmdExecutor(kafkaZKClient, System.getenv("KAFKA_VERSION_PATH")),
+                kafkaZKClient));
+
+        apiResources.addAll(additionalResources);
+        LOGGER.info("Starting API server with additional resources: {}", apiResources);
         super.startApiServer(defaultScheduler, apiPort, apiResources);
     }
 }
