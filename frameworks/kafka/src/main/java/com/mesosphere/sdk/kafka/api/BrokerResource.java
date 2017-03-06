1c32bce401c27a54402caaa95b414e11e36f9b0c
Mehmet <mbalman@mesosphere.com>
2017-03-06 12:56:51 -0800

    [KAFKA] Fix BrokerResource TopicController endpoints

--- frameworks/kafka/src/main/java/com/mesosphere/sdk/kafka/api/BrokerResource.java
+++ frameworks/kafka/src/main/java/com/mesosphere/sdk/kafka/api/BrokerResource.java
@@ -1,13 +1,12 @@
 package com.mesosphere.sdk.kafka.api;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.json.JSONObject;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.PathParam;
+import org.json.JSONObject;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Optional;
@@ -18,7 +17,7 @@ import java.util.Optional;
 @Path("/v1/brokers")
 @Produces("application/json")
 public class BrokerResource {
-    private final Log log = LogFactory.getLog(BrokerResource.class);
+    private final Logger log = LoggerFactory.getLogger(BrokerResource.class);
     private KafkaZKClient kafkaZkClient;
 
     public BrokerResource(KafkaZKClient kafkaZkClient) {
@@ -39,9 +38,10 @@ public class BrokerResource {
     @Path("/{id}")
     public Response getBroker(@PathParam("id") String id) {
         try {
-            Optional<JSONObject> brokerObj = kafkaZkClient.getBroker(id);
-            if (brokerObj.isPresent()){
-                return Response.ok(brokerObj.get(), MediaType.WILDCARD_TYPE.APPLICATION_JSON).build();
+            Optional<JSONObject> optionalBroker = kafkaZkClient.getBroker(id);
+            if (optionalBroker.isPresent()){
+                log.info(" Broker id: {} content: {}", id, optionalBroker.get());
+                return Response.ok(optionalBroker.get().toString(), MediaType.APPLICATION_JSON).build();
             }
             return Response.status(Response.Status.NOT_FOUND).build();
         } catch (Exception ex) {
