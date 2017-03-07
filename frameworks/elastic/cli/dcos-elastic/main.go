a198a97996e8ff12fdce099ab36449a47659bd4a
Loren Siebert <loren@siebert.org>
2017-03-07 11:31:57 -0800

    [ELASTIC-45] Fixes #531 Elastic CLI help errors (#651)
    - Changed boilerplate text
    - Removed unused example section

--- frameworks/elastic/cli/dcos-elastic/main.go
+++ frameworks/elastic/cli/dcos-elastic/main.go
@@ -14,17 +14,16 @@ func main() {
 		log.Fatalf(err.Error())
 	}
 
-	app, err := cli.NewApp("0.1.0", "Mesosphere", "Provides an example for DC/OS service developers")
+	app, err := cli.NewApp("0.1.0", "Mesosphere", "Manage Elastic framework")
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 
-	cli.HandleCommonFlags(app, modName, "Example DC/OS CLI Module")
+	cli.HandleCommonFlags(app, modName, "Elastic CLI")
 	cli.HandleConfigSection(app)
 	cli.HandleEndpointsSection(app)
 	cli.HandlePlanSection(app)
 	cli.HandlePodsSection(app)
-	handleExampleSection(app)
 
 	// Omit modname:
 	kingpin.MustParse(app.Parse(cli.GetArguments()))
@@ -44,12 +43,3 @@ func (cmd *ExampleCommand) runEcho(c *kingpin.ParseContext) error {
 	return nil
 }
 
-func handleExampleSection(app *kingpin.Application) {
-	cmd := &ExampleCommand{}
-	example := app.Command("example", "Example custom commands")
-
-	// 'example echo -n <text>'
-	echo := example.Command("echo", "Echos some text").Action(cmd.runEcho)
-	echo.Arg("text", "What to echo").StringsVar(&cmd.echoText)
-	echo.Flag("no-newline", "Omit newline").Short('n').BoolVar(&cmd.echoOmitNewline)
-}
