6ffd82fc22c4ff412e1f50841fff37248161bc9f
mgummelt <mgummelt1@gmail.com>
2017-03-07 12:51:32 -0800

    [INFINITY-1074] Authorize marathon to launch tasks as root in strict mode tests (#645)
    * Authorize marathon to launch tasks as root in strict mode tests
    
    * remove comments
    
    * revert explicit service-acct parameters

--- tools/setup_permissions.sh
+++ tools/setup_permissions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 SERVICE_ACCOUNT_NAME=service-acct
-SECRET_NAME=secret
 LINUX_USER=$1
 ROLE=$2
+
 if [ -n "$CLUSTER_URL" ]
 then
     # CI
@@ -11,6 +11,7 @@ then
 else
     DCOS_URL=$(dcos config show core.dcos_url)
 fi
+
 if [ -n "$CLUSTER_AUTH_TOKEN" ]
 then
     # CI
@@ -19,81 +20,94 @@ else
     ACS_TOKEN=$(dcos config show core.dcos_acs_token)
 fi
 
-echo Setting up permissions for user=$LINUX_USER role=$ROLE
 
-echo Role can register a framework...
-curl -k -L -X PUT \
-     -H "Authorization: token=${ACS_TOKEN}" \
-     "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:framework:role:${ROLE}" \
-     -d '{"description":"Allows ${ROLE} to register as a framework with the Mesos master"}' \
-     -H 'Content-Type: application/json'
 
-echo User can execute tasks...
-curl -k -L -X PUT \
-     -H "Authorization: token=${ACS_TOKEN}" \
-     "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:task:user:${LINUX_USER}" \
-     -d '{"description":"Allows ${LINUX_USER} to execute tasks"}' \
-     -H 'Content-Type: application/json'
+function grant_registration() {
+    echo "Authorizing ${SERVICE_ACCOUNT_NAME} to register as a Mesos framework with role=${ROLE}"
 
-echo Role can reserve resources...
-curl -k -L -X PUT \
-     -H "Authorization: token=${ACS_TOKEN}" \
-     "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:reservation:role:${ROLE}" \
-     -d '{"description":"Allows ${ROLE} to reserve resources"}' \
-     -H 'Content-Type: application/json'
+    curl -k -L -X PUT \
+         -H "Authorization: token=${ACS_TOKEN}" \
+         "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:framework:role:${ROLE}" \
+         -d '{"description":"Register with the Mesos master with role=${ROLE}"}' \
+         -H 'Content-Type: application/json'
+    curl -k -L -X PUT \
+         -H "Authorization: token=${ACS_TOKEN}" \
+         "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:framework:role:${ROLE}/users/${SERVICE_ACCOUNT_NAME}/create"
+}
 
-echo Role can unreserve resources...
-curl -k -L -X PUT \
-     -H "Authorization: token=${ACS_TOKEN}" \
-     "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:reservation:principal:${SERVICE_ACCOUNT_NAME}" \
-     -d '{"description":"Allows ${SERVICE_ACCOUNT_NAME} to unreserve resources"}' \
-     -H 'Content-Type: application/json'
+function grant_task_execution() {
+    echo "Authorizing ${SERVICE_ACCOUNT_NAME} to execute Mesos tasks as user=${LINUX_USER}"
+    curl -k -L -X PUT \
+         -H "Authorization: token=${ACS_TOKEN}" \
+         "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:task:user:${LINUX_USER}" \
+         -d '{"description":"Execute Mesos tasks as user=${LINUX_USER}"}' \
+         -H 'Content-Type: application/json'
+    curl -k -L -X PUT \
+         -H "Authorization: token=${ACS_TOKEN}" \
+         "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:task:user:${LINUX_USER}/users/${SERVICE_ACCOUNT_NAME}/create"
 
-echo Role can create volumes...
-curl -k -L -X PUT \
-     -H "Authorization: token=${ACS_TOKEN}" \
-     "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:volume:role:${ROLE}" \
-     -d '{"description":"Allows ${ROLE} to create volumes"}' \
-     -H 'Content-Type: application/json'
+    # In order for the Spark Dispatcher to register with Mesos as
+    # root, we must launch the dispatcher task as root.  The other
+    # frameworks are launched as nobody, but then register as
+    # service.user, which defaults to root
+    echo "Authorizing dcos_marathon to execute Mesos tasks as root"
+    curl -k -L -X PUT \
+         -H "Authorization: token=${ACS_TOKEN}" \
+         "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:task:user:root" \
+         -d '{"description":"Execute Mesos tasks as user=root"}' \
+         -H 'Content-Type: application/json'
+    curl -k -L -X PUT \
+         -H "Authorization: token=${ACS_TOKEN}" \
+         "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:task:user:root/users/dcos_marathon/create"
+}
 
-echo Role can destroy volumes...
-curl -k -L -X PUT \
-     -H "Authorization: token=${ACS_TOKEN}" \
-     "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:volume:principal:${SERVICE_ACCOUNT_NAME}" \
-     -d '{"description":"Allows ${SERVICE_ACCOUNT_NAME} to destroy volumes"}' \
-     -H 'Content-Type: application/json'
+function grant_resources {
+    echo "Authorizing ${SERVICE_ACCOUNT_NAME} to reserve Mesos resources with role=${ROLE}"
+    curl -k -L -X PUT \
+         -H "Authorization: token=${ACS_TOKEN}" \
+         "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:reservation:role:${ROLE}" \
+         -d '{"description":"Reserve Mesos resources with role=${ROLE}"}' \
+         -H 'Content-Type: application/json'
+    curl -k -L -X PUT \
+         -H "Authorization: token=${ACS_TOKEN}" \
+         "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:reservation:role:${ROLE}/users/${SERVICE_ACCOUNT_NAME}/create"
 
-echo Assign permissions...
-curl -k -L -X PUT \
-     -H "Authorization: token=${ACS_TOKEN}" \
-     "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:framework:role:${ROLE}/users/${SERVICE_ACCOUNT_NAME}/create"
+    echo "Authorizing ${SERVICE_ACCOUNT_NAME} to unreserve Mesos resources with principal=${SERVICE_ACCOUNT_NAME}"
+    curl -k -L -X PUT \
+         -H "Authorization: token=${ACS_TOKEN}" \
+         "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:reservation:principal:${SERVICE_ACCOUNT_NAME}" \
+         -d '{"description":"Reserve Mesos resources with principal=${SERVICE_ACCOUNT_NAME}"}' \
+         -H 'Content-Type: application/json'
+    curl -k -L -X PUT \
+         -H "Authorization: token=${ACS_TOKEN}" \
+         "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:reservation:principal:${SERVICE_ACCOUNT_NAME}/users/${SERVICE_ACCOUNT_NAME}/delete"
+}
 
-curl -k -L -X PUT \
-     -H "Authorization: token=${ACS_TOKEN}" \
-     "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:framework:role:${ROLE}/users/${SERVICE_ACCOUNT_NAME}/delete"
+function grant_volumes {
+    echo "Authorizing ${SERVICE_ACCOUNT_NAME} to create Mesos volumes with role=${ROLE}"
+    curl -k -L -X PUT \
+         -H "Authorization: token=${ACS_TOKEN}" \
+         "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:volume:role:${ROLE}" \
+         -d '{"description":"Create Mesos volumes with role=${ROLE}"}' \
+         -H 'Content-Type: application/json'
+    curl -k -L -X PUT \
+         -H "Authorization: token=${ACS_TOKEN}" \
+         "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:volume:role:${ROLE}/users/${SERVICE_ACCOUNT_NAME}/create"
 
-curl -k -L -X PUT \
-     -H "Authorization: token=${ACS_TOKEN}" \
-     "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:task:user:${LINUX_USER}/users/${SERVICE_ACCOUNT_NAME}/create"
+    echo "Authorizing ${SERVICE_ACCOUNT_NAME} to delete Mesos volumes with principal=${SERVICE_ACCOUNT_NAME}"
+    curl -k -L -X PUT \
+         -H "Authorization: token=${ACS_TOKEN}" \
+         "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:volume:principal:${SERVICE_ACCOUNT_NAME}" \
+         -d '{"description":"Create Mesos volumes with principal=${SERVICE_ACCOUNT_NAME}"}' \
+         -H 'Content-Type: application/json'
+    curl -k -L -X PUT \
+         -H "Authorization: token=${ACS_TOKEN}" \
+         "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:volume:principal:${SERVICE_ACCOUNT_NAME}/users/${SERVICE_ACCOUNT_NAME}/delete"
+}
 
-curl -k -L -X PUT \
-     -H "Authorization: token=${ACS_TOKEN}" \
-     "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:task:user:${LINUX_USER}/users/${SERVICE_ACCOUNT_NAME}/delete"
-
-curl -k -L -X PUT \
-     -H "Authorization: token=${ACS_TOKEN}" \
-     "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:reservation:role:${ROLE}/users/${SERVICE_ACCOUNT_NAME}/create"
-
-curl -k -L -X PUT \
-     -H "Authorization: token=${ACS_TOKEN}" \
-     "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:reservation:principal:${SERVICE_ACCOUNT_NAME}/users/${SERVICE_ACCOUNT_NAME}/delete"
-
-curl -k -L -X PUT \
-     -H "Authorization: token=${ACS_TOKEN}" \
-     "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:volume:role:${ROLE}/users/${SERVICE_ACCOUNT_NAME}/create"
-
-curl -k -L -X PUT \
-     -H "Authorization: token=${ACS_TOKEN}" \
-     "${DCOS_URL}/acs/api/v1/acls/dcos:mesos:master:volume:principal:${SERVICE_ACCOUNT_NAME}/users/${SERVICE_ACCOUNT_NAME}/delete"
-
-echo Permission setup completed for user=$LINUX_USER role=$ROLE
+echo "Granting permissions to ${SERVICE_ACCOUNT_NAME}..."
+grant_registration
+grant_task_execution
+grant_resources
+grant_volumes
+echo "Permission setup completed for ${SERVICE_ACCOUNT_NAME}"
