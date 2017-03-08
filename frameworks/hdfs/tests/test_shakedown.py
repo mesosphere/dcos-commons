a5ed9f5f417af66107184aa3548ba0e96055bf7f
nick <nick@mesosphere.io>
2017-03-07 17:33:11 -0800

    More fixes

--- frameworks/hdfs/tests/test_shakedown.py
+++ frameworks/hdfs/tests/test_shakedown.py
@@ -7,7 +7,6 @@ import sdk_cmd as cmd
 import sdk_install as install
 import sdk_marathon as marathon
 import sdk_tasks as tasks
-import sdk_utils as utils
 from tests.config import (
     PACKAGE_NAME,
     check_healthy,
@@ -23,13 +22,13 @@ HDFS_POD_TYPES = {"journal", "name", "data"}
 
 
 def setup_module(module):
-    install.uninstall(PACKAGE_NAME)
-    utils.gc_frameworks()
+    shakedown.uninstall_package_and_data(PACKAGE_NAME, PACKAGE_NAME)
+    install.gc_frameworks()
     install.install(PACKAGE_NAME, DEFAULT_TASK_COUNT)
 
 
 def teardown_module(module):
-    install.uninstall(PACKAGE_NAME)
+    shakedown.uninstall_package_and_data(PACKAGE_NAME, PACKAGE_NAME)
 
 
 @pytest.mark.data_integrity
