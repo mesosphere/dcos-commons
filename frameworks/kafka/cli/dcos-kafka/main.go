2bb08636e2d29b8d51ab314d9f1023896e48a9e1
Nicholas Parker <nick@nickbp.com>
2017-03-08 16:31:16 -0800

    Reduce per-CLI bootstrapping: remove cumbersome custom description strings (#669)

--- frameworks/kafka/cli/dcos-kafka/main.go
+++ frameworks/kafka/cli/dcos-kafka/main.go
@@ -7,21 +7,11 @@ import (
 	"log"
 	"net/url"
 	"strconv"
-	"strings"
 )
 
 func main() {
-	modName, err := cli.GetModuleName()
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
+	app := cli.New()
 
-	app, err := cli.NewApp("0.1.0", "Mesosphere", "Manage Apache Kafka framework")
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-
-	cli.HandleCommonFlags(app, modName, fmt.Sprintf("%s DC/OS CLI Module", strings.Title(modName)))
 	cli.HandleConfigSection(app)
 	cli.HandleEndpointsSection(app)
 	cli.HandlePlanSection(app)
@@ -31,7 +21,6 @@ func main() {
 	handleBrokerSection(app)
 	handleTopicSection(app)
 
-	// Omit modname:
 	kingpin.MustParse(app.Parse(cli.GetArguments()))
 }
 
