e5e585e003fefa41bde3e1b7d9e75f9c7b7bdd31
Mohit Soni <mohitsoni1989@gmail.com>
2017-03-14 15:54:21 -0700

    Address review comments

--- new-standalone-service.sh
+++ new-standalone-service.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
+set -e
+
+CLEANUP_PATH=`pwd`
+VERSION="0.12.0"
+
 cleanup() {
-    # TODO(mohit): Maybe add a trap for SIGINT.
-    rm -rf template.zip
+    debug "Cleaning up"
+    rm -rf $CLEANUP_PATH/template.zip
 }
 
+trap cleanup INT TERM
+
+error_msg() {
+    echo "---"
+    echo "Failed to generate the project: Exited early at $0:L$1"
+    echo "To try again, re-run this script."
+    echo "---"
+}
+trap 'error_msg ${LINENO}' ERR
+
 info() {
-    MESSAGE=$1
-    echo $MESSAGE
+    echo $1
 }
 
 debug() {
     if [[ -z "${DEBUG// }" ]]; then
         return
     fi
-    MESSAGE=$1
-    echo "DEBUG: $MESSAGE"
+    echo "DEBUG: $1"
 }
 
 PROJECT_NAME=$1
@@ -37,7 +50,7 @@ debug "Scaffolding $PROJECT_NAME from template"
 
 cp -R frameworks/template $PROJECT_PATH/$PROJECT_NAME
 cp -R tools $PROJECT_PATH/$PROJECT_NAME/tools
-
+cp ./.gitignore $PROJECT_PATH/$PROJECT_NAME
 rm -rf $PROJECT_PATH/$PROJECT_NAME/build
 rm -rf $PROJECT_PATH/$PROJECT_NAME/cli/dcos-*/*.whl
 rm -rf $PROJECT_PATH/$PROJECT_NAME/cli/dcos-*/dcos-*
@@ -63,7 +76,6 @@ ${FRAMEWORK_DIR}/tools/build_framework.sh $PUBLISH_STEP $REPO_NAME $FRAMEWORK_DI
 
 # capture anonymous metrics for reporting
 curl https://mesosphere.com/wp-content/themes/mesosphere/library/images/assets/sdk/build-sh-finish.png >/dev/null 2>&1
-
 EOF
 
 chmod +x $PROJECT_PATH/$PROJECT_NAME/build.sh
@@ -81,28 +93,19 @@ UPPER_CASE_PROJECT_NAME=$(echo $PROJECT_NAME | awk '{print toupper($0)}')
 
 find $PROJECT_PATH/$PROJECT_NAME -type f -exec sed -i.bak "s/template/$PROJECT_NAME/g; s/TEMPLATE/$UPPER_CASE_PROJECT_NAME/g; s/template/$PROJECT_NAME/g" {} \;
 sed -i.bak -e '21,$ d' $PROJECT_PATH/$PROJECT_NAME/src/main/dist/svc.yml
-find $PROJECT_PATH/$PROJECT_NAME -type f -name *.bak -exec rm {} \;
+find $PROJECT_PATH/$PROJECT_NAME -type f -name *.bak -exec rm -f {} \;
 
-sed -i.bak "s/compile project(\":scheduler\")/compile \"mesosphere:scheduler:0.12.0\"/g" $PROJECT_PATH/$PROJECT_NAME/build.gradle
-sed -i.bak "s/compile project(\":executor\")/compile \"mesosphere:executor:0.12.0\"/g" $PROJECT_PATH/$PROJECT_NAME/build.gradle
-sed -i.bak "s/testCompile project(\":testing\")/testCompile \"mesosphere:testing:0.12.0\"/g" $PROJECT_PATH/$PROJECT_NAME/build.gradle
+sed -i.bak "s/compile project(\":scheduler\")/compile \"mesosphere:scheduler:$VERSION\"/g" $PROJECT_PATH/$PROJECT_NAME/build.gradle
+sed -i.bak "s/compile project(\":executor\")/compile \"mesosphere:executor:$VERSION\"/g" $PROJECT_PATH/$PROJECT_NAME/build.gradle
+sed -i.bak "s/testCompile project(\":testing\")/testCompile \"mesosphere:testing:$VERSION\"/g" $PROJECT_PATH/$PROJECT_NAME/build.gradle
 sed -i.bak '/distZip.dependsOn ":executor:distZip"/d' $PROJECT_PATH/$PROJECT_NAME/build.gradle
 sed -i.bak '/distZip.finalizedBy copyExecutor/d' $PROJECT_PATH/$PROJECT_NAME/build.gradle
+rm -f $PROJECT_PATH/$PROJECT_NAME/build.gradle.bak
 
-GRADLE_EXISTS="yes"
-command -v gradle >/dev/null 2>&1 || { echo >&2 "I require gradle but it's not installed. Please install gradle and then run 'gradle wrapper --gradle-version 3.4' inside $PROJECT_PATH/$PROJECT_NAME"; GRADLE_EXISTS="no"; }
+GRADLEW=$(pwd)/gradlew
 
-if [ "$GRADLE_EXISTS" == "yes" ]; then
-    pushd $PROJECT_PATH/$PROJECT_NAME
-    gradle wrapper --gradle-version 3.4
-    popd
-fi
+pushd $PROJECT_PATH/$PROJECT_NAME
+$GRADLEW wrapper --gradle-version 3.4.1
+popd
 
-if [ $? -eq 0 ]; then
-    echo "New project created successfully"
-    cleanup
-else
-    echo "Failed to create new project"
-    cleanup
-    exit 1
-fi
+echo "New project created successfully"
