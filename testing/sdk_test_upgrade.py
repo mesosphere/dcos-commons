7037722bae7078a14bb5080a1228bca86406e0c5
Susan X. Huynh <xhuynh@mesosphere.com>
2017-03-14 11:19:28 -0700

    Added more validation in the upgrade test: check that the deployment finishes and that all tasks have restarted.

--- testing/sdk_test_upgrade.py
+++ testing/sdk_test_upgrade.py
@@ -5,7 +5,9 @@ import shakedown
 import sdk_cmd as cmd
 import sdk_install as install
 import sdk_marathon as marathon
+import sdk_plan as plan
 import sdk_spin as spin
+import sdk_tasks as tasks
 
 
 # (1) Installs Universe version of framework.
@@ -44,24 +46,32 @@ def upgrade_downgrade(package_name, running_task_count):
     add_last_repo('Universe', universe_url, universe_version, package_name)
 
     print('Upgrading to test version')
-    marathon.destroy_app(package_name)
-    install.install(package_name, running_task_count)
+    upgrade_or_downgrade(package_name, running_task_count)
 
     # Move the Universe repo to the top of the repo list
     shakedown.remove_package_repo('Universe')
     add_repo('Universe', universe_url, test_version, 0, package_name)
 
     print('Downgrading to master version')
-    marathon.destroy_app(package_name)
-    install.install(package_name, running_task_count)
+    upgrade_or_downgrade(package_name, running_task_count)
 
     # Move the Universe repo to the bottom of the repo list
     shakedown.remove_package_repo('Universe')
     add_last_repo('Universe', universe_url, universe_version, package_name)
 
     print('Upgrading to test version')
+    upgrade_or_downgrade(package_name, running_task_count)
+
+
+def upgrade_or_downgrade(package_name, running_task_count):
+    task_ids = tasks.get_task_ids(package_name, '')
     marathon.destroy_app(package_name)
     install.install(package_name, running_task_count)
+    print('Waiting for upgrade / downgrade deployment to complete')
+    spin.time_wait_noisy(lambda: (
+        plan.get_deployment_plan(package_name).json()['status'] == 'COMPLETE'))
+    print('Checking that all tasks have restarted')
+    tasks.check_tasks_updated(package_name, '', task_ids)
 
 
 def get_test_repo_info():
