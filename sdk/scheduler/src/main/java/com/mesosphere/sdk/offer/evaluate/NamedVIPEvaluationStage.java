ec27c7cf9aacd8cebb4cab9e7b62b0768850eb6d
Gabriel Hartmann <gabriel@mesosphere.io>
2017-03-16 17:09:12 -0700

    [SDK] Fix repetition of environment variables.

--- sdk/scheduler/src/main/java/com/mesosphere/sdk/offer/evaluate/NamedVIPEvaluationStage.java
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/offer/evaluate/NamedVIPEvaluationStage.java
@@ -50,7 +50,7 @@ public class NamedVIPEvaluationStage extends PortEvaluationStage {
         super.setProtos(podInfoBuilder, resource);
 
         // If this is an existing TaskInfo or ExecutorInfo with the VIP already set, we don't have to do anything.
-        if (getTaskName().isPresent() ) {
+        if (getTaskName().isPresent()) {
             boolean didUpdate = maybeUpdateVIP(podInfoBuilder.getTaskBuilder(getTaskName().get()));
 
             if (!didUpdate) {
