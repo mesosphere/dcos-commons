8191dbeb22c3b7ace0639e9bcad89a08dc1d2e86
benclarkwood <benwood@Mesospheres-MacBook-Pro.local>
2017-03-17 22:38:10 -0700

    Fix style

--- sdk/scheduler/src/main/java/com/mesosphere/sdk/scheduler/plan/Status.java
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/scheduler/plan/Status.java
@@ -13,8 +13,11 @@ package com.mesosphere.sdk.scheduler.plan;
  * generally not a legal operation so attempting to create a {@link Step} {@link Element} to accomplish this work would
  * result in a {@link Step} with an ERROR state.
  *
- * The WAITING is a special state only used to show propagating effect of interrupted elements.  It indicates that either current {@link Element} is interrupted or at least one of its children is interrepted. It is only returned by PlanUtils.getStatus(). Status will never set to WAITING and should never be comparating whether it is WAITING or not. 
-Internally, interrupt is a separate state, and isInterrupted() call is used to check whether an {@link Element} is interrupted or not. 
+ * The WAITING is a special state only used to show propagating effect of interrupted elements.  It indicates that
+ * either current {@link Element} is interrupted or at least one of its children is interrepted. It is only returned by
+ * PlanUtils.getStatus(). Status will never set to WAITING and should never be comparating whether it is WAITING or not.
+ * Internally, interrupt is a separate state, and isInterrupted() call is used to check whether an {@link Element} is
+ * interrupted or not.
  */
 public enum Status {
 
