cfb9849f58496055ab449d6851a1eea65e147ed4
Michael Gummelt <mgummelt@mesosphere.io>
2017-03-22 14:44:49 -0700

    [INFINITY-1188] fix ParentElement.getStatus

---
+++ sdk/scheduler/src/test/java/com/mesosphere/sdk/scheduler/plan/DefaultPhaseTest.java
@@ -0,0 +1,34 @@
+package com.mesosphere.sdk.scheduler.plan;
+
+import com.mesosphere.sdk.scheduler.plan.strategy.SerialStrategy;
+import com.mesosphere.sdk.scheduler.plan.strategy.StrategyGenerator;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.mockito.Mockito.when;
+
+public class DefaultPhaseTest {
+
+    @Test
+    public void testGetStatus() {
+        Step step1 = Mockito.mock(DeploymentStep.class);
+        Step step2 = Mockito.mock(DeploymentStep.class);
+
+        final DefaultPhase phase = new DefaultPhase(
+                "test-phase",
+                Arrays.asList(step1, step2),
+                new SerialStrategy<>(),
+                Collections.emptyList());
+
+        when(step1.getStatus()).thenReturn(Status.PENDING);
+        when(step2.getStatus()).thenReturn(Status.WAITING);
+
+        Assert.assertEquals(Status.PENDING, phase.getStatus());
+    }
+}
