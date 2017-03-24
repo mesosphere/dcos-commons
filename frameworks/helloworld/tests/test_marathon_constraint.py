d4df33f090ea1f6c54544dd0218c31add57b00e4
Loren Siebert <loren@siebert.org>
2017-03-24 10:39:38 -0700

    Added hello-world test coverage
    - Marathon constraint + pod replacement scenario described in https://jira.mesosphere.com/browse/INFINITY-1028
    - PEP-8 formatting on config.py

---
+++ frameworks/helloworld/tests/test_marathon_constraint.py
@@ -0,0 +1,58 @@
+import pytest
+import shakedown
+
+import sdk_cmd as cmd
+import sdk_install as install
+import sdk_plan as plan
+import sdk_tasks as tasks
+from tests.config import (
+    PACKAGE_NAME
+)
+
+num_private_agents = len(shakedown.get_private_agents())
+
+
+def setup_module(module):
+    install.uninstall(PACKAGE_NAME)
+    options = {
+        "service": {
+            "spec_file": "examples/marathon_constraint.yml"
+        },
+        "hello": {
+            "count": 1,
+            "placement": "hostname:UNIQUE"
+        },
+        "world": {
+            "count": num_private_agents
+        }
+    }
+
+    install.install(PACKAGE_NAME, num_total_tasks(), additional_options=options)
+
+
+def teardown_module(module):
+    install.uninstall(PACKAGE_NAME)
+
+
+@pytest.mark.sanity
+@pytest.mark.recovery
+def test_deploy():
+    # hello deploys first so Marathon placement constraint is satisfied. One "world" task will end up placed with it.
+    plan.get_deployment_plan(PACKAGE_NAME)
+
+    cmd.run_cli('hello-world pods replace hello-0')
+    # allow up to 10 seconds for hello-0 to show up as TASK_KILLED
+    tasks.check_running(PACKAGE_NAME, num_private_agents, timeout_seconds=10)
+
+    # Now hello node should not be placed anywhere, as there is a "world" task on each private node already
+    try:
+        # if hello-0 is going to get staged, it'll happen pretty quickly
+        shakedown.wait_for_task_property_value(PACKAGE_NAME, 'hello-0-server', 'state', 'TASK_STAGING', timeout_sec=10)
+        assert False, "hello-0 should not have been staged"
+    except shakedown.TimeoutExpired:
+        print("Passed: hello-0 did not get staged")
+        pass
+
+
+def num_total_tasks():
+    return num_private_agents + 1
