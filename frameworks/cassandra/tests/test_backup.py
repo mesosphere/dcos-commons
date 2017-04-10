6f8f4c22cbf2ce04ec843223d0597da49fc2f9dd
Michael Browning <mbrowning@mesosphere.com>
2017-04-10 16:48:18 -0700

    Add test for full backup and restore flow

---
+++ frameworks/cassandra/tests/test_backup.py
@@ -0,0 +1,113 @@
+import os
+import sys
+import uuid
+
+import pytest
+
+from tests.config import *
+import sdk_cmd as cmd
+import sdk_install as install
+import sdk_plan as plan
+import sdk_spin as spin
+import sdk_tasks as tasks
+import sdk_utils as utils
+
+
+WRITE_DATA_JOB = 'write-data'
+VERIFY_DATA_JOB = 'verify-data'
+DELETE_DATA_JOB = 'delete-data'
+VERIFY_DELETION_JOB = 'verify-deletion'
+TEST_JOBS = (
+    WRITE_DATA_JOB, VERIFY_DATA_JOB, DELETE_DATA_JOB, VERIFY_DELETION_JOB
+)
+
+
+def setup_module(module):
+    install.uninstall(PACKAGE_NAME)
+    utils.gc_frameworks()
+
+    # check_suppression=False due to https://jira.mesosphere.com/browse/CASSANDRA-568
+    install.install(PACKAGE_NAME, DEFAULT_TASK_COUNT, check_suppression=False)
+
+    jobs_folder = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), 'jobs'
+    )
+    for job in TEST_JOBS:
+        cmd.run_cli('job add {}'.format(
+            os.path.join(jobs_folder, '{}.json'.format(job))
+        ))
+#        spin.time_wait_noisy(lambda: (
+#            cmd.run_cli('job add {}'.format(
+#                os.path.join(jobs_folder, '{}.json'.format(job))
+#            ))
+#        ))
+
+
+def setup_function(function):
+    tasks.check_running(PACKAGE_NAME, DEFAULT_TASK_COUNT)
+
+
+def teardown_module(module):
+    install.uninstall(PACKAGE_NAME)
+
+    for job in TEST_JOBS:
+        cmd.run_cli('job remove {}'.format(qualified_job_name(job)))
+
+
+def qualified_job_name(job_name):
+    return 'test.cassandra.{}'.format(job_name)
+
+
+def launch_and_verify_job(job_name, expected_successes=1):
+    cmd.run_cli('job run {}'.format(qualified_job_name(job_name)))
+
+    spin.time_wait_noisy(lambda: (
+        'Successful runs: {}'.format(expected_successes) in
+        cmd.run_cli('job history {}'.format(qualified_job_name(job_name)))
+    ))
+
+
+def test_backup_and_restore_flow():
+    backup_parameters = {
+        'S3_BUCKET_NAME': os.getenv(
+            'AWS_BUCKET_NAME', 'infinity-framework-test'
+        ),
+        'AWS_ACCESS_KEY_ID': os.getenv('AWS_ACCESS_KEY_ID'),
+        'AWS_SECRET_ACCESS_KEY': os.getenv('AWS_SECRET_ACCESS_KEY'),
+        'AWS_REGION': os.getenv('AWS_REGION', 'us-west-2'),
+        'SNAPSHOT_NAME': str(uuid.uuid1()),
+        'CASSANDRA_KEYSPACES': '"testspace1 testspace2"',
+    }
+
+    # Write data to cassandra on service health, using a job
+    launch_and_verify_job(WRITE_DATA_JOB)
+
+    # Verify data written
+    launch_and_verify_job(VERIFY_DATA_JOB)
+
+    # Run backup plan using CI AWS account
+    plan.start_plan(PACKAGE_NAME, 'backup-aws', parameters=backup_parameters)
+    spin.time_wait_noisy(
+        lambda: (
+            plan.get_plan(PACKAGE_NAME, 'backup-aws').json()['status'] ==
+            'COMPLETE'
+        )
+    )
+
+    # Truncate tables, delete keyspace, using a job
+    launch_and_verify_job(DELETE_DATA_JOB)
+
+    # Verify data destroyed
+    launch_and_verify_job(VERIFY_DELETION_JOB)
+
+    # Run restore job
+    plan.start_plan(PACKAGE_NAME, 'restore-aws', parameters=backup_parameters)
+    spin.time_wait_noisy(
+        lambda: (
+            plan.get_plan(PACKAGE_NAME, 'restore-aws').json()['status'] ==
+            'COMPLETE'
+        )
+    )
+
+    # Run same data written verification to verify restore successful
+    launch_and_verify_job(VERIFY_DATA_JOB, expected_successes=2)
