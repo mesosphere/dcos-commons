c8a01d3cab64cf4b9a9344ad97739476721f55fc
Michael Browning <mrbrowning@distopia.net>
2017-04-13 14:43:45 -0700

    Only look for docker image if release desired (#858)

--- tools/release_builder.py
+++ tools/release_builder.py
@@ -422,8 +422,10 @@ class UniverseReleaseBuilder(object):
 
         original_artifact_urls = self._update_package_get_artifact_source_urls(pkgdir)
         self._copy_artifacts_s3(scratchdir, original_artifact_urls)
-        orig_docker_image = self._original_docker_image(pkgdir)
-        if orig_docker_image and self._release_docker_image:
+        if self._release_docker_image:
+            orig_docker_image = self._original_docker_image(pkgdir)
+            if not orig_docker_image:
+                raise Exception('Release to docker specified, but no docker image found in resource.json')
             self._copy_docker_image(pkgdir, orig_docker_image)
         (branch, commitmsg_path) = self._create_universe_branch(scratchdir, pkgdir)
         return self._create_universe_pr(branch, commitmsg_path)
