3a9665c7963db3edf227530e4263cb33bdab8aca
Danny Purcell <dpurcelljr@gmail.com>
2017-04-13 10:46:59 -0500

    Allows optional values to be cleared (#775)

--- sdk/scheduler/src/main/java/com/mesosphere/sdk/scheduler/DefaultScheduler.java
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/scheduler/DefaultScheduler.java
@@ -160,7 +160,7 @@ public class DefaultScheduler implements Scheduler, Observer {
                 // Any customization of the state store must be applied BEFORE getStateStore() is ever called.
                 throw new IllegalStateException("State store is already set. Was getStateStore() invoked before this?");
             }
-            this.stateStoreOptional = Optional.of(stateStore);
+            this.stateStoreOptional = Optional.ofNullable(stateStore);
             return this;
         }
 
@@ -194,7 +194,7 @@ public class DefaultScheduler implements Scheduler, Observer {
          * while also storing historical configurations.
          */
         public Builder setConfigStore(ConfigStore<ServiceSpec> configStore) {
-            this.configStoreOptional = Optional.of(configStore);
+            this.configStoreOptional = Optional.ofNullable(configStore);
             return this;
         }
 
@@ -206,7 +206,7 @@ public class DefaultScheduler implements Scheduler, Observer {
          * invoked.
          */
         public Builder setConfigValidators(Collection<ConfigValidator<ServiceSpec>> configValidators) {
-            this.configValidatorsOptional = Optional.of(configValidators);
+            this.configValidatorsOptional = Optional.ofNullable(configValidators);
             return this;
         }
 
@@ -216,7 +216,7 @@ public class DefaultScheduler implements Scheduler, Observer {
          * and/or replaced.
          */
         public Builder setRestartHook(RestartHook restartHook) {
-            this.restartHookOptional = Optional.of(restartHook);
+            this.restartHookOptional = Optional.ofNullable(restartHook);
             return this;
         }
 
