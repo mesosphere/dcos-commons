66bb2f7de52c49ba55284d1e3ae69e391be8a43d
Michael Browning <mbrowning@mesosphere.com>
2017-04-14 17:03:40 -0700

    Undo automatic append of -beta to package version for beta releases

--- tools/release_builder.py
+++ tools/release_builder.py
@@ -400,7 +400,6 @@ class UniverseReleaseBuilder(object):
             package_json = json.load(f)
 
         package_json['name'] = 'beta-' + package_json['name']
-        package_json['version'] = package_json['version'] + '-beta'
 
         with open(package_file_name, 'w') as f:
             json.dump(package_json, f, indent=4)
