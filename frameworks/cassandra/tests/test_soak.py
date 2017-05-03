a448b56fc7aafbf68c913e9a904a2916a3eae736
Michael Browning <mbrowning@mesosphere.com>
2017-05-03 12:11:24 -0700

    Add soak test for upgrade downgrade

--- frameworks/cassandra/tests/test_soak.py
+++ frameworks/cassandra/tests/test_soak.py
@@ -1,7 +1,17 @@
+import json
+
 import pytest
 
-from tests.config import *
+from tests.config import (
+    DEFAULT_TASK_COUNT,
+    DELETE_DATA_JOB,
+    PACKAGE_NAME,
+    install_cassandra_jobs,
+    launch_and_verify_job,
+    remove_cassandra_jobs,
+)
 from tests.test_backup import run_backup_and_restore
+import sdk_test_upgrade
 
 
 def setup_module(module):
@@ -16,6 +26,19 @@ def teardown_module(module):
 def test_backup_and_restore():
     run_backup_and_restore()
 
-    # Since this is run on the soak cluster and state is retained, we have to also delete the test
-    # data in preparation for the next run.
+    # Since this is run on the soak cluster and state is retained, we have to
+    # also delete the test data in preparation for the next run.
     launch_and_verify_job(DELETE_DATA_JOB, expected_successes=2)
+
+
+@pytest.mark.soak_upgrade
+def test_soak_upgrade_downgrade():
+    """Install the Cassandra Universe package and attempt upgrade to master.
+    
+    Assumes that the install options file is placed in the repo root."""
+    with open('cassandra.json') as options_file:
+        install_options = json.load(options_file)
+
+    sdk_test_upgrade.soak_upgrade_downgrade(
+        PACKAGE_NAME, DEFAULT_TASK_COUNT, install_options
+    )
