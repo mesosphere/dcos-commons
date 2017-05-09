8133661624306ae12ee4a725e8d6971f6ea74178
Sunil Shah <sunil@mesosphere.io>
2017-05-09 11:08:13 -0700

    Remove whitespace.

--- test.py
+++ test.py
@@ -462,7 +462,7 @@ def run_tests(run_attrs, repo_root):
     try: # all clusters are set up inside this try
         all_passed = False
         if run_attrs.parallel:
-            logger.debug("Running m ulticluster test run")
+            logger.debug("Running multicluster test run")
             all_passed = _multicluster_linear_per_cluster(run_attrs, repo_root)
         else:
             all_passed = _one_cluster_linear_tests(run_attrs, repo_root)
