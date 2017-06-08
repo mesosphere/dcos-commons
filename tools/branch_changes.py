da29a580469a1dee7a724318ea03cc983c331021
Joshua Rodman <jrodman@mesosphere.com>
2017-06-07 19:48:06 -0700

    Script fixes.

--- tools/branch_changes.py
+++ tools/branch_changes.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 
 import subprocess
+import sys
 
 def fetch_origin_master():
-    subprocess.check_call([b'git', b'fetch', b'origin', b'master'])
+    subprocess.check_call([b'git', b'fetch', b'origin', b'master', b'--quiet'])
 
 def get_changed_files():
     fetch_origin_master()
 
-    output = subprocess.check_output([b'git', b'diff', b'...origin/master',
+    output = subprocess.check_output([b'git', b'diff', b'origin/master',
                                       b'--name-only'])
     files = output.splitlines()
     return files
 
 def categorize_file(filename):
-    if filename.endswith(".md"):
+    if filename.endswith(b".md"):
         return "DOC"
-    if filename.startswith("doc/"):
+    if filename.startswith(b"doc/"):
         return "DOC"
-    if filename.startswith("frameworks/"):
-        literal, specific_framework, rest = filename.split('/', 2)
-        return specific_framework
+    if filename.startswith(b"frameworks/"):
+        literal, specific_framework, rest = filename.split(b'/', 2)
+        return specific_framework.decode('utf-8')
     return "SDK"
 
 def categorize_branch_changes():
@@ -31,10 +32,10 @@ def categorize_branch_changes():
 
     # any SDK changes means build/test everything
     if "SDK" in file_types:
-        return "SDK"
+        return {"SDK"}
     # only DOC changes means build/test nothing
     if file_types == {"DOC"}:
-        return "DOC"
+        return {"DOC"}
     # Otherwise return the frameworks changed
     file_types.discard("DOC")
     return file_types
@@ -49,3 +50,7 @@ def memoized_branch_changes():
 get_branch_changetypes=memoized_branch_changes
 
 __all__ = ('get_branch_changetypes',)
+
+if __name__ == "__main__":
+    for item in get_branch_changetypes():
+        sys.stdout.write(item + "\n")
