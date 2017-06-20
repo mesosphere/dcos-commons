ccb04e928748353c471dcdb27c8d4c8aac7b2007
nick <nick@mesosphere.io>
2017-06-20 15:50:56 -0700

    Reenable documented support for custom ZK location for kafka
    Updates tests to exercise the support

--- frameworks/kafka/src/main/java/com/mesosphere/sdk/kafka/api/KafkaZKClient.java
+++ frameworks/kafka/src/main/java/com/mesosphere/sdk/kafka/api/KafkaZKClient.java
@@ -10,9 +10,9 @@ import org.apache.zookeeper.KeeperException;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.security.InvalidParameterException;
 import java.util.Optional;
 import java.util.List;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 /**
@@ -23,79 +23,61 @@ public class KafkaZKClient {
 
     private static final int POLL_DELAY_MS = 1000;
     private static final int CURATOR_MAX_RETRIES = 3;
-    private static final String BROKER_ID_PATH = "/brokers/ids";
+    private static final String IDS_PATH = "/brokers/ids";
     private static final String TOPICS_PATH = "/brokers/topics";
 
     private final CuratorFramework zkClient;
-    private final String kafkaZkUri;
-    private final String kafkaServicePath;
 
-    public String getZKString() {
-        return  kafkaZkUri + kafkaServicePath;
-    }
-
-    public KafkaZKClient(String kafkaZkUri, String servicePath) {
-        if (kafkaZkUri == null) {
-            throw new InvalidParameterException("Zookeeper URI is not set. Can not start client.");
-        }
-        if (servicePath == null) {
-            throw new InvalidParameterException("Service Path is not set. Can not start client.");
-        }
-        this.zkClient = CuratorFrameworkFactory.newClient(
-                kafkaZkUri,
-                new ExponentialBackoffRetry(POLL_DELAY_MS, CURATOR_MAX_RETRIES));
+    /**
+     * @param kafkaConnectString the ZK URI being used by kafka, of the form "host:port/path/to/node",
+     *      e.g. "mesos.master:2181/dcos-service-path__to__kafka"
+     */
+    public KafkaZKClient(String kafkaConnectString) {
+        log.info("Zookeeper Client URI: {}", kafkaConnectString);
+        this.zkClient = CuratorFrameworkFactory.builder()
+                .connectString(kafkaConnectString)
+                .retryPolicy(new ExponentialBackoffRetry(POLL_DELAY_MS, CURATOR_MAX_RETRIES))
+                .build();
         this.zkClient.start();
-        this.kafkaZkUri = kafkaZkUri;
-        this.kafkaServicePath = servicePath;
-        log.info("Zookeeper Client URI: {} servicePath: {}", kafkaZkUri, kafkaServicePath);
     }
 
     public JSONArray listBrokers() throws Exception {
         try {
-            log.info("Zookeeper Client list brokers path: {}{}", kafkaServicePath,  BROKER_ID_PATH);
-            return new JSONArray(zkClient.getChildren().forPath(kafkaServicePath + BROKER_ID_PATH));
+            return new JSONArray(zkClient.getChildren().forPath(IDS_PATH));
         } catch (KeeperException.NoNodeException e) {
-            log.info("List path: " + kafkaServicePath + BROKER_ID_PATH
+            log.info("List path: " + IDS_PATH
                     + " doesn't exist, returning empty brokers list. Kafka not running yet?", e);
             return new JSONArray();
         }
     }
 
     public Optional<JSONObject> getBroker(String id) throws Exception {
-        log.info("Zookeeper Client get broker path: {}{}/{} ", kafkaServicePath, BROKER_ID_PATH, id);
-        List<String> ids = zkClient.getChildren().forPath(kafkaServicePath + BROKER_ID_PATH);
+        List<String> ids = zkClient.getChildren().forPath(IDS_PATH);
         if (!ids.contains(id)) {
-            log.warn("Zookeeper Client broker id {} does not exists", id);
             return Optional.empty();
         }
-        return Optional.of(
-                new JSONObject(
-                        new String(zkClient.getData().
-                               forPath(kafkaServicePath + BROKER_ID_PATH
-                                       + "/" + id), "UTF-8")));
+        return Optional.of(new JSONObject(new String(
+                zkClient.getData().forPath(IDS_PATH + "/" + id), StandardCharsets.UTF_8)));
     }
 
     public JSONArray listTopics() throws Exception {
         try {
-            log.info("Zookeeper Client list topics path: " + kafkaServicePath + BROKER_ID_PATH);
-            return new JSONArray(zkClient.getChildren().forPath(kafkaServicePath + TOPICS_PATH));
+            return new JSONArray(zkClient.getChildren().forPath(TOPICS_PATH));
         } catch (KeeperException.NoNodeException e) {
-            log.info("List path: " + kafkaServicePath + TOPICS_PATH
+            log.info("List path: " + TOPICS_PATH
                     + " doesn't exist, returning empty topics list. Kafka not running yet?", e);
             return new JSONArray();
         }
     }
 
     public JSONObject getTopic(String topicName) throws Exception {
-        List<String> partitionIdList = zkClient.getChildren()
-                .forPath(kafkaServicePath + TOPICS_PATH + "/" + topicName + "/partitions");
+        String partitionsNode = TOPICS_PATH + "/" + topicName + "/partitions";
+        List<String> partitionIdList = zkClient.getChildren().forPath(partitionsNode);
         List<JSONObject> partitions = new ArrayList<JSONObject>();
         for (String partitionId : partitionIdList) {
-            JSONObject state = new JSONObject(
-                                    new String(zkClient.getData().forPath (
-                                            kafkaServicePath + TOPICS_PATH + "/"
-                                                    + topicName + "/partitions" + "/" + partitionId + "/state"),
-                                            "UTF-8"));
+            JSONObject state = new JSONObject(new String(
+                    zkClient.getData().forPath(partitionsNode + "/" + partitionId + "/state"),
+                    StandardCharsets.UTF_8));
             partitions.add((new JSONObject()).put(partitionId, state));
         }
         return (new JSONObject()).put("partitions", partitions);
@@ -104,13 +86,11 @@ public class KafkaZKClient {
     public List<String> getBrokerEndpoints() {
         List<String> endpoints = new ArrayList<>();
         try {
-            List<String> ids = zkClient.getChildren().forPath(kafkaServicePath + BROKER_ID_PATH);
+            List<String> ids = zkClient.getChildren().forPath(IDS_PATH);
             for (String id : ids) {
-                byte[] bytes = zkClient.getData().forPath(kafkaServicePath + BROKER_ID_PATH + "/" + id);
-                JSONObject broker = new JSONObject(new String(bytes, "UTF-8"));
-                String host = (String) broker.get("host");
-                Integer port = (Integer) broker.get("port");
-                endpoints.add(host + ":" + port);
+                byte[] bytes = zkClient.getData().forPath(IDS_PATH + "/" + id);
+                JSONObject broker = new JSONObject(new String(bytes, StandardCharsets.UTF_8));
+                endpoints.add(broker.getString("host") + ":" + broker.getInt("port"));
             }
         } catch (Exception ex) {
             log.error("Failed to retrieve broker endpoints with exception: ", ex);
