b6b3a6be58c821f16924d3ea134ce12380c8e02b
Sam Pringle <15springle@gmail.com>
2017-06-20 09:55:29 -0700

    shortened to 'dcos cockroachdb sql'

--- frameworks/cockroachdb/cli/dcos-cockroachdb/main.go
+++ frameworks/cockroachdb/cli/dcos-cockroachdb/main.go
@@ -19,11 +19,7 @@ func main() {
 	kingpin.MustParse(app.Parse(cli.GetArguments()))
 }
 
-type CockroachHandler struct {
-	command string
-}
-
-func (cmd *CockroachHandler) sql(c *kingpin.ParseContext) error {
+func sql(c *kingpin.ParseContext) error {
 	runCommand("task",
 		"exec",
 		"-it",
@@ -42,14 +38,10 @@ func runCommand(arg ...string) {
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
 	if err != nil {
-		fmt.Printf("Error: %s\n", err)
+		fmt.Printf("[err] %s\n", err)
 	}
 }
 
 func handleCockroachSection(app *kingpin.Application) {
-	cmd := &CockroachHandler{}
-	cockroach := app.Command("cockroach", "CockroachDB CLI")
-
-	sql := cockroach.Command("sql", "Equivalent to running `cockroach sql <command>` on task.").Action(cmd.sql)
-	sql.Arg("command", "CockroachDB sql command to run.").StringVar(&cmd.command)
+	app.Command("sql", "Opens interactive Cockroachdb SQL shell").Action(sql)
 }
