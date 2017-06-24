c7d7bf56e5c1e377785a6a9fe8b4540cf87ae428
Sunil Shah <sunil@mesosphere.io>
2017-06-23 18:15:03 -0700

    [INFINITY-1609] [INFINITY-1640] Additional commands for update (#1084)
    * Add package-versions command.
    
    * Move JSON parsing function into client library and add initial pass at --package-versions command.
    
    * Remove unnecessary function call.
    
    * Add array to Cosmos unit test.
    
    * Add unit test for package-versions command.
    
    * Rename plan commands and add aliases.
    
    * Tidy up variable names to make warnings go away and reorder plan subcommands.
    
    * Add additional commands to update from plan, change wording and move tests. Change plan_test.go to use a TestSuite (which will allow us to test the HTTP calls).
    
    * Add tests.
    
    * Remove log output in favour of print.
    
    * Add catch for 502.
    
    * Fix problems outlined by golint.
    
    * Return a 208 if the user has already interrupted or continued a plan or phase.
    
    * Add additional check for isComplete.
    
    * Improve error message.
    
    * Fix Response bug for 404 and 208 errors.
    
    * This change ended up being larger than I was intending. Added error catching and tests for plan methods and refactored http code to pass around byte arrays instead of the Response object.
    
    * Tidy up output and tests.
    
    * Better package version output in the case of no package versions.
    
    * Address review comments.
    
    * Fix Kafka CLI build.
    
    * Update output for package versions command.
    
    * Return 208 if step to be forceCompleted has already completed.
    
    * Defaults plan name to update if running the plan commands via the update command.
    
    * Fix test output, add missing test data and tidy up plan help text.
    
    * Update .gitignore.

--- cli/config/config.go
+++ cli/config/config.go
@@ -1,16 +1,27 @@
 package config
 
 var (
+	// DcosAuthToken used to authenticate against DC/OS. Read from the DC/OS CLI, if set.
 	DcosAuthToken string
-	DcosUrl       string
-	CosmosUrl     string
-	ModuleName    string
-	ServiceName   string
-	Command       string
+	// DcosURL is the DC/OS master's URL. Read from the DC/OS CLI, if set.
+	DcosURL string
+	// CosmosURL can point to a local Cosmos instance for testing. Read from the DC/OS CLI, if set.
+	CosmosURL string
+	// ModuleName represents the name of this CLI module (e.g. "hello-world") and is read in from the $1 argument.
+	ModuleName string
+	// ServiceName represents the name of this instantiation of the service. If unspecified by the user, this defaults to
+	// the same as ModuleName.
+	ServiceName string
+	// Command represents the name of the specific subcommand and is used to provide more helpful error messages.
+	Command string
 
-	TlsForceInsecure bool
-	TlsCliSetting    tlsSetting = TlsUnknown
-	TlsCACertPath    string
+	// TLSForceInsecure forces insecure connections if set.
+	TLSForceInsecure bool
+	// TLSCliSetting represents whether or not certificates should be verified or not.
+	TLSCliSetting = TLSUnknown
+	// TLSCACertPath represents the path to a certificate to use when speaking to a DC/OS cluster.
+	TLSCACertPath string
 
+	// Verbose will print additional messages to aid with debugging if set to true.
 	Verbose bool
 )
