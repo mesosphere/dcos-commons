6311326abf73881e381f38a7e259560b37c19df2
Robin Oh <OhRobin@users.noreply.github.com>
2017-06-26 15:42:22 -0700

    formatting changes to test_multi_instance

--- frameworks/cockroachdb/tests/test_mult_instances.py
+++ frameworks/cockroachdb/tests/test_mult_instances.py
@@ -11,9 +11,8 @@ from tests.config import (
     PACKAGE_NAME,
     DEFAULT_TASK_COUNT
 )
-TASK_COUNT = 1
-SERVICE_NAMES = ["firstservice", "secondservice"]
 
+SERVICE_NAMES = ["firstservice", "secondservice"]
 def teardown_module(module):
     uninstall_two_cockroach_services()
     install.uninstall(PACKAGE_NAME)
@@ -35,21 +34,22 @@ def uninstall_two_cockroach_services():
 @pytest.mark.beta
 @pytest.mark.sanity
 def test_two_service_install_with_one_task_each():
+    task_count = 1
     uninstall_two_cockroach_services()
     for SERVICE_NAME in SERVICE_NAMES:
         install.install(
             PACKAGE_NAME,
-            TASK_COUNT,
+            task_count,
             service_name=SERVICE_NAME,
             check_suppression=False,
             additional_options={
                                 "service": { "name": SERVICE_NAME },
-                                "node": { "count": TASK_COUNT }
+                                "node": { "count": task_count }
                                 }, #overrides the marathon configuration
             package_version="1.0.1")
-        tasks.check_running(SERVICE_NAME, TASK_COUNT)
+        tasks.check_running(SERVICE_NAME, task_count)
 
-""" Make sure that you have at least 2 * DEFAULT_TASK_COUNT in your cluster for this test to pass
+""" Make sure that you have at least DEFAULT_TASK_COUNT in your cluster for this test to pass
 """
 @pytest.mark.beta
 @pytest.mark.sanity
@@ -65,4 +65,4 @@ def test_two_service_install_with_DEFAULT_TASK_COUNT_tasks_each():
                                 "service": { "name": SERVICE_NAME } #overrides marathon configuration
                                 },
             package_version="1.0.1")
-        tasks.check_running(SERVICE_NAME, TASK_COUNT)
+        tasks.check_running(SERVICE_NAME, DEFAULT_TASK_COUNT)
