6078f3ccf5449d1df930c232fe29660b259e423c
Robin Oh <OhRobin@users.noreply.github.com>
2017-06-26 16:10:56 -0700

    Adding Tests for recovery
    1. test_pod_restart

--- frameworks/cockroachdb/tests/config.py
+++ frameworks/cockroachdb/tests/config.py
@@ -3,5 +3,3 @@ SERVICE_NAME = 'cockroachdb'
 DEFAULT_TASK_COUNT = 3
 DEFAULT_POD_TYPE = 'cockroachdb'
 DEFAULT_TASK_NAME = 'node'
-def check_running(service_name=PACKAGE_NAME):
-    tasks.check_running(service_name, configured_task_count(service_name))
