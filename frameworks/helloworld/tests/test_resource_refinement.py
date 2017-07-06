0d009d43d886c3e120856ef32df30e7962b8b164
Gabriel Hartmann <gabriel@mesosphere.io>
2017-07-06 13:17:34 -0700

    [INFINITY-1811] Enable resource refinement (#1142)
    * Enable resource refinement
    
    * Remove role from disk resources in ResourceBuilder.
    
    * Tweak fix for role issue
    
    * Correct fix to pass unit tests
    
    * Fix RoundRobin test capabilities

---
+++ frameworks/helloworld/tests/test_resource_refinement.py
@@ -0,0 +1,27 @@
+import pytest
+
+import sdk_install as install
+import sdk_utils as utils
+from tests.config import (
+    check_running,
+    DEFAULT_TASK_COUNT,
+    PACKAGE_NAME
+)
+
+def setup_module(module):
+    install.uninstall(PACKAGE_NAME)
+    install.install(
+        PACKAGE_NAME,
+        DEFAULT_TASK_COUNT,
+        service_name=PACKAGE_NAME,
+        additional_options={"service": { "spec_file": "examples/pre-reserved.yml"} })
+
+
+def teardown_module(module):
+    install.uninstall(PACKAGE_NAME)
+
+
+@pytest.mark.sanity
+@pytest.mark.smoke
+def test_install():
+    check_running(PACKAGE_NAME)
