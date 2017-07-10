3440900237c18160cf05210e0f3a799c33bf7d08
Gabriel Hartmann <gabriel@mesosphere.io>
2017-07-10 13:18:19 -0700

    Fix TaskSpec cloning

---
+++ sdk/scheduler/src/test/java/com/mesosphere/sdk/specification/DefaultTaskSpecTest.java
@@ -0,0 +1,56 @@
+package com.mesosphere.sdk.specification;
+
+import com.mesosphere.sdk.testutils.TestConstants;
+import org.apache.mesos.Protos;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * This class tests DefaultTaskSpec.
+ */
+public class DefaultTaskSpecTest {
+    @Test
+    public void cloneTaskSpec() {
+        DefaultTaskSpec original = new DefaultTaskSpec(
+                "task",
+                GoalState.RUNNING,
+                new DefaultResourceSet(
+                        "rs-id",
+                        Arrays.asList(
+                                new DefaultResourceSpec(
+                                        "cpus",
+                                        Protos.Value.newBuilder()
+                                                .setType(Protos.Value.Type.SCALAR)
+                                                .setScalar(Protos.Value.Scalar.newBuilder()
+                                                        .setValue(1.0))
+                                                .build(),
+                                        TestConstants.ROLE,
+                                        TestConstants.PRE_RESERVED_ROLE,
+                                        TestConstants.PRINCIPAL,
+                                        "env-key")),
+                        Arrays.asList(
+                                new DefaultVolumeSpec(
+                                        100,
+                                        VolumeSpec.Type.ROOT,
+                                        TestConstants.CONTAINER_PATH,
+                                        TestConstants.ROLE,
+                                        TestConstants.PRE_RESERVED_ROLE,
+                                        TestConstants.PRINCIPAL,
+                                        "env-key")),
+                        TestConstants.ROLE,
+                        TestConstants.PRE_RESERVED_ROLE,
+                        TestConstants.PRINCIPAL),
+                new DefaultCommandSpec("./cmd", new HashMap<>()),
+                new DefaultHealthCheckSpec("./health-check", 1, 1, 1, 1, 1),
+                new DefaultReadinessCheckSpec("./readiness-check", 2, 2, 2),
+                Arrays.asList(
+                        new DefaultConfigFileSpec("name", "relative-path", "template-content")),
+                new DefaultDiscoverySpec("prefix", Protos.DiscoveryInfo.Visibility.CLUSTER));
+
+        DefaultTaskSpec clone = DefaultTaskSpec.newBuilder(original).build();
+        Assert.assertEquals(original, clone);
+    }
+}
