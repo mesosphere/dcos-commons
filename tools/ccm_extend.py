890d62617804c7a6a424266c27cd4642e552b077
benclarkwood <ben@benclarkwood.com>
2017-07-13 18:26:21 -0700

    Mistyped var name

--- tools/ccm_extend.py
+++ tools/ccm_extend.py
@@ -14,7 +14,7 @@ cluster_name = cluster_info['deployment_name']
 ccm_token = os.environ['CCM_TOKEN']
 
 # Get all the clusters
-headers = {'Authorization': 'Token {}'.format(CCM_TOKEN)}
+headers = {'Authorization': 'Token {}'.format(ccm_token)}
 
 all_clusters = json.loads(requests.get(base_url+'/api/cluster/active/all', headers=headers).text)
 for cluster in all_clusters:
