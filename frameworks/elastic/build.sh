f39258e514f0f58361aef3d28997824e8c925386
Joshua Rodman <jrodman+github@fastmail.fm>
2017-07-19 17:24:17 -0700

    [INFINITY-1989] Add framework flag to just build the cli. (#1271)
    * Add framework flag to just build the cli.
    
    Enable ./frameworks/cassandra/build.sh --cli-only to get reasonable timeframe
    cycles on CLI compiles.
    Also can do `export CLI_BUILD_SKIP_UPX=y` because upx takes more than a minute
    for me.
    
    * Fix shell var name.

--- frameworks/elastic/build.sh
+++ frameworks/elastic/build.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 set -e
 
+usage() {
+    echo "Syntax: $0 [--cli-only] [aws|local]"
+}
+
+cli_only=
+while :; do
+    case $1 in
+        --help|-h|-\?)
+            usage
+            exit
+            ;;
+        --cli-only)
+            cli_only="true"
+            ;;
+        --)
+            shift
+            break
+            ;;
+        -*)
+            echo "unknown option $1" >&2
+            exit 1
+            ;;
+        *)
+            break
+            ;;
+    esac
+
+    shift
+done
+
 # capture anonymous metrics for reporting
 curl https://mesosphere.com/wp-content/themes/mesosphere/library/images/assets/sdk/build-sh-start.png >/dev/null 2>&1
 
@@ -11,7 +41,12 @@ ROOT_DIR="$(dirname "$(dirname $FRAMEWORK_DIR)")"
 export TOOLS_DIR=${ROOT_DIR}/tools
 BUILD_DIR=$FRAMEWORK_DIR/build/distributions
 PUBLISH_STEP=${1-none}
-${ROOT_DIR}/gradlew distZip -p ${ROOT_DIR}/sdk/executor
+
+if [ x"$cli_only" = xtrue ]; then
+    ${ROOT_DIR}/tools/build_framework.sh --cli-only $PUBLISH_STEP elastic $FRAMEWORK_DIR $BUILD_DIR/executor.zip $BUILD_DIR/elastic-scheduler.zip
+    exit $?
+fi
+
 ${ROOT_DIR}/tools/build_framework.sh $PUBLISH_STEP elastic $FRAMEWORK_DIR $BUILD_DIR/executor.zip $BUILD_DIR/elastic-scheduler.zip
 
 # capture anonymous metrics for reporting
--- frameworks/kafka/build.sh
+++ frameworks/kafka/build.sh
@@ -1,9 +1,46 @@
 #!/bin/bash
 set -e
 
+usage() {
+    echo "Syntax: $0 [--cli-only] [aws|local]"
+}
+
+cli_only=
+while :; do
+    case $1 in
+        --help|-h|-\?)
+            usage
+            exit
+            ;;
+        --cli-only)
+            cli_only="true"
+            ;;
+        --)
+            shift
+            break
+            ;;
+        -*)
+            echo "unknown option $1" >&2
+            exit 1
+            ;;
+        *)
+            break
+            ;;
+    esac
+
+    shift
+done
+
+
 FRAMEWORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="$(dirname "$(dirname $FRAMEWORK_DIR)")"
 export TOOLS_DIR=${ROOT_DIR}/tools
 BUILD_DIR=$FRAMEWORK_DIR/build/distributions
 PUBLISH_STEP=${1-none}
-${ROOT_DIR}/tools/build_framework.sh $PUBLISH_STEP kafka $FRAMEWORK_DIR $BUILD_DIR/executor.zip $BUILD_DIR/kafka-scheduler.zip
+
+cli_flag=
+if [ x"$cli_only" = xtrue ]; then
+    cli_flag=--cli-only
+fi
+
+${ROOT_DIR}/tools/build_framework.sh $cli_flag $PUBLISH_STEP kafka $FRAMEWORK_DIR $BUILD_DIR/executor.zip $BUILD_DIR/kafka-scheduler.zip
