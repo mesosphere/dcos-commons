5f51e66d998bdc3853b42235ae6b8f5b3d5fbb07
nick <nick@mesosphere.io>
2017-07-20 19:56:29 -0700

    Fix elastic endpoints test to reflect that host != endpoint name

--- frameworks/elastic/tests/test_shakedown.py
+++ frameworks/elastic/tests/test_shakedown.py
@@ -52,10 +52,11 @@ def test_service_health():
 @pytest.mark.sanity
 def test_endpoints():
     # check that we can reach the scheduler via admin router, and that returned endpoints are sanitized:
-    for nodetype in ENDPOINT_TYPES:
-        endpoints = json.loads(cmd.run_cli('elastic --name={} endpoints {}'.format(FOLDERED_SERVICE_NAME, nodetype)))
-        assert endpoints['dns'][0].startswith(sdk_hosts.autoip_host(FOLDERED_SERVICE_NAME, nodetype + '-0-node'))
-        assert endpoints['vip'].startswith(sdk_hosts.vip_host(FOLDERED_SERVICE_NAME, nodetype))
+    for endpoint in ENDPOINT_TYPES:
+        endpoints = json.loads(cmd.run_cli('elastic --name={} endpoints {}'.format(FOLDERED_SERVICE_NAME, endpoint)))
+        host = endpoint.split('-')[0] # 'coordinator-http' => 'coordinator'
+        assert endpoints['dns'][0].startswith(sdk_hosts.autoip_host(FOLDERED_SERVICE_NAME, host + '-0-node'))
+        assert endpoints['vip'].startswith(sdk_hosts.vip_host(FOLDERED_SERVICE_NAME, host))
 
 
 @pytest.mark.sanity
