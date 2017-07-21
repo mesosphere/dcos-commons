9c55e75eff5d8012bda75308027beb3706fb3b4f
nick <nick@mesosphere.io>
2017-07-20 18:32:01 -0700

    Missed an endpoint lookup in elastic

--- frameworks/elastic/tests/test_shakedown.py
+++ frameworks/elastic/tests/test_shakedown.py
@@ -6,6 +6,7 @@ import sdk_install
 import sdk_test_upgrade
 import sdk_utils
 import sdk_metrics
+
 from tests.config import *
 
 FOLDERED_SERVICE_NAME = sdk_utils.get_foldered_name(PACKAGE_NAME)
@@ -45,11 +46,7 @@ def test_service_health():
 @pytest.mark.sanity
 def test_endpoints():
     # check that we can reach the scheduler via admin router, and that returned endpoints are sanitized:
-    for nodetype in (
-            'coordinator-http', 'coordinator-transport',
-            'data-http', 'data-transport',
-            'ingest-http', 'ingest-transport',
-            'master-http', 'master-transport'):
+    for nodetype in ENDPOINT_TYPES:
         endpoints = json.loads(cmd.run_cli('elastic --name={} endpoints {}'.format(FOLDERED_SERVICE_NAME, nodetype)))
         assert endpoints['dns'][0].startswith(sdk_hosts.autoip_host(FOLDERED_SERVICE_NAME, nodetype + '-0-node'))
         assert endpoints['vip'].startswith(sdk_hosts.vip_host(FOLDERED_SERVICE_NAME, nodetype))
