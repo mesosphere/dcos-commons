4894fc9dcfd83f1620a53b8c21b7cff1ff6c4f0e
Gabriel Hartmann <gabriel@mesosphere.io>
2017-07-22 15:40:34 -0700

    Fix HDFS data integrity tests

--- frameworks/hdfs/docker-client/configure-hdfs.sh
+++ frameworks/hdfs/docker-client/configure-hdfs.sh
@@ -8,7 +8,7 @@ usage() {
 [[ -n "${HDFS_SERVICE_NAME}" ]] || usage
 
 cd /
-wget -P / "api.hdfs.marathon.l4lb.thisdcos.directory:80/v1/endpoints/core-site.xml"
-wget -P / "api.hdfs.marathon.l4lb.thisdcos.directory:80/v1/endpoints/hdfs-site.xml"
-cp /core-site.xml /hadoop-2.6.0-cdh5.9.1/etc/hadoop
-cp /hdfs-site.xml /hadoop-2.6.0-cdh5.9.1/etc/hadoop
+wget -P / "api.${HDFS_SERVICE_NAME}.marathon.l4lb.thisdcos.directory:80/v1/endpoints/core-site.xml" >>/configure-hdfs.log 2>&1
+wget -P / "api.${HDFS_SERVICE_NAME}.marathon.l4lb.thisdcos.directory:80/v1/endpoints/hdfs-site.xml" >>/configure-hdfs.log 2>&1
+cp /core-site.xml /hadoop-2.6.0-cdh5.9.1/etc/hadoop >>/configure-hdfs.log 2>&1
+cp /hdfs-site.xml /hadoop-2.6.0-cdh5.9.1/etc/hadoop >>/configure-hdfs.log 2>&1
