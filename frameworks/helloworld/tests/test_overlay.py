8c3c7ce12c35d49d4e54fa163d1afe3ff1075026
Michael Browning <mbrowning@mesosphere.com>
2017-07-22 23:46:31 -0700

    Conditionally wait for recovery plan initiation/completion in overlay test

--- frameworks/helloworld/tests/test_overlay.py
+++ frameworks/helloworld/tests/test_overlay.py
@@ -71,6 +71,14 @@ def test_overlay_network():
     assert(len(deployment_plan["phases"][3]["steps"]) == 1)
     assert(len(deployment_plan["phases"][4]["steps"]) == 4)
 
+    # Due to DNS resolution flakiness, some of the deployed tasks can fail. If so,
+    # we wait for them to redeploy, but if they don't fail we still want to proceed.
+    try:
+        sdk_plan.wait_for_in_progress_recovery(PACKAGE_NAME, timeout_seconds=60)
+        sdk_plan.wait_for_completed_recovery(PACKAGE_NAME, timeout_seconds=60)
+    except AssertionError:
+        pass
+
     # test that the tasks are all up, which tests the overlay DNS
     framework_tasks = [task for task in shakedown.get_service_tasks(PACKAGE_NAME, completed=False)]
     framework_task_names = [t["name"] for t in framework_tasks]
