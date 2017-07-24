e4ff407a54f80e059534a0020944da6c645702db
James Gorlick <jgorlick@mesosphere.com>
2017-07-24 13:56:59 -0700

    corrected value for updateRequest.Replace to `false` to employ expectged hash merge semantic.

--- cli/commands/update.go
+++ cli/commands/update.go
@@ -132,7 +132,7 @@ func parseUpdateResponse(responseBytes []byte) (string, error) {
 
 func doUpdate(optionsFile, packageVersion string) {
 	// TODO: figure out KingPin's error handling
-	request := updateRequest{AppID: config.ServiceName, Replace: true}
+	request := updateRequest{AppID: config.ServiceName, Replace: false}
 	if len(packageVersion) == 0 && len(optionsFile) == 0 {
 		client.PrintMessage("Either --options and/or --package-version must be specified. See --help.")
 		return
