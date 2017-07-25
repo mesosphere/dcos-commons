d55cd2aec9a6cf6ac1a24b9b4a62cb3a952d00aa
Nima Vaziri <NimaVaziri@users.noreply.github.com>
2017-07-25 16:39:26 -0700

    Re-enabled overlay tests in strict (#1329)

--- frameworks/kafka/tests/test_overlay.py
+++ frameworks/kafka/tests/test_overlay.py
@@ -11,9 +11,6 @@ import sdk_utils
 
 from tests.test_utils import  *
 
-overlay_nostrict = pytest.mark.skipif(os.environ.get("SECURITY") == "strict",
-    reason="overlay tests currently broken in strict")
-
 @pytest.fixture(scope='module', autouse=True)
 def configure_package(configure_universe):
     try:
@@ -35,7 +32,6 @@ def configure_package(configure_universe):
 @pytest.mark.overlay
 @pytest.mark.smoke
 @pytest.mark.sanity
-@overlay_nostrict
 @sdk_utils.dcos_1_9_or_higher
 def test_service_overlay_health():
     """Installs SDK based Kafka on with virtual networks set to True. Tests that the deployment completes
@@ -54,7 +50,6 @@ def test_service_overlay_health():
 @pytest.mark.smoke
 @pytest.mark.sanity
 @pytest.mark.overlay
-@overlay_nostrict
 @sdk_utils.dcos_1_9_or_higher
 def test_overlay_network_deployment_and_endpoints():
     # double check
@@ -71,7 +66,6 @@ def test_overlay_network_deployment_and_endpoints():
 
 @pytest.mark.sanity
 @pytest.mark.overlay
-@overlay_nostrict
 @sdk_utils.dcos_1_9_or_higher
 def test_pod_restart_on_overlay():
     restart_broker_pods()
@@ -80,7 +74,6 @@ def test_pod_restart_on_overlay():
 
 @pytest.mark.sanity
 @pytest.mark.overlay
-@overlay_nostrict
 @sdk_utils.dcos_1_9_or_higher
 def test_pod_replace_on_overlay():
     replace_broker_pod()
@@ -89,7 +82,6 @@ def test_pod_replace_on_overlay():
 
 @pytest.mark.sanity
 @pytest.mark.overlay
-@overlay_nostrict
 @sdk_utils.dcos_1_9_or_higher
 def test_topic_create_overlay():
     create_topic()
@@ -97,7 +89,6 @@ def test_topic_create_overlay():
 
 @pytest.mark.sanity
 @pytest.mark.overlay
-@overlay_nostrict
 @sdk_utils.dcos_1_9_or_higher
 def test_topic_delete_overlay():
     delete_topic()
