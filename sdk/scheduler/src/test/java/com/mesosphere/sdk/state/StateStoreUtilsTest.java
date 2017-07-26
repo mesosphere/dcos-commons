ab7c17c572a902e9ee78b472aa8f523abd0a5937
Evan Lezar <elezar@mesosphere.com>
2017-07-25 22:25:08 -0700

    Initial unit tests for fetchTasksNeedingRecovery

--- sdk/scheduler/src/test/java/com/mesosphere/sdk/state/StateStoreUtilsTest.java
+++ sdk/scheduler/src/test/java/com/mesosphere/sdk/state/StateStoreUtilsTest.java
@@ -2,17 +2,16 @@ package com.mesosphere.sdk.state;
 
 import com.google.common.collect.ImmutableList;
 import com.mesosphere.sdk.config.ConfigStore;
-import com.mesosphere.sdk.config.ConfigStoreException;
-import com.mesosphere.sdk.config.StringConfiguration;
 import com.mesosphere.sdk.offer.CommonIdUtils;
 import com.mesosphere.sdk.offer.TaskException;
+import com.mesosphere.sdk.offer.taskdata.TaskLabelWriter;
+import com.mesosphere.sdk.scheduler.SchedulerFlags;
 import com.mesosphere.sdk.specification.DefaultServiceSpec;
 import com.mesosphere.sdk.specification.ServiceSpec;
+import com.mesosphere.sdk.specification.yaml.RawServiceSpec;
 import com.mesosphere.sdk.storage.MemPersister;
 import com.mesosphere.sdk.storage.Persister;
-import net.jcip.annotations.ThreadSafe;
-import org.apache.mesos.Protos;
-import org.apache.mesos.Protos.Labels;
+import com.mesosphere.sdk.testutils.OfferRequirementTestUtils;
 import org.apache.mesos.Protos.SlaveID;
 import org.apache.mesos.Protos.TaskID;
 import org.apache.mesos.Protos.TaskInfo;
@@ -21,20 +20,20 @@ import org.apache.mesos.Protos.TaskStatus;
 import org.junit.Test;
 import org.mockito.Mock;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.io.File;
+import java.util.UUID;
 
 import static org.hamcrest.Matchers.empty;
-import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Tests for {@link StateStoreUtils}.
  */
 public class StateStoreUtilsTest {
 
-    @Mock private ServiceSpec mockServiceSpec;
+    @Mock
+    private ServiceSpec mockServiceSpec;
 
     @Test
     public void emptyStateStoreReturnsEmptyArray() {
@@ -175,6 +174,342 @@ public class StateStoreUtilsTest {
         TaskStatus taskStatus = TaskStatus.newBuilder().setTaskId(CommonIdUtils.toTaskId("not-" + taskName)).setState(TaskState.TASK_UNKNOWN).build();
 
         StateStoreUtils.getTaskInfo(stateStore, taskStatus);
+    }
+
+
+    @Test
+    public void emptyStateStoreHasNoTasksNeedingRecovery() throws TaskException {
+        final StateStore stateStore = new DefaultStateStore(new MemPersister());
+        assertThat(StateStoreUtils.fetchTasksNeedingRecovery(stateStore, null), is(empty()));
+    }
+
+
+    @Test
+    public void taskWithNoStatusDoesNotNeedRecovery() throws TaskException {
+        final StateStore stateStore = new DefaultStateStore(new MemPersister());
+
+        final String taskName = "test-task";
+        final TaskID taskID = CommonIdUtils.toTaskId(taskName);
+
+        // Create task info
+        TaskInfo taskInfo = TaskInfo.newBuilder()
+                .setName(taskName)
+                .setTaskId(taskID)
+                .setSlaveId(SlaveID.newBuilder().setValue("ignored")) // proto field required
+                .build();
+
+        // Add a task to the state store
+        stateStore.storeTasks(ImmutableList.of(taskInfo));
+
+        assertThat(StateStoreUtils.fetchTasksNeedingRecovery(stateStore, null), is(empty()));
+
+    }
+
+    @Test
+    public void runningTaskDoesNotNeedRecoveryIfRunning() throws Exception {
+
+        final Persister persister = new MemPersister();
+
+        ConfigStore<ServiceSpec> configStore = newConfigStore(persister);
+
+        final StateStore stateStore = new DefaultStateStore(persister);
+
+        //
+        final String taskName = "name-0-node";
+        final TaskID taskID = CommonIdUtils.toTaskId(taskName);
+
+        // Create task info
+        TaskInfo.Builder taskInfoBuilder = TaskInfo.newBuilder()
+                .setName(taskName)
+                .setTaskId(taskID)
+                .setSlaveId(SlaveID.newBuilder().setValue("ignored")) // proto field required
+                ;
+
+        // create default labels:
+        taskInfoBuilder.setLabels(new TaskLabelWriter(taskInfoBuilder)
+                .setTargetConfiguration(configStore.getTargetConfig())
+                .setType("name")
+                .setIndex(0)
+                .toProto());
+
+        TaskInfo taskInfo = taskInfoBuilder.build();
+
+        // Add a task to the state store
+        stateStore.storeTasks(ImmutableList.of(taskInfo));
+
+        TaskStatus taskStatus = TaskStatus.newBuilder()
+                .setTaskId(taskID)
+                .setState(TaskState.TASK_RUNNING).build();
+        stateStore.storeStatus(taskStatus);
+
+        configStore.getTargetConfig();
+
+        assertThat(StateStoreUtils.fetchTasksNeedingRecovery(stateStore, configStore), is(empty()));
+    }
+
+    @Test
+    public void runningTaskNeedsRecoveryIfFailed() throws Exception {
+        final Persister persister = new MemPersister();
+
+        ConfigStore<ServiceSpec> configStore = newConfigStore(persister);
+
+        final StateStore stateStore = new DefaultStateStore(persister);
+
+        //
+        final String taskName = "name-0-node";
+        final TaskID taskID = CommonIdUtils.toTaskId(taskName);
+
+        // Create task info
+        TaskInfo.Builder taskInfoBuilder = TaskInfo.newBuilder()
+                .setName(taskName)
+                .setTaskId(taskID)
+                .setSlaveId(SlaveID.newBuilder().setValue("ignored")) // proto field required
+                ;
+
+        // create default labels:
+        taskInfoBuilder.setLabels(new TaskLabelWriter(taskInfoBuilder)
+                .setTargetConfiguration(configStore.getTargetConfig())
+                .setType("name")
+                .setIndex(0)
+                .toProto());
+
+        TaskInfo taskInfo = taskInfoBuilder.build();
+
+        // Add a task to the state store
+        stateStore.storeTasks(ImmutableList.of(taskInfo));
+
+        TaskStatus taskStatus = TaskStatus.newBuilder()
+                .setTaskId(taskID)
+                .setState(TaskState.TASK_FAILED).build();
+        stateStore.storeStatus(taskStatus);
+
+        configStore.getTargetConfig();
+
+
+        assertThat(StateStoreUtils.fetchTasksNeedingRecovery(stateStore, configStore),
+                is(ImmutableList.of(taskInfo)));
+    }
+
+
+    @Test(expected = TaskException.class)
+    public void nonPresentTaskRaisesError() throws Exception {
+        final Persister persister = new MemPersister();
+
+        ConfigStore<ServiceSpec> configStore = newConfigStore(persister);
+
+        final StateStore stateStore = new DefaultStateStore(persister);
+
+        //
+        final String taskName = "name-0-not-present";
+        final TaskID taskID = CommonIdUtils.toTaskId(taskName);
+
+        // Create task info
+        TaskInfo.Builder taskInfoBuilder = TaskInfo.newBuilder()
+                .setName(taskName)
+                .setTaskId(taskID)
+                .setSlaveId(SlaveID.newBuilder().setValue("ignored")) // proto field required
+                ;
+
+        // create default labels:
+        taskInfoBuilder.setLabels(new TaskLabelWriter(taskInfoBuilder)
+                .setTargetConfiguration(configStore.getTargetConfig())
+                .setType("name")
+                .setIndex(0)
+                .toProto());
+
+        TaskInfo taskInfo = taskInfoBuilder.build();
+
+        // Add a task to the state store
+        stateStore.storeTasks(ImmutableList.of(taskInfo));
+
+        TaskStatus taskStatus = TaskStatus.newBuilder()
+                .setTaskId(taskID)
+                .setState(TaskState.TASK_RUNNING).build();
+        stateStore.storeStatus(taskStatus);
+
+        StateStoreUtils.fetchTasksNeedingRecovery(stateStore, configStore);
+    }
+
+    @Test
+    public void taskInfoWithNoStatusRequiresNoRecovery() throws Exception {
+        final Persister persister = new MemPersister();
+
+        ConfigStore<ServiceSpec> configStore = newConfigStore(persister);
+
+        final StateStore stateStore = new DefaultStateStore(persister);
+
+        //
+        final String taskName = "name-0-not-present";
+        final TaskID taskID = CommonIdUtils.toTaskId(taskName);
+
+        // Create task info
+        TaskInfo.Builder taskInfoBuilder = TaskInfo.newBuilder()
+                .setName(taskName)
+                .setTaskId(taskID)
+                .setSlaveId(SlaveID.newBuilder().setValue("ignored")) // proto field required
+                ;
+
+        // create default labels:
+        taskInfoBuilder.setLabels(new TaskLabelWriter(taskInfoBuilder)
+                .setTargetConfiguration(configStore.getTargetConfig())
+                .setType("name")
+                .setIndex(0)
+                .toProto());
+
+        TaskInfo taskInfo = taskInfoBuilder.build();
+
+        // Add a task to the state store
+        stateStore.storeTasks(ImmutableList.of(taskInfo));
+
+        assertThat(StateStoreUtils.fetchTasksNeedingRecovery(stateStore, configStore), is(empty()));
+    }
+
+
+    @Test
+    public void finishedTaskDoesNotNeedRecoveryIfFailed() throws Exception {
+        final Persister persister = new MemPersister();
+
+        ConfigStore<ServiceSpec> configStore = newConfigStore(persister);
+
+        final StateStore stateStore = new DefaultStateStore(persister);
+
+        //
+        final String taskName = "name-0-format";
+        final TaskID taskID = CommonIdUtils.toTaskId(taskName);
+
+        // Create task info
+        TaskInfo.Builder taskInfoBuilder = TaskInfo.newBuilder()
+                .setName(taskName)
+                .setTaskId(taskID)
+                .setSlaveId(SlaveID.newBuilder().setValue("ignored")) // proto field required
+                ;
+
+        // create default labels:
+        taskInfoBuilder.setLabels(new TaskLabelWriter(taskInfoBuilder)
+                .setTargetConfiguration(configStore.getTargetConfig())
+                .setType("name")
+                .setIndex(0)
+                .toProto());
+
+        TaskInfo taskInfo = taskInfoBuilder.build();
+
+        // Add a task to the state store
+        stateStore.storeTasks(ImmutableList.of(taskInfo));
+
+        TaskStatus taskStatus = TaskStatus.newBuilder()
+                .setTaskId(taskID)
+                .setState(TaskState.TASK_FAILED).build();
+        stateStore.storeStatus(taskStatus);
+
+        configStore.getTargetConfig();
+
+
+        assertThat(StateStoreUtils.fetchTasksNeedingRecovery(stateStore, configStore),
+                is(empty()));
+    }
+
+    @Test
+    public void finishedTaskDoesNotNeedRecoveryIfFinished() throws Exception {
+        final Persister persister = new MemPersister();
+
+        ConfigStore<ServiceSpec> configStore = newConfigStore(persister);
+
+        final StateStore stateStore = new DefaultStateStore(persister);
+
+        //
+        final String taskName = "name-0-format";
+        final TaskID taskID = CommonIdUtils.toTaskId(taskName);
+
+        // Create task info
+        TaskInfo.Builder taskInfoBuilder = TaskInfo.newBuilder()
+                .setName(taskName)
+                .setTaskId(taskID)
+                .setSlaveId(SlaveID.newBuilder().setValue("ignored")) // proto field required
+                ;
+
+        // create default labels:
+        taskInfoBuilder.setLabels(new TaskLabelWriter(taskInfoBuilder)
+                .setTargetConfiguration(configStore.getTargetConfig())
+                .setType("name")
+                .setIndex(0)
+                .toProto());
+
+        TaskInfo taskInfo = taskInfoBuilder.build();
+
+        // Add a task to the state store
+        stateStore.storeTasks(ImmutableList.of(taskInfo));
+
+        TaskStatus taskStatus = TaskStatus.newBuilder()
+                .setTaskId(taskID)
+                .setState(TaskState.TASK_FINISHED).build();
+        stateStore.storeStatus(taskStatus);
+
+        configStore.getTargetConfig();
+
+
+        assertThat(StateStoreUtils.fetchTasksNeedingRecovery(stateStore, configStore),
+                is(empty()));
+    }
+
+    @Test
+    public void finishedTaskDoesNotNeedRecoveryIfRunning() throws Exception {
+        final Persister persister = new MemPersister();
+
+        ConfigStore<ServiceSpec> configStore = newConfigStore(persister);
+
+        final StateStore stateStore = new DefaultStateStore(persister);
+
+        //
+        final String taskName = "name-0-format";
+        final TaskID taskID = CommonIdUtils.toTaskId(taskName);
+
+        // Create task info
+        TaskInfo.Builder taskInfoBuilder = TaskInfo.newBuilder()
+                .setName(taskName)
+                .setTaskId(taskID)
+                .setSlaveId(SlaveID.newBuilder().setValue("ignored")) // proto field required
+                ;
+
+        // create default labels:
+        taskInfoBuilder.setLabels(new TaskLabelWriter(taskInfoBuilder)
+                .setTargetConfiguration(configStore.getTargetConfig())
+                .setType("name")
+                .setIndex(0)
+                .toProto());
+
+        TaskInfo taskInfo = taskInfoBuilder.build();
+
+        // Add a task to the state store
+        stateStore.storeTasks(ImmutableList.of(taskInfo));
+
+        TaskStatus taskStatus = TaskStatus.newBuilder()
+                .setTaskId(taskID)
+                .setState(TaskState.TASK_RUNNING).build();
+        stateStore.storeStatus(taskStatus);
+
+        configStore.getTargetConfig();
+
+
+        assertThat(StateStoreUtils.fetchTasksNeedingRecovery(stateStore, configStore),
+                is(empty()));
+    }
+
+
+    private ConfigStore<ServiceSpec> newConfigStore(final Persister persister) throws Exception {
+        final SchedulerFlags flags = OfferRequirementTestUtils.getTestSchedulerFlags();
+
+        ClassLoader classLoader = StateStoreUtilsTest.class.getClassLoader();
+        File file = new File(classLoader.getResource("resource-set-seq.yml").getFile());
+        DefaultServiceSpec serviceSpec = DefaultServiceSpec.newGenerator(RawServiceSpec.newBuilder(file).build(), flags)
+                .build();
+
+        DefaultConfigStore<ServiceSpec> configStore = new DefaultConfigStore(DefaultServiceSpec.getConfigurationFactory(serviceSpec), persister);
+        // At startup, the the service spec must be stored, and the target config must be set to the
+        // stored spec.
+        UUID targetConfig = configStore.store(serviceSpec);
+        configStore.setTargetConfig(targetConfig);
+
+        return configStore;
     }
 
 }
