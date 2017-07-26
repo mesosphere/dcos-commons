3330a060e13e9a84b25f9aad60c5f0b2c127df59
benclarkwood <ben@benclarkwood.com>
2017-07-25 17:46:17 -0700

    Add logging to detect sequence of events

--- frameworks/template/tests/conftest.py
+++ frameworks/template/tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
 import sdk_repository as repo
-
+import sdk_utils
 
 @pytest.fixture(scope='session')
 def configure_universe():
     stub_urls = {}
     try:
+        sdk_utils.out("Adding stub universe URL")
         stub_urls = repo.add_universe_repos()
         yield # let the test session execute
     finally:
