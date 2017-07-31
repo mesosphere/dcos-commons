584584b65e80c06c66c88e43a3ca9b8bc9b3aac2
Susan X. Huynh <xhuynh@mesosphere.com>
2017-07-31 09:20:37 -0700

    In Secrets test: installed dcos-enterprise-cli package in order for "dcos security" command to be available.

--- frameworks/helloworld/tests/test_soak.py
+++ frameworks/helloworld/tests/test_soak.py
@@ -37,6 +37,7 @@ def test_soak_secrets_update():
     secret_content_alternative = "hello-world-secret-data-alternative"
     test_soak_secrets_framework_alive()
 
+    sdk_cmd.run_cli("package install --cli dcos-enterprise-cli")
     sdk_cmd.run_cli("security secrets update --value={} secrets/secret1".format(secret_content_alternative))
     sdk_cmd.run_cli("security secrets update --value={} secrets/secret2".format(secret_content_alternative))
     sdk_cmd.run_cli("security secrets update --value={} secrets/secret3".format(secret_content_alternative))
