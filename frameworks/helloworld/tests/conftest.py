662b6412d1fb820bac92b8825545a4063d8366e9
Kevin Wood <kwood@mesosphere.com>
2017-08-08 12:57:16 -0500

    login to cluster and configure cli

--- frameworks/helloworld/tests/conftest.py
+++ frameworks/helloworld/tests/conftest.py
@@ -1,10 +1,15 @@
 import pytest
+import sdk_login
 import sdk_repository
 import sdk_security
 
 
 @pytest.fixture(scope='session')
-def configure_universe():
+def configure_login():
+    yield from sdk_login.login_session()
+
+@pytest.fixture(scope='session')
+def configure_universe(configure_login):
     yield from sdk_repository.universe_session()
 
 @pytest.fixture(scope='session')
