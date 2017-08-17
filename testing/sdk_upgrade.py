005147732d2b248a11a21860904034058d32c2cb
nick <nick@mesosphere.io>
2017-08-17 16:41:01 -0700

    Exercise new upgrade flows
    Includes a rename workaround: Cosmos doesn't support upgrades across package names, which causes problems for our "beta-pkg" => "pkg" flow.
    To get around this, the test will perform surgery on the Scheduler's labels to make it appear to be from the target package.

--- testing/sdk_upgrade.py
+++ testing/sdk_upgrade.py
@@ -1,14 +1,22 @@
+import base64
+import gzip
 import json
 import logging
 import re
 import shakedown
+import tempfile
 
 import sdk_cmd as cmd
 import sdk_install as install
 import sdk_marathon as marathon
+import sdk_plan as plan
 import sdk_tasks as tasks
 
 log = logging.getLogger(__name__)
+
+PACKAGE_NAME_LABEL = 'DCOS_PACKAGE_NAME'
+PACKAGE_METADATA_LABEL = 'DCOS_PACKAGE_METADATA'
+PACKAGE_DEF_LABEL = 'DCOS_PACKAGE_DEFINITION'
 
 # Installs a universe version, then upgrades it to a test version
 #
@@ -68,7 +76,9 @@ def test_upgrade(
 
     log.info('Upgrading to test version: {}={}'.format(test_package_name, test_version))
     _upgrade_or_downgrade(
+        universe_package_name,
         test_package_name,
+        test_version,
         service_name,
         running_task_count,
         test_version_options,
@@ -110,7 +120,9 @@ def test_downgrade(
 
         log.info('Downgrading to Universe version: {}={}'.format(universe_package_name, universe_version))
         _upgrade_or_downgrade(
+            test_package_name,
             universe_package_name,
+            universe_version,
             service_name,
             running_task_count,
             additional_options,
@@ -125,7 +137,9 @@ def test_downgrade(
     if reinstall_test_version:
         log.info('Re-upgrading to test version before exiting: {}={}'.format(test_package_name, test_version))
         _upgrade_or_downgrade(
+            universe_package_name,
             test_package_name,
+            test_version,
             service_name,
             running_task_count,
             test_version_options,
@@ -181,19 +195,25 @@ def soak_upgrade_downgrade(
         running_task_count,
         install_options={},
         timeout_seconds=25*60):
-    print('Upgrading to test version')
+    version = 'stub-universe'
+    print('Upgrading to test version: {} => {} {}'.format(universe_package_name, test_package_name, version))
     _upgrade_or_downgrade(
+        universe_package_name,
         test_package_name,
+        version,
         service_name,
         running_task_count,
         install_options,
         timeout_seconds,
-        package_version='stub-universe')
+        package_version)
 
-    print('Downgrading to Universe version')
     # Default Universe is at --index=0
+    version = _get_pkg_version(universe_package_name)
+    print('Downgrading to Universe version: {} => {} {}'.format(test_package_name, universe_package_name, version))
     _upgrade_or_downgrade(
+        test_package_name,
         universe_package_name,
+        version,
         service_name,
         running_task_count,
         install_options,
@@ -210,23 +230,88 @@ def _get_universe_url():
 
 
 def _upgrade_or_downgrade(
-        package_name,
+        from_package_name,
+        to_package_name,
+        to_package_version,
         service_name,
         running_task_count,
         additional_options,
-        timeout_seconds,
-        package_version=None):
+        timeout_seconds):
     task_ids = tasks.get_task_ids(service_name, '')
-    marathon.destroy_app(service_name)
-    install.install(
-        package_name,
-        running_task_count,
-        service_name=service_name,
-        additional_options=additional_options,
-        timeout_seconds=timeout_seconds,
-        package_version=package_version)
+    if shakedown.dcos_version_less_than("1.10") or shakedown.ee_version() is None:
+        # Marathon upgrade flow
+        marathon.destroy_app(service_name)
+        install.install(
+            to_package_name,
+            running_task_count,
+            service_name=service_name,
+            additional_options=additional_options,
+            timeout_seconds=timeout_seconds,
+            package_version=to_package_version)
+    else:
+        # CLI upgrade flow
+        if from_package_name != to_package_name:
+            # cosmos doesn't support upgrades across package names.
+            # perform surgery on the running app's labels so that cosmos thinks the package names match.
+            log.info('Renaming package in app {}: {} => {}'.format(service_name, from_package_name, to_package_name))
+            new_config = _rename_package(marathon.get_config(service_name), to_package_name)
+            marathon.update_app(service_name, new_config)
+            plan.wait_for_completed_deployment(service_name)
+        if additional_options:
+            with tempfile.NamedTemporaryFile() as opts_f:
+                opts_f.write(json.dumps(additional_options))
+                cmd.run_cli('{} --name={} update start --package-version={} --options={}'.format(to_package_name, service_name, to_package_version, opts_f.name))
+        else:
+            cmd.run_cli('{} --name={} update start --package-version={}'.format(to_package_name, service_name, to_package_version))
     log.info('Checking that all tasks have restarted')
     tasks.check_tasks_updated(service_name, '', task_ids)
+
+
+def _rename_package(marathon_app, desired_name):
+    '''Updates the package name in the provided package.
+    This hack allows us to test upgrades across package names, such as 'beta-kafka' v6 => 'kafka' stub-universe'''
+    labels = marathon_app['labels']
+
+    for expected_label in [PACKAGE_NAME_LABEL, PACKAGE_METADATA_LABEL, PACKAGE_DEF_LABEL]:
+        if expected_label not in labels:
+            raise 'Missing required label "{}" in marathon app: {}.'.format(expected_label, marathon_app['labels'])
+
+    # PACKAGE_NAME
+    log.info('Updating {}:'.format(PACKAGE_NAME_LABEL))
+    log.info('  {} => {}'.format(labels[PACKAGE_NAME_LABEL], desired_name))
+    labels[PACKAGE_NAME_LABEL] = desired_name
+
+    # PACKAGE_METADATA
+    # b64decode => update blob.name => b64encode
+    log.info('Updating {}.{}:'.format(PACKAGE_METADATA_LABEL, 'name'))
+    pkg_metadata = json.loads(base64.b64decode(labels[PACKAGE_METADATA_LABEL]).decode('utf-8'))
+    log.info('  {} => {}'.format(pkg_metadata['name'], desired_name))
+    pkg_metadata['name'] = desired_name
+    labels[PACKAGE_METADATA_LABEL] = base64.b64encode(json.dumps(pkg_metadata).encode('utf-8')).decode('utf-8')
+
+    # PACKAGE_DEFINITION
+    # b64decode => get data field => b64decode => gunzip => set blob.name => gzip => b64encode => update data field => b64encode
+    log.info('Updating {}.data.{}:'.format(PACKAGE_METADATA_LABEL, 'name'))
+
+    pkg_def = json.loads(base64.b64decode(labels[PACKAGE_DEF_LABEL]).decode('utf-8'))
+    use_gzip = pkg_def['metadata']['Content-Encoding'] == 'gzip'
+
+    pkg_def_data_raw = base64.b64decode(pkg_def['data'])
+    if use_gzip:
+        pkg_def_data_raw = gzip.decompress(pkg_def_data_raw)
+    pkg_def_data = json.loads(pkg_def_data_raw.decode('utf-8'))
+
+    log.info('  {} => {}'.format(pkg_def_data['name'], desired_name))
+    pkg_def_data['name'] = desired_name
+
+    pkg_def_data_raw = json.dumps(pkg_def_data).encode('utf-8')
+    if use_gzip:
+        pkg_def_data_raw = gzip.compress(pkg_def_data_raw)
+    pkg_def['data'] = base64.b64encode(pkg_def_data_raw).decode('utf-8')
+    labels[PACKAGE_DEF_LABEL] = base64.b64encode(json.dumps(pkg_def).encode('utf-8')).decode('utf-8')
+
+    marathon_app['labels'] = labels
+    return marathon_app
 
 
 def _get_pkg_version(package_name):
