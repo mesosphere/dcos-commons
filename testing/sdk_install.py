4b01167e27ba4362bce38a3705c0b3804ef853b0
Mehmet <mbalman@mesosphere.com>
2017-08-17 15:36:59 -0700

    Give principal in additional options, to install a previous package version. New version uses service_account instead of principal in config.json.

--- testing/sdk_install.py
+++ testing/sdk_install.py
@@ -193,6 +193,7 @@ def get_package_options(additional_options={}):
         return _merge_dictionaries(additional_options, {
             'service': {
                 'service_account': 'service-acct',
+                'principal': 'service-acct',
                 'secret_name': 'secret',
                 'mesos_api_version': 'V0'
             }
