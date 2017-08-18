2fc8bb6412a83a18ab7a033f82cfec552b91bd5f
Kevin Wood <kwood@mesosphere.com>
2017-08-18 11:44:57 -0500

    handle header line; get stdout, and stderr

--- conftest.py
+++ conftest.py
@@ -21,13 +21,15 @@ logging.basicConfig(
     format='[%(asctime)s|%(name)s|%(levelname)s]: %(message)s',
     level=log_level)
 
+log = logging.getLogger(__name__)
+
 
 def get_task_ids(user: str=None):
     """ This function uses dcos task WITHOUT the JSON options because
     that can return the wrong user for schedulers
     """
     tasks = subprocess.check_output(['dcos', 'task']).decode().split('\n')
-    for task_str in tasks:
+    for task_str in tasks[1:]: # First line is the header line
         task = task_str.split()
         if len(task) < 5:
             continue
@@ -35,13 +37,14 @@ def get_task_ids(user: str=None):
             yield task[4]
 
 
-def get_task_logs_for_id(task_id: str, lines: int=1000000):
+def get_task_logs_for_id(task_id: str,  task_file: str='stdout', lines: int=1000000):
     try:
         task_logs = subprocess.check_output([
-            'dcos', 'task', 'log', task_id, '--lines', str(lines), 'stderr'
+            'dcos', 'task', 'log', task_id, '--lines', str(lines), task_file
         ]).decode()
         return task_logs
-    except subprocess.CalledProcessError:
+    except subprocess.CalledProcessError as e:
+        log.exception('Failed to get {} task log for task_id={}'.format(task_file, task_id))
         return None
 
 
@@ -71,10 +74,10 @@ def get_task_logs_on_failure(request):
             continue
         if not getattr(request.node, report).failed:
             continue
-        # Scheduler should be the only task running as root
-        for root_task in get_task_ids():
-            log_name = '{}_{}.log'.format(request.node.name, root_task)
-            task_logs = get_task_logs_for_id(root_task)
-            if task_logs:
-                with open(log_name, 'w') as f:
-                    f.write(task_logs)
+        for task_id in get_task_ids():
+            for task_file in ('stderr', 'stdout'):
+                log_name = '{}_{}_{}.log'.format(request.node.name, task_id, task_file)
+                task_logs = get_task_logs_for_id(task_id, task_file=task_file)
+                if task_logs:
+                    with open(log_name, 'w') as f:
+                        f.write(task_logs)
