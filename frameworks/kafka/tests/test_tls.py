67c631d205c977244e9347cfd1dc2b19afea080e
benclarkwood <ben@benclarkwood.com>
2017-08-21 21:24:38 -0700

    Conver new file to import style

--- frameworks/kafka/tests/test_tls.py
+++ frameworks/kafka/tests/test_tls.py
@@ -10,12 +10,9 @@ import sdk_plan
 import sdk_security
 import sdk_utils
 
+from tests import config
 
 from tests.test_utils import (
-    DEFAULT_TOPIC_NAME,
-    DEFAULT_PARTITION_COUNT,
-    DEFAULT_BROKER_COUNT,
-    PACKAGE_NAME,
     service_cli
 )
 
@@ -51,8 +48,8 @@ def service_account(dcos_security_cli):
 @pytest.fixture(scope='module')
 def kafka_service_tls(service_account):
     sdk_install.install(
-        PACKAGE_NAME,
-        DEFAULT_BROKER_COUNT,
+        config.PACKAGE_NAME,
+        config.DEFAULT_BROKER_COUNT,
         service_name=service_account,
         additional_options={
             "service": {
@@ -63,14 +60,14 @@ def kafka_service_tls(service_account):
         }
     )
 
-    sdk_plan.wait_for_completed_deployment(PACKAGE_NAME)
+    sdk_plan.wait_for_completed_deployment(config.PACKAGE_NAME)
 
     # Wait for service health check to pass
-    shakedown.service_healthy(PACKAGE_NAME)
+    shakedown.service_healthy(config.PACKAGE_NAME)
 
     yield service_account
 
-    sdk_install.uninstall(PACKAGE_NAME)
+    sdk_install.uninstall(config.PACKAGE_NAME)
 
 
 @pytest.mark.tls
@@ -78,14 +75,14 @@ def kafka_service_tls(service_account):
 @pytest.mark.sanity
 @sdk_utils.dcos_1_10_or_higher
 def test_tls_endpoints(kafka_service_tls):
-    endpoints = sdk_networks.get_and_test_endpoints("", PACKAGE_NAME, 2)
+    endpoints = sdk_networks.get_and_test_endpoints("", config.PACKAGE_NAME, 2)
     assert BROKER_TLS_ENDPOINT in endpoints
 
     # Test that broker-tls endpoint is available
     endpoint_tls = service_cli(
         'endpoints {name}'.format(name=BROKER_TLS_ENDPOINT)
     )
-    assert len(endpoint_tls['dns']) == DEFAULT_BROKER_COUNT
+    assert len(endpoint_tls['dns']) == config.DEFAULT_BROKER_COUNT
 
 
 @pytest.mark.tls
@@ -93,15 +90,15 @@ def test_tls_endpoints(kafka_service_tls):
 @pytest.mark.sanity
 @sdk_utils.dcos_1_10_or_higher
 def test_producer_over_tls(kafka_service_tls):
-    service_cli('topic create {}'.format(DEFAULT_TOPIC_NAME))
+    service_cli('topic create {}'.format(config.DEFAULT_TOPIC_NAME))
 
-    topic_info = service_cli('topic describe {}'.format(DEFAULT_TOPIC_NAME))
-    assert len(topic_info['partitions']) == DEFAULT_PARTITION_COUNT
+    topic_info = service_cli('topic describe {}'.format(config.DEFAULT_TOPIC_NAME))
+    assert len(topic_info['partitions']) == config.DEFAULT_PARTITION_COUNT
 
     # Warm up TLS connections
-    write_info = service_cli('topic producer_test_tls {} {}'.format(DEFAULT_TOPIC_NAME, 10))
+    write_info = service_cli('topic producer_test_tls {} {}'.format(config.DEFAULT_TOPIC_NAME, 10))
 
     num_messages = 10
-    write_info = service_cli('topic producer_test_tls {} {}'.format(DEFAULT_TOPIC_NAME, num_messages))
+    write_info = service_cli('topic producer_test_tls {} {}'.format(config.DEFAULT_TOPIC_NAME, num_messages))
     assert len(write_info) == 1
     assert write_info['message'].startswith('Output: {} records sent'.format(num_messages))
