5f6412feb6a1e7bd4b475f7b5d98f38cac0dbdb9
Evan Lezar <elezar@mesosphere.com>
2017-08-28 17:41:40 +0200

    Add unit tests for PreReservedRolesCannotChange validator

--- sdk/scheduler/src/main/java/com/mesosphere/sdk/config/validate/PreReservationCannotChange.java
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/config/validate/PreReservationCannotChange.java
@@ -3,8 +3,6 @@ package com.mesosphere.sdk.config.validate;
 import com.mesosphere.sdk.specification.PodSpec;
 import com.mesosphere.sdk.specification.ServiceSpec;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -14,34 +12,35 @@ import java.util.stream.Collectors;
  */
 public class PreReservationCannotChange implements ConfigValidator<ServiceSpec> {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(PreReservationCannotChange.class);
-
     @Override
     public Collection<ConfigValidationError> validate(Optional<ServiceSpec> oldConfig, ServiceSpec newConfig) {
         if (!oldConfig.isPresent()) {
             return Collections.emptyList();
         }
 
-        Map<String, PodSpec> newPods = newConfig.getPods().stream()
+        final Map<String, PodSpec> newPods = newConfig.getPods().stream()
                 .collect(Collectors.toMap(podSpec -> podSpec.getType(), podSpec -> podSpec));
 
-        List<ConfigValidationError> errors = new ArrayList<>();
-        for (PodSpec oldPod : oldConfig.get().getPods()) {
+        final List<ConfigValidationError> errors = new ArrayList<>();
+        for (final PodSpec oldPod : oldConfig.get().getPods()) {
             final PodSpec newPod = newPods.get(oldPod.getType());
 
             if (newPod == null) {
                 continue;
             }
 
-            if (!StringUtils.equals(newPod.getPreReservedRole(), oldPod.getPreReservedRole())) {
+            final String oldPodPreReservedRole = oldPod.getPreReservedRole();
+            final String newPodPreReservedRole = newPod.getPreReservedRole();
+
+            if (!StringUtils.equals(newPodPreReservedRole, oldPodPreReservedRole)) {
                 errors.add(ConfigValidationError.transitionError(
-                        String.format("PodSpec[pre-reserved-role:%s]", oldPod.getPreReservedRole()),
-                        oldPod.getPreReservedRole(),
-                        newPod.getPreReservedRole(),
+                        String.format("PodSpec[pre-reserved-role:%s]", oldPodPreReservedRole),
+                        oldPodPreReservedRole,
+                        newPodPreReservedRole,
                         String.format(
                                 "New config has changed the pre-reserved-role of PodSpec named '%s'" +
                                         " (expected it to stay '%s')",
-                                oldPod.getType(), oldPod.getPreReservedRole())));
+                                oldPod.getType(), oldPodPreReservedRole)));
             }
         }
 
