5e657e01c09828e1baec9e220bfc0338d650707c
Loren Siebert <loren@siebert.org>
2017-09-05 10:04:21 -0700

    [ELASTIC-112] Test replace of each pod type
    - Added tests for data, coordinator, and ingest nodes

--- frameworks/elastic/tests/test_ingest.py
+++ frameworks/elastic/tests/test_ingest.py
@@ -1,10 +1,13 @@
 import pytest
+import shakedown
 
+import sdk_cmd as cmd
 import sdk_install
 import sdk_marathon
 import sdk_tasks
+import sdk_utils
 from tests import config
-import shakedown
+
 
 # This file tests installing the elastic framework with the new default of 0 ingest nodes.
 # We can't test it in `test_sanity.py` because the upgrade test in `configure_package` won't work,
@@ -42,5 +45,15 @@ def test_zero_to_one_ingest():
     marathon_config = sdk_marathon.get_config(config.SERVICE_NAME)
     marathon_config['env']['INGEST_NODE_COUNT'] = "1"
     sdk_marathon.update_app(config.SERVICE_NAME, marathon_config)
+    sdk_tasks.check_running(config.SERVICE_NAME, config.DEFAULT_TASK_COUNT)
+    config.wait_for_expected_nodes_to_exist()
+
+
+@pytest.mark.recovery
+@pytest.mark.sanity
+def test_ingest_node_replace():
+    ingest_ids = sdk_tasks.get_task_ids(sdk_utils.get_foldered_name(config.SERVICE_NAME), 'ingest-0')
+    cmd.svc_cli(config.PACKAGE_NAME, sdk_utils.get_foldered_name(config.SERVICE_NAME), 'pod replace ingest-0')
+    sdk_tasks.check_tasks_updated(sdk_utils.get_foldered_name(config.SERVICE_NAME), 'ingest-0', ingest_ids)
     sdk_tasks.check_running(config.SERVICE_NAME, config.DEFAULT_TASK_COUNT)
     config.wait_for_expected_nodes_to_exist()
