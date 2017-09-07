0398f6e36e9325baf7798dce1fa8f647fb9ea03f
Ben Wood <ben@benclarkwood.com>
2017-09-07 16:33:30 -0700

    [INFINITY-2374] Disable tests that will not pass in open (#1567) (#1579)
    * Detect DC/OS Open and disable tests for features that do not work there.
    
    * Bad python
    
    * Disable C* TLS test in open

--- testing/sdk_utils.py
+++ testing/sdk_utils.py
@@ -4,6 +4,7 @@ import logging
 import dcos
 import shakedown
 import pytest
+import os
 
 log = logging.getLogger(__name__)
 
@@ -41,6 +42,17 @@ def get_zk_path(service_name):
 @functools.lru_cache()
 def dcos_version_less_than(version):
     return shakedown.dcos_version_less_than("1.10")
+
+
+def is_open_dcos():
+    '''Determine if the tests are being run against open DC/OS. This is presently done by
+    checking the envvar DCOS_ENTERPRISE.'''
+    return os.environ.get('DCOS_ENTERPRISE', 'true').lower() != 'true'
+
+
+dcos_ee_only = pytest.mark.skipif(
+    'sdk_utils.is_open_dcos',
+    reason="Feature only supported in DC/OS EE.")
 
 
 # WARNING: Any file that uses these must also "import shakedown" in the same file.
