e15224fa698c21cea6ed6ce452bea730077ff5ee
Loren Siebert <loren@siebert.org>
2017-09-07 14:13:36 -0700

    [ELASTIC-112] Test replace of each pod type (#1550)
    * [ELASTIC-112] Test replace of each pod type
    
    - Added tests for data, coordinator, and ingest nodes
    
    * [ELASTIC-112] Test replace of each pod type
    
    - Fix failing test
    
    * [ELASTIC-112] Test replace of each pod type
    
    - Fix failing test

--- frameworks/elastic/tests/test_ingest.py
+++ frameworks/elastic/tests/test_ingest.py
@@ -1,10 +1,13 @@
 import pytest
+import shakedown
 
+import sdk_cmd as cmd
 import sdk_install
 import sdk_marathon
 import sdk_tasks
+import sdk_utils
 from tests import config
-import shakedown
+
 
 # This file tests installing the elastic framework with the new default of 0 ingest nodes.
 # We can't test it in `test_sanity.py` because the upgrade test in `configure_package` won't work,
@@ -25,22 +28,32 @@ def configure_package(configure_security):
         sdk_install.uninstall(config.PACKAGE_NAME, config.SERVICE_NAME)
 
 
-@pytest.fixture(autouse=True)
-def pre_test_setup():
-    sdk_tasks.check_running(config.SERVICE_NAME, config.NO_INGEST_TASK_COUNT)
-    config.wait_for_expected_nodes_to_exist(task_count=config.NO_INGEST_TASK_COUNT)
-
-
 @pytest.mark.sanity
 def test_service_health():
+    sdk_tasks.check_running(config.SERVICE_NAME, config.NO_INGEST_TASK_COUNT)
+    config.wait_for_expected_nodes_to_exist(task_count=config.NO_INGEST_TASK_COUNT)
     assert shakedown.service_healthy(config.SERVICE_NAME)
 
 
 @pytest.mark.recovery
 @pytest.mark.sanity
 def test_zero_to_one_ingest():
+    sdk_tasks.check_running(config.SERVICE_NAME, config.NO_INGEST_TASK_COUNT)
+    config.wait_for_expected_nodes_to_exist(task_count=config.NO_INGEST_TASK_COUNT)
     marathon_config = sdk_marathon.get_config(config.SERVICE_NAME)
     marathon_config['env']['INGEST_NODE_COUNT'] = "1"
     sdk_marathon.update_app(config.SERVICE_NAME, marathon_config)
+    sdk_tasks.check_running(config.SERVICE_NAME, config.DEFAULT_TASK_COUNT)
+    config.wait_for_expected_nodes_to_exist()
+
+
+@pytest.mark.recovery
+@pytest.mark.sanity
+def test_ingest_node_replace():
+    sdk_tasks.check_running(config.SERVICE_NAME, config.DEFAULT_TASK_COUNT)
+    config.wait_for_expected_nodes_to_exist()
+    ingest_ids = sdk_tasks.get_task_ids(config.SERVICE_NAME, 'ingest-0')
+    cmd.svc_cli(config.PACKAGE_NAME, config.SERVICE_NAME, 'pod replace ingest-0')
+    sdk_tasks.check_tasks_updated(config.SERVICE_NAME, 'ingest-0', ingest_ids)
     sdk_tasks.check_running(config.SERVICE_NAME, config.DEFAULT_TASK_COUNT)
     config.wait_for_expected_nodes_to_exist()
