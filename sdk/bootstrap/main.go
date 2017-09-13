2ddfb861cd74ca9170d2c62e88bbdea2f65b57af
Ben Wood <ben@benclarkwood.com>
2017-09-12 23:45:08 -0700

    Revert "[INFINITY-23222] Fix bootstrap networking shim (#1517)" (#1614)
    This reverts commit b0e86f34889ed41739c1b6506670cbb8ca6ad82c.

--- sdk/bootstrap/main.go
+++ sdk/bootstrap/main.go
@@ -336,13 +336,31 @@ func isFile(path string) (bool, error) {
 	return false, nil
 }
 
-func getContainerIPAddress() (string, error) {
-	ip, err := ContainerIP()
+func GetLocalIPold() string {
+	addrs, err := net.InterfaceAddrs()
 	if err != nil {
-		return "", err
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
+}
+
+func GetLocalIP() (addr string, err error) {
+	ip, err := ContainerIP()
+
+	if err != nil {
+		return ip.String(), err
 	}
 
-	return ip.String(), nil
+	addr = ip.String()
+	return ip.String(), err
 }
 
 // main
@@ -350,29 +368,20 @@ func getContainerIPAddress() (string, error) {
 func main() {
 	args := parseArgs()
 
-	log.Printf("Looking up container IP address...")
-	containerIP, err := getContainerIPAddress()
+	pod_ip, err := GetLocalIP()
 	if err != nil {
-		log.Fatalf("Cannot look up container's IP address: %s", err)
+		log.Fatalf("Cannot find the container's IP address: ", err)
 	}
-	log.Printf("Container IP address was found to be: %s", containerIP)
-	// Set both LIBPROCESS_IP and MESOS_CONTAINER_IP so that SDK developers
-	// can consume either seamlessly.
-	log.Printf("Overwriting %s and %s...", libprocessIP, mesosContainerIP)
-	log.Printf("Overwriting %s from %s => %s", libprocessIP, os.Getenv(libprocessIP), containerIP)
-	err = os.Setenv(libprocessIP, containerIP)
+
+	err = os.Setenv("LIBPROCESS_IP", pod_ip)
+	err = os.Setenv("MESOS_CONTAINER_IP", pod_ip)
 	if err != nil {
-		log.Fatalf("Failed to SET new %s: %v", libprocessIP, err)
-	}
-	log.Printf("Overwriting %s from %s => %s", mesosContainerIP, os.Getenv(mesosContainerIP), containerIP)
-	err = os.Setenv(mesosContainerIP, containerIP)
-	if err != nil {
-		log.Fatalf("Failed to SET new %s: %v", mesosContainerIP, err)
+		log.Fatalf("Failed to SET new LIBPROCESS_IP: ", err)
 	}
 
 	if args.getTaskIp {
-		log.Printf("Printing container IP: %s", containerIP)
-		fmt.Printf("%s", containerIP)
+		log.Printf("Printing new task IP: %s", pod_ip)
+		fmt.Printf("%s", pod_ip)
 		os.Exit(0)
 	}
 
@@ -396,6 +405,6 @@ func main() {
 		installDCOSCertIntoJRE()
 	}
 
-	log.Printf("Local IP --> %s", containerIP)
+	log.Printf("Local IP --> %s", pod_ip)
 	log.Printf("SDK Bootstrap successful.")
 }
