8719f816d0b380fe6ddc8c115e8968a865e952bf
Evan Lezar <elezar@mesosphere.com>
2017-09-15 13:07:30 +0200

    conftest cannot import config

--- frameworks/kafka/tests/conftest.py
+++ frameworks/kafka/tests/conftest.py
@@ -1,4 +1,3 @@
-import config
 import pytest
 import sdk_repository
 import sdk_security
@@ -10,4 +9,4 @@ def configure_universe():
 
 @pytest.fixture(scope='session')
 def configure_security(configure_universe):
-    yield from sdk_security.security_session(config.SERVICE_NAME)
+    yield from sdk_security.security_session('kafka')
