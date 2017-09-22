bc6d282b332049b8a302e0442689bdd9701bcc2b
Nima Vaziri <NimaVaziri@users.noreply.github.com>
2017-09-22 16:55:14 -0700

    [INFINITY-2299][HDFS] Automate replace of a journal node (#1628)
    * Add VERSION file upload in journal node start up
    
    * Add recovery logic for journal node replace
    
    * Fixed log statement
    
    * Fixed dns name to hit jetty server for a foldered service
    
    * Wrap VERSION file fetch with echos

--- frameworks/hdfs/src/main/java/com/mesosphere/sdk/hdfs/scheduler/HdfsRecoveryPlanOverrider.java
+++ frameworks/hdfs/src/main/java/com/mesosphere/sdk/hdfs/scheduler/HdfsRecoveryPlanOverrider.java
@@ -18,40 +18,62 @@ import java.util.*;
  */
 public class HdfsRecoveryPlanOverrider implements RecoveryPlanOverrider {
     protected final Logger logger = LoggerFactory.getLogger(getClass());
-    private static final String NN_PHASE_NAME = "permanent-nn-failure-recovery";
+    private static final String PHASE_NAME = "permanent-%s-failure-recovery";
+    private static final String NN_PHASE_NAME = "name";
+    private static final String JN_PHASE_NAME = "journal";
     private final StateStore stateStore;
-    private final Plan replaceNameNodePlan;
+    private final Plan replacePlan;
 
-    public HdfsRecoveryPlanOverrider(StateStore stateStore, Plan replaceNameNodePlan) {
+    public HdfsRecoveryPlanOverrider(StateStore stateStore, Plan replacePlan) {
         this.stateStore = stateStore;
-        this.replaceNameNodePlan = replaceNameNodePlan;
+        this.replacePlan = replacePlan;
     }
 
     @Override
     public Optional<Phase> override(PodInstanceRequirement stoppedPod) {
-        if (!stoppedPod.getPodInstance().getPod().getType().equals("name")
+        if (stoppedPod.getPodInstance().getPod().getType().equals("data")
                 || stoppedPod.getRecoveryType() != RecoveryType.PERMANENT) {
-            logger.info("No overrides necessary. Pod is not a name node or it isn't a permanent failure.");
+            logger.info("No overrides necessary. Pod is not a journal or name node or it isn't a permanent failure.");
             return Optional.empty();
         }
 
-        switch (stoppedPod.getPodInstance().getIndex()) {
-            case 0:
-                logger.info("Returning replacement plan for namenode 0.");
-                return Optional.of(getNNRecoveryPhase(replaceNameNodePlan, 0));
-            case 1:
-                logger.info("Returning replacement plan for namenode 1.");
-                return Optional.of(getNNRecoveryPhase(replaceNameNodePlan, 1));
-            default:
-                logger.error(
-                        "Encountered unexpected index: {}, falling back to default recovery plan manager.",
-                        stoppedPod.getPodInstance().getIndex());
-                return Optional.empty();
+        if (stoppedPod.getPodInstance().getPod().getType().equals("name")) {
+            switch (stoppedPod.getPodInstance().getIndex()) {
+                case 0:
+                    logger.info("Returning replacement plan for namenode 0.");
+                    return Optional.of(getRecoveryPhase(replacePlan, 0, NN_PHASE_NAME));
+                case 1:
+                    logger.info("Returning replacement plan for namenode 1.");
+                    return Optional.of(getRecoveryPhase(replacePlan, 1, NN_PHASE_NAME));
+                default:
+                    logger.error(
+                            "Encountered unexpected index: {}, falling back to default recovery plan manager.",
+                            stoppedPod.getPodInstance().getIndex());
+                    return Optional.empty();
+            }
+        } else {
+            switch (stoppedPod.getPodInstance().getIndex()) {
+                case 0:
+                    logger.info("Returning replacement plan for journalnode 0.");
+                    return Optional.of(getRecoveryPhase(replacePlan, 0, JN_PHASE_NAME));
+                case 1:
+                    logger.info("Returning replacement plan for journalnode 1.");
+                    return Optional.of(getRecoveryPhase(replacePlan, 1, JN_PHASE_NAME));
+                case 2:
+                    logger.info("Returning replacement plan for journalnode 2.");
+                    return Optional.of(getRecoveryPhase(replacePlan, 2, JN_PHASE_NAME));
+                default:
+                    logger.error(
+                            "Encountered unexpected index: {}, falling back to default recovery plan manager.",
+                            stoppedPod.getPodInstance().getIndex());
+                    return Optional.empty();
+            }
+
         }
     }
 
-    private Phase getNNRecoveryPhase(Plan inputPlan, int index) {
-        Phase inputPhase = inputPlan.getChildren().get(0);
+    private Phase getRecoveryPhase(Plan inputPlan, int index, String phaseName) {
+        Phase inputPhase = getPhaseForNodeType(inputPlan, phaseName);
         int offset = index * 2;
 
         // Bootstrap
@@ -70,7 +92,7 @@ public class HdfsRecoveryPlanOverrider implements RecoveryPlanOverrider {
                         new UnconstrainedLaunchConstrainer(),
                         stateStore);
 
-        // NameNode
+        // JournalNode or NameNode
         Step inputNodeStep = inputPhase.getChildren().get(offset + 1);
         PodInstanceRequirement nameNodePpodInstanceRequirement =
                 PodInstanceRequirement.newBuilder(
@@ -87,9 +109,22 @@ public class HdfsRecoveryPlanOverrider implements RecoveryPlanOverrider {
                         stateStore);
 
         return new DefaultPhase(
-                NN_PHASE_NAME,
+                PHASE_NAME.format(phaseName),
                 Arrays.asList(bootstrapStep, nodeStep),
                 new SerialStrategy<>(),
                 Collections.emptyList());
+    }
+
+    private Phase getPhaseForNodeType(Plan inputPlan, String phaseName) {
+        Optional<Phase> phaseOptional = inputPlan.getChildren().stream()
+                .filter(phase -> phase.getName().equals(phaseName))
+                .findFirst();
+
+        if (!phaseOptional.isPresent()) {
+            throw new RuntimeException(
+                    String.format("Expected phase name %s does not exist in the service spec plan", phaseName));
+        }
+
+        return phaseOptional.get();
     }
 }
