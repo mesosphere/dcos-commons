240cdd8fb09745108500b99570a5fd0bf0a1b697
Evan Lezar <elezar@mesosphere.com>
2017-09-22 14:01:54 +0200

    Update build.sh and bootstrap build

--- sdk/bootstrap/build.sh
+++ sdk/bootstrap/build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-
-# exit immediately on failure
 set -e
 
 CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -9,13 +7,12 @@ PKG_FILENAME=${EXE_FILENAME}.zip
 
 cd $CUR_DIR
 
-../../tools/build_go_exe.sh sdk/bootstrap/ linux
+# required env:
+export REPO_ROOT_DIR=$(dirname $(dirname $CUR_DIR))
+export REPO_NAME=$(basename $REPO_ROOT_DIR)
+../../tools/build_go_exe.sh sdk/bootstrap/ bootstrap linux
 
 rm -f $PKG_FILENAME
-# build_go_exe.sh produces 'bootstrap-linux': add to zip as 'bootstrap'
-mv ${EXE_FILENAME}-linux $EXE_FILENAME
 zip -q $PKG_FILENAME $EXE_FILENAME
-# preserve original naming so that build_go_exe.sh can skip builds when unneeded:
-mv $EXE_FILENAME ${EXE_FILENAME}-linux
 
 echo $(pwd)/${PKG_FILENAME}
