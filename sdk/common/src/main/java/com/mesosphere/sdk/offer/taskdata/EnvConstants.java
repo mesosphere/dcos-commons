3e267cb5f8bdeb3b0ff6e3afd8c846bbf5312ba4
Nicholas Parker <nick@nickbp.com>
2017-10-04 10:05:49 -0700

    Add task envvars: service VIP domain, scheduler API hostname (#1705)
    - Adds FRAMEWORK_VIP_HOST to parallel FRAMEWORK_HOST: a domain for pointing service tasks to VIPs within the service
    - Replaces FRAMEWORK_SCHEDULER_NAME with SCHEDULER_API_HOSTNAME (originally added for, and currently only used by, HDFS), to avoid hardcoding the rest of the API VIP in tasks.
    - Update docs and examples to reflect the above

--- sdk/common/src/main/java/com/mesosphere/sdk/offer/taskdata/EnvConstants.java
+++ sdk/common/src/main/java/com/mesosphere/sdk/offer/taskdata/EnvConstants.java
@@ -12,15 +12,23 @@ public class EnvConstants {
 
     /** Provides the Task/Pod index of the instance, starting at 0. */
     public static final String POD_INSTANCE_INDEX_TASKENV = "POD_INSTANCE_INDEX";
+
     /** Prefix used for port environment variables which advertise reserved ports by their name. */
     public static final String PORT_NAME_TASKENV_PREFIX = "PORT_";
+
     /** Provides the configured name of the framework/service. */
     public static final String FRAMEWORK_NAME_TASKENV = "FRAMEWORK_NAME";
     /** Provides the name of the pod/task within the service. */
     public static final String TASK_NAME_TASKENV = "TASK_NAME";
-    /** Provides the host domain of the service. Pods within the service would be accessed as subdomains under this
-     *  domain. For example, <TASK_NAME>.<FRAMEWORK_HOST> => pod hostname. **/
+
+    /** Provides the host domain for pods within the service. Pods within the service would be accessed as subdomains
+     *  under this domain. For example, <TASK_NAME>.<FRAMEWORK_HOST> => pod hostname. */
     public static final String FRAMEWORK_HOST_TASKENV = "FRAMEWORK_HOST";
-    /** Provides the converted name of the scheduler in case the service has a foldered name. **/
-    public static final String FRAMEWORK_SCHEDULER_NAME_TASKENV = "FRAMEWORK_SCHEDULER_NAME";
+    /** Provides the host domain for VIPs within the service. VIPs within the service would be accessed as subdomains
+     *  under this domain. For example, <VIP_NAME>.<FRAMEWORK_VIP_HOST> => vip hostname. */
+    public static final String FRAMEWORK_VIP_HOST_TASKENV = "FRAMEWORK_VIP_HOST";
+
+    /** Provides the hostname of the Scheduler's own API. Pods within the service may access scheduler-hosted APIs under
+     *  this hostname. For example, <SCHEDULER_API_HOSTNAME>/v1/state/files/notavirus.exe.txt */
+    public static final String SCHEDULER_API_HOSTNAME_TASKENV = "SCHEDULER_API_HOSTNAME";
 }
