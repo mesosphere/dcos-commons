f3292551e92fd69cc30b7df47a7a22685a7c3bc2
Loren Siebert <loren@siebert.org>
2017-10-11 06:19:57 -0700

    Ensure all task types didn't restart, not just masters

--- frameworks/elastic/tests/test_sanity.py
+++ frameworks/elastic/tests/test_sanity.py
@@ -252,9 +252,9 @@ def test_plugin_install_and_uninstall(default_populated_index):
 @pytest.mark.sanity
 def test_unchanged_scheduler_restarts_without_restarting_tasks():
     foldered_name = sdk_utils.get_foldered_name(config.SERVICE_NAME)
-    initial_task_ids = sdk_tasks.get_task_ids(foldered_name, "master")
+    initial_task_ids = sdk_tasks.get_task_ids(foldered_name, '')
     shakedown.kill_process_on_host(sdk_marathon.get_scheduler_host(foldered_name), "elastic.scheduler.Main")
-    sdk_tasks.check_tasks_not_updated(foldered_name, "master", initial_task_ids)
+    sdk_tasks.check_tasks_not_updated(foldered_name, '', initial_task_ids)
 
 
 @pytest.mark.recovery
