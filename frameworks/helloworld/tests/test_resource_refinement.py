441cae3724d70d2a151054f299b1b71270e809d7
Evan Lezar <elezar@mesosphere.com>
2017-10-17 19:17:33 +0200

    Add module-level skip for test_resource_refinement in strict (#1744)

--- frameworks/helloworld/tests/test_resource_refinement.py
+++ frameworks/helloworld/tests/test_resource_refinement.py
@@ -2,6 +2,8 @@ import pytest
 import sdk_install
 from tests import config
 
+pytestmark = pytest.mark.skipif(sdk_utils.is_strict_mode(),
+                                reason='resource refinement is not yet supported in strict mode')
 
 @pytest.fixture(scope='module', autouse=True)
 def configure_package(configure_security):
@@ -13,9 +15,12 @@ def configure_package(configure_security):
             }
         }
 
-        sdk_install.install(config.PACKAGE_NAME, config.DEFAULT_TASK_COUNT, additional_options=options)
+        sdk_install.install(config.PACKAGE_NAME,
+                            config.SERVICE_NAME,
+                            config.DEFAULT_TASK_COUNT,
+                            additional_options=options)
 
-        yield # let the test session execute
+        yield  # let the test session execute
     finally:
         sdk_install.uninstall(config.PACKAGE_NAME)
 
