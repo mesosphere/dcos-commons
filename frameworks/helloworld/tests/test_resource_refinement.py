8310b2c8abb2c09bd9ffbec216a49dbd0f38a49f
Ben Wood <ben@benclarkwood.com>
2017-10-20 16:55:36 -0700

    [BACKPORT][INFINITY-2540] Add module-level skip for test_resource_refinement in strict (#1744) (#1750)
    * Add module-level skip for test_resource_refinement in strict (#1744)
    
    * Fix import
    
    * Fix merge issue

--- frameworks/helloworld/tests/test_resource_refinement.py
+++ frameworks/helloworld/tests/test_resource_refinement.py
@@ -1,7 +1,10 @@
 import pytest
 import sdk_install
+import sdk_utils
 from tests import config
 
+pytestmark = pytest.mark.skipif(sdk_utils.is_strict_mode(),
+                                reason='resource refinement is not yet supported in strict mode')
 
 @pytest.fixture(scope='module', autouse=True)
 def configure_package(configure_security):
@@ -13,9 +16,11 @@ def configure_package(configure_security):
             }
         }
 
-        sdk_install.install(config.PACKAGE_NAME, config.DEFAULT_TASK_COUNT, additional_options=options)
+        sdk_install.install(config.PACKAGE_NAME,
+                            config.DEFAULT_TASK_COUNT,
+                            additional_options=options)
 
-        yield # let the test session execute
+        yield  # let the test session execute
     finally:
         sdk_install.uninstall(config.PACKAGE_NAME)
 
