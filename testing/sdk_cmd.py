bbdf0959755ab472ec0c2c95de913cc9405f4c68
Evan Lezar <elezar@mesosphere.com>
2017-10-24 22:42:49 +0200

    [INFINITY-2552] Add basic logging to get_framework_srv_records (#1747) (#1765)
    * Add basic logging to get_framework_srv_records
    
    * Add retry to get_framework_srv_records
    
    * Make json conversions for commands a little safer

--- testing/sdk_cmd.py
+++ testing/sdk_cmd.py
@@ -25,16 +25,37 @@ def request(method, url, retry=True, log_args=True, **kwargs):
         return fn()
 
 
-def run_cli(cmd, print_output=True, return_stderr_in_stdout=False):
-    (stdout, stderr, ret) = shakedown.run_dcos_command(cmd, print_output=print_output)
-    if ret != 0:
-        err = 'Got error code {} when running command "dcos {}":\nstdout: "{}"\nstderr: "{}"'.format(
-            ret, cmd, stdout, stderr)
+def svc_cli(package_name, service_name, service_cmd, json=False, print_output=True, return_stderr_in_stdout=False):
+    full_cmd = '{} --name={} {}'.format(package_name, service_name, service_cmd)
+
+    if not json:
+        return run_cli(full_cmd, print_output=print_output, return_stderr_in_stdout=return_stderr_in_stdout)
+    else:
+        # TODO(elezar): We shouldn't use json=True and return_stderr_in_stdout=True together
+        # assert not return_stderr_in_stdout, json=True and return_stderr_in_stdout=True together should not be used together
+        return get_json_output(full_cmd, print_output=print_output)
+
+
+def run_raw_cli(cmd, print_output):
+    stdout, stderr, ret = shakedown.run_dcos_command(cmd, print_output=print_output)
+    if ret:
+        err = 'Got error code {} when running command "dcos {}":\n'\
+              'stdout: "{}"\n'\
+              'stderr: "{}"'.format(ret, cmd, stdout, stderr)
         log.error(err)
         raise dcos.errors.DCOSException(err)
+
+    return stdout, stderr
+
+
+def run_cli(cmd, print_output=True, return_stderr_in_stdout=False):
+
+    stdout, stderr = run_raw_cli(cmd, print_output)
+
     if return_stderr_in_stdout:
-        stdout = stdout + "\n" + stderr
-    return stdout
+        return stdout + "\n" + stderr
+    else:
+        return stdout
 
 
 def convert_string_list_to_list(output):
@@ -44,3 +65,19 @@ def convert_string_list_to_list(output):
     In spite of being a one-liner it standardizes the conversion.
     """
     return [element.strip() for element in ast.literal_eval(output)]
+
+
+def get_json_output(cmd, print_output=True):
+    stdout, stderr = run_raw_cli(cmd, print_output)
+
+    if stderr:
+        log.warn("stderr for command '%s' is non-empty: %s", cmd, stderr)
+
+    try:
+        json_stdout = jsonlib.loads(stdout)
+    except Exception as e:
+        log.error("Error converting stdout=%s to json", stdout)
+        log.error(e)
+        raise e
+
+    return json_stdout
