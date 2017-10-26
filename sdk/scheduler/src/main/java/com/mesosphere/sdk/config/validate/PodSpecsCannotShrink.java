3dc5b2a92b18fa006cdb4b54624dcd0f5e80254a
nick <nick@mesosphere.io>
2017-10-26 15:06:14 -0700

    Add opt-in bit to pod specs allowing truncation of pods
    (TODO: Actually implement the shrink itself)
    
    - Update validator to honor the opt-in bit.
    - Catch up YAML reference to current reality

--- sdk/scheduler/src/main/java/com/mesosphere/sdk/config/validate/PodSpecsCannotShrink.java
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/config/validate/PodSpecsCannotShrink.java
@@ -32,16 +32,24 @@ public class PodSpecsCannotShrink implements ConfigValidator<ServiceSpec> {
         Collection<ConfigValidationError> errors = new ArrayList<>();
         for (PodSpec oldPod : oldConfig.get().getPods()) {
             PodSpec newPod = newPods.get(oldPod.getType());
-            if (newPod == null) {
+            if (newPod == null && !oldPod.getAllowDecommission()) {
+                // Old pod is now missing. Allow the removal only if the old pod's allow-decommission bit was set.
+                // This could be used as an admittedly convoluted path for migrating away from an old service layout.
                 errors.add(ConfigValidationError.transitionError(
                         String.format("PodSpec[name:%s]", oldPod.getType()),
                         String.valueOf(oldPod.getCount()),
                         "null",
                         String.format("New config is missing PodSpec named '%s' (expected present with >= %d tasks)",
                                 oldPod.getType(), oldPod.getCount())));
-            } else if (newPod.getCount() < oldPod.getCount()) {
+            } else if (newPod != null && newPod.getCount() < oldPod.getCount() && !newPod.getAllowDecommission()) {
+                // New pod is present and is smaller than old pod.
+                // Only allow the new pod to be shrunk if its allow-decommission bit is set. We intentionally check
+                // allow-decommission against the new pod instead of the old pod with the following reasoning:
+                // - Older version disallows, newer version allows: Allow count to decrease now that it's supported
+                // - Older version allows, newer version disallows: Disallow count from decreasing now that it's no
+                //   longer supported
                 errors.add(ConfigValidationError.transitionError(
-                        String.format("PodSpec[setname:%s]", newPod.getType()),
+                        String.format("PodSpec[name:%s]", newPod.getType()),
                         String.valueOf(oldPod.getCount()),
                         String.valueOf(newPod.getCount()),
                         String.format("New config's PodSpec named '%s' has %d tasks, expected >=%d tasks",
