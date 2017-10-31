dd86e04a2ee074d537826cf73865528166797e67
Gabriel Hartmann <gabriel@mesosphere.io>
2017-10-31 10:35:31 -0700

    Abstract placement rule commonalities for easier extension

--- sdk/scheduler/src/main/java/com/mesosphere/sdk/offer/evaluate/placement/MaxPerAttributeRule.java
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/offer/evaluate/placement/MaxPerAttributeRule.java
@@ -10,6 +10,8 @@ import com.mesosphere.sdk.specification.validation.ValidationUtils;
 import org.apache.mesos.Protos.Offer;
 import org.apache.mesos.Protos.TaskInfo;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.stream.Collectors;
--- sdk/scheduler/src/test/java/com/mesosphere/sdk/offer/evaluate/placement/MaxPerHostnameRuleTest.java
+++ sdk/scheduler/src/test/java/com/mesosphere/sdk/offer/evaluate/placement/MaxPerHostnameRuleTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.Collections;
+import javax.validation.ConstraintViolationException;
 
 /**
  * Tests for {@link MaxPerHostnameRule}.
