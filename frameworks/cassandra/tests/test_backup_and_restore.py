cabf012d4c09765a0e82208499aa0e2a72ac2ecd
Ben Wood <ben@benclarkwood.com>
2017-11-03 18:02:27 -0700

    Don't set user=root when running in strict (#1802)

--- frameworks/cassandra/tests/test_backup_and_restore.py
+++ frameworks/cassandra/tests/test_backup_and_restore.py
@@ -22,11 +22,16 @@ def configure_package(configure_security):
         test_jobs = config.get_all_jobs(node_address=config.get_foldered_node_address())
         sdk_install.uninstall(foldered_service_name, package_name=config.PACKAGE_NAME)
         # user=root because Azure CLI needs to run in root...
+        # We don't run the Azure tests in strict however, so don't set it then.
+        if os.environ.get("SECURITY") == "strict":
+            additional_options={"service": { "name": config.get_foldered_service_name()} }
+        else:
+            additional_options={"service": { "name": config.get_foldered_service_name(), "user": "root" } }
+
         sdk_install.install(
             config.PACKAGE_NAME,
             config.DEFAULT_TASK_COUNT,
-            service_name=foldered_service_name,
-            additional_options={"service": {"name": foldered_service_name, "user": "root"}})
+            additional_options=additional_options)
 
         tmp_dir = tempfile.mkdtemp(prefix='cassandra-test')
         for job in test_jobs:
