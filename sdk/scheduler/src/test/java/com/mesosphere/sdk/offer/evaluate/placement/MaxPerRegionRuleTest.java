eabdeaa117b57a4df14cded2da3af787a3f8dd62
Gabriel Hartmann <gabriel@mesosphere.io>
2017-11-08 16:46:23 -0800

    Add more MaxPerRegionRule tests to complete code coverage

--- sdk/scheduler/src/test/java/com/mesosphere/sdk/offer/evaluate/placement/MaxPerRegionRuleTest.java
+++ sdk/scheduler/src/test/java/com/mesosphere/sdk/offer/evaluate/placement/MaxPerRegionRuleTest.java
@@ -1,10 +1,15 @@
 package com.mesosphere.sdk.offer.evaluate.placement;
 
 import com.mesosphere.sdk.config.SerializationUtils;
+import com.mesosphere.sdk.offer.taskdata.TaskLabelWriter;
+import com.mesosphere.sdk.testutils.OfferTestUtils;
+import com.mesosphere.sdk.testutils.TestConstants;
+import org.apache.mesos.Protos;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Collection;
 
 /**
  * This class tests the {@link MaxPerRegionRule} class.
@@ -33,5 +38,47 @@ public class MaxPerRegionRuleTest {
                 str,
                 PlacementRule.class,
                 TestPlacementUtils.OBJECT_MAPPER);
+    }
+
+    @Test
+    public void getEmptyOfferKeys() {
+        MaxPerRegionRule rule = new MaxPerRegionRule(2);
+        Protos.Offer offer = OfferTestUtils.getEmptyOfferBuilder().build();
+        Assert.assertTrue(rule.getKeys(offer).isEmpty());
+    }
+
+    @Test
+    public void getRegionOfferKeys() {
+        MaxPerRegionRule rule = new MaxPerRegionRule(2);
+        Protos.Offer offer = OfferTestUtils.getEmptyOfferBuilder()
+                .setDomain(TestConstants.DOMAIN_INFO)
+                .build();
+        Collection<String> keys = rule.getKeys(offer);
+        Assert.assertEquals(1, keys.size());
+        Assert.assertEquals(
+                TestConstants.DOMAIN_INFO.getFaultDomain().getRegion().getName(),
+                keys.stream().findFirst().get());
+    }
+
+    @Test
+    public void getEmptyTaskKeys() {
+        MaxPerRegionRule rule = new MaxPerRegionRule(2);
+        Assert.assertTrue(rule.getKeys(TestConstants.TASK_INFO).isEmpty());
+    }
+
+    @Test
+    public void getRegionTaskKeys() {
+        MaxPerRegionRule rule = new MaxPerRegionRule(2);
+        Protos.TaskInfo taskInfo = TestConstants.TASK_INFO.toBuilder()
+                .setLabels(
+                        new TaskLabelWriter(TestConstants.TASK_INFO)
+                                .setRegion(TestConstants.DOMAIN_INFO.getFaultDomain().getRegion())
+                                .toProto())
+                .build();
+        Collection<String> keys = rule.getKeys(taskInfo);
+        Assert.assertEquals(1, keys.size());
+        Assert.assertEquals(
+                TestConstants.DOMAIN_INFO.getFaultDomain().getRegion().getName(),
+                keys.stream().findFirst().get());
     }
 }
