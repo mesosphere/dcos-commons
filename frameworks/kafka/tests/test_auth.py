fd4ba63092dcb28bf70e749fae24678d177bb7f4
Evan Lezar <evan.lezar@zalando.de>
2017-11-14 18:11:31 +0100

    Handle tuples in process output

--- frameworks/kafka/tests/test_auth.py
+++ frameworks/kafka/tests/test_auth.py
@@ -129,11 +129,11 @@ def test_client_can_read_and_write(kafka_client):
         producer_output = sdk_tasks.task_exec(kafka_client, ' '.join(producer_cmd))
         log.info("Producer output(%s): %s", i, producer_output)
 
-    assert "Sent message: '{message}'".format(message=str(message)) in producer_output
+    assert "Sent message: '{message}'".format(message=str(message)) in ' '.join(str(p) for p in producer_output)
 
     consumer_cmd = ['/tmp/kafkaconfig/start.sh', 'consumer', 'single']
     log.info("Running %s", consumer_cmd)
     consumer_output = sdk_tasks.task_exec(kafka_client,  ' '.join(consumer_cmd))
     log.info("Consumer output: %s", consumer_output)
 
-    assert str(message) in consumer_output
+    assert str(message) in ' '.join(str(c) for c in consumer_output)
