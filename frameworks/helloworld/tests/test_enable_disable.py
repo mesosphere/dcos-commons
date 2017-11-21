6ddcd4efc6e4676cf21109f86ea00a0720bd48c9
Gabriel Hartmann <gabriel@mesosphere.io>
2017-11-21 12:44:43 -0800

    Fix enable/disable integration test

--- frameworks/helloworld/tests/test_enable_disable.py
+++ frameworks/helloworld/tests/test_enable_disable.py
@@ -26,10 +26,10 @@ def configure_package(configure_security):
 def test_disable():
     sdk_plan.wait_for_completed_deployment(config.SERVICE_NAME)
     sdk_plan.recovery_plan_is_empty(config.SERVICE_NAME)
-    sdk_tasks.check_running(config.SERVICE_NAME, 6)
+    sdk_tasks.check_running(config.SERVICE_NAME, 6, timeout_seconds=30, allow_more=False)
     set_test_boolean('false')
     sdk_plan.wait_for_completed_deployment(config.SERVICE_NAME)
-    sdk_tasks.check_running(config.SERVICE_NAME, 3)
+    sdk_tasks.check_running(config.SERVICE_NAME, 3, timeout_seconds=30, allow_more=False)
     sdk_plan.recovery_plan_is_empty(config.SERVICE_NAME)
 
 
@@ -37,10 +37,10 @@ def test_disable():
 def test_enable():
     sdk_plan.wait_for_completed_deployment(config.SERVICE_NAME)
     sdk_plan.recovery_plan_is_empty(config.SERVICE_NAME)
-    sdk_tasks.check_running(config.SERVICE_NAME, 3)
+    sdk_tasks.check_running(config.SERVICE_NAME, 3, timeout_seconds=30, allow_more=False)
     set_test_boolean('true')
     sdk_plan.wait_for_completed_deployment(config.SERVICE_NAME)
-    sdk_tasks.check_running(config.SERVICE_NAME, 6)
+    sdk_tasks.check_running(config.SERVICE_NAME, 6, timeout_seconds=30, allow_more=False)
     sdk_plan.recovery_plan_is_empty(config.SERVICE_NAME)
 
 
