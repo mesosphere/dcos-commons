ab8114cfd633fdc58b45cd4c9c642b5ca0f1e713
Evan Lezar <evan.lezar@zalando.de>
2017-12-05 21:47:59 -0800

    Move common kerberos funcitons to auth.py

--- frameworks/kafka/tests/test_kerberos_auth.py
+++ frameworks/kafka/tests/test_kerberos_auth.py
@@ -1,5 +1,6 @@
 import logging
 import pytest
+import uuid
 
 import sdk_auth
 import sdk_cmd
@@ -10,7 +11,6 @@ import sdk_utils
 
 from tests import auth
 from tests import config
-from tests import test_utils
 
 log = logging.getLogger(__name__)
 
@@ -33,7 +33,14 @@ def kafka_principals():
             domain=fqdn,
             realm=sdk_auth.REALM))
 
-    principals.append("client@{realm}".format(realm=sdk_auth.REALM))
+    clients = [
+        "client",
+        "authorized",
+        "unauthorized",
+        "super"
+    ]
+    for c in clients:
+        principals.append("{client}@{realm}".format(client=c, realm=sdk_auth.REALM))
 
     yield principals
 
@@ -146,12 +153,14 @@ def kafka_client(kerberos, kafka_server):
 @sdk_utils.dcos_ee_only
 @pytest.mark.sanity
 def test_client_can_read_and_write(kafka_client, kafka_server):
+    client_id = kafka_client["id"]
+
     auth.wait_for_brokers(kafka_client["id"], kafka_client["brokers"])
 
-    topicname = kafka_client["env"]["KAFKA_TOPIC"]
-    sdk_cmd.svc_cli(kafka_server["package_name"], kafka_server["service"]["name"],
-                    "topic create {}".format(topicname),
-                    json=True)
-    test_utils.wait_for_topic(kafka_server, topicname)
+    topic_name = "authn.test"
 
-    auth.send_and_receive_message(kafka_client["id"])
+    message = str(uuid.uuid4())
+
+    assert ">>" in auth.write_to_topic("client", client_id, topic_name, message)
+
+    assert message in auth.read_from_topic("client", client_id, topic_name, 1)
