c2ac83bf8f6d81d6d558ed6208983af5f1463460
benclarkwood <ben@benclarkwood.com>
2017-12-12 14:00:00 -0800

    Upstream change from zk repo

--- testing/sdk_api.py
+++ testing/sdk_api.py
@@ -12,17 +12,29 @@ import shakedown
 log = logging.getLogger(__name__)
 
 
-def get(service_name, endpoint):
+def get(service_name, endpoint, **kwargs):
     '''
     :param endpoint: endpoint of the form /v1/...
     :type endpoint: str
     :returns: JSON response from the provided scheduler API endpoint
     :rtype: Response
     '''
+
+    # Some of our APIs (cough looking at you plans)
+    # will return a meaningful 417.
+    def is_success(status_code):
+        return (200 <= status_code < 300) or status_code == 417
+
+
     response = dcos.http.get("{}{}".format(
         shakedown.dcos_service_url(service_name),
-        endpoint))
-    response.raise_for_status()
+        endpoint),
+        is_success=is_success)
+
+    # 417 == plan error
+    if response.status_code != 417:
+        response.raise_for_status()
+
     return response
 
 
