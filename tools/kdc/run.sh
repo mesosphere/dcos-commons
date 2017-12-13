b5c9830df029579a7f3303ce68c6fdde328b9c6f
NimaVaziri <nvaziri@mesosphere.io>
2017-12-13 14:47:51 -0800

    Cleanup and make strict work

--- tools/kdc/run.sh
+++ tools/kdc/run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -e
 
-sed -e "s/{{LOG_LEVEL}}/$LOG_LEVEL/g" -i /etc/heimdal-kdc/kdc.conf
-sed -e "s/{{ENC_TYPES}}/$ENC_TYPES/g" -i /etc/heimdal-kdc/kdc.conf
-
-nohup python -m SimpleHTTPServer 8000 &
-
 echo
 echo KDC LOGS
 echo ========
