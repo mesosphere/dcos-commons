4948b63e622e17805f6b07aac7cb0d591a6e854a
nick <nick@mesosphere.io>
2017-12-22 17:08:21 +0100

    Clarify log

--- testing/sdk_install.py
+++ testing/sdk_install.py
@@ -96,7 +96,7 @@ def install(
     if wait_for_deployment:
         # this can take a while, default is 15 minutes. for example with HDFS, we can hit the expected
         # total task count via FINISHED tasks, without actually completing deployment
-        log.info('Waiting for {}/{} to finish deployment plan...'.format(
+        log.info('Waiting for package={} service={} to finish deployment plan...'.format(
             package_name, service_name))
         sdk_plan.wait_for_completed_deployment(service_name, timeout_seconds)
 
