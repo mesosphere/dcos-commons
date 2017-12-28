feb62c6c590f5554e41a746243cef26030078de2
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2017-12-27 17:39:09 -0800

    Fix more errors

--- frameworks/elastic/tests/test_overlay.py
+++ frameworks/elastic/tests/test_overlay.py
@@ -54,13 +54,12 @@ def test_service_health():
     stop_max_delay=config.DEFAULT_ELASTIC_TIMEOUT*1000,
     retry_on_result=lambda res: not res)
 def test_indexing(default_populated_index):
-    def fun():
-        indices_stats = config.get_elasticsearch_indices_stats(config.DEFAULT_INDEX_NAME)
-        observed_count = indices_stats["_all"]["primaries"]["docs"]["count"]
-        assert observed_count == 1, "Indices has incorrect count: should be 1, got {}".format(observed_count)
-        doc = config.get_document(config.DEFAULT_INDEX_NAME, config.DEFAULT_INDEX_TYPE, 1)
-        observed_name = doc["_source"]["name"]
-        return observed_name == "Loren"
+    indices_stats = config.get_elasticsearch_indices_stats(config.DEFAULT_INDEX_NAME)
+    observed_count = indices_stats["_all"]["primaries"]["docs"]["count"]
+    assert observed_count == 1, "Indices has incorrect count: should be 1, got {}".format(observed_count)
+    doc = config.get_document(config.DEFAULT_INDEX_NAME, config.DEFAULT_INDEX_TYPE, 1)
+    observed_name = doc["_source"]["name"]
+    return observed_name == "Loren"
 
 
 @pytest.mark.sanity
