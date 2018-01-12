8e1edb1c825068d3f1a5b3d5ac6b2bd7fcabbfdf
Michael Browning <mrbrowning@distopia.net>
2018-01-12 10:36:00 -0800

    Reenable resource refinement tests in strict mode (#2064)
    * Renable resource refinement tests in strict mode
    
    * Also setup permissions for slave_public parent role in strict
    
    * url encode role name

--- testing/sdk_security.py
+++ testing/sdk_security.py
@@ -210,6 +210,11 @@ def setup_security(framework_name: str) -> None:
     )
     grant_permissions(
         linux_user='nobody',
+        role_name='slave_public%252F{}-role'.format(framework_name),
+        service_account_name='service-acct'
+    )
+    grant_permissions(
+        linux_user='nobody',
         role_name='test__integration__{}-role'.format(framework_name),
         service_account_name='service-acct'
     )
