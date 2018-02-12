44bb5fd4cf1a1d7e4fbd34e90ecb074d441d1e26
Evan Lezar <elezar@mesosphere.com>
2018-02-12 20:44:25 +0100

    [INFINITY-3111] Gracefully handle invalid placement constraints (#2221)
    This PR adds better validation support for placement constraints. Invalid constraints are wrapped in a placement rule and a validator is added to check for this.
    
    This means that:
    * If the initial deployment has an invalid placement constraint, the scheduler does not crash and one can successfully uninstall it.
    * If a config update introduces an invalid constraint, deployment plan has an error as with any other invalid config change.
    
    Example output of a failed update:
    ```
    deploy (serial strategy) (ERROR)
    ├─ hello (serial strategy) (COMPLETE)
    │  └─ hello-0:[server] (COMPLETE)
    └─ world (serial strategy) (COMPLETE)
       ├─ world-0:[server] (COMPLETE)
       └─ world-1:[server] (COMPLETE)
    
    Errors:
    - Field: 'PlacementRule'; Value: 'AndRule{rules=[IsLocalRegionRule, InvalidPlacementRule{constraints=[[\"hostname\", "UNIQUE"]], exception=Invalid number of entries in rule. Expected 2 or 3, got 1: [[[\"hostname\"]}]}'; Message: 'The PlacementRule for PodSpec 'hello' had invalid constraints'
    ```

---
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/config/validate/PlacementRuleIsValid.java
@@ -0,0 +1,55 @@
+package com.mesosphere.sdk.config.validate;
+
+import com.mesosphere.sdk.offer.evaluate.placement.AndRule;
+import com.mesosphere.sdk.offer.evaluate.placement.InvalidPlacementRule;
+import com.mesosphere.sdk.offer.evaluate.placement.OrRule;
+import com.mesosphere.sdk.offer.evaluate.placement.PlacementRule;
+import com.mesosphere.sdk.specification.PodSpec;
+import com.mesosphere.sdk.specification.ServiceSpec;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+/**
+ * A {@link ConfigValidator} that checks for valid placement constraints.
+ */
+public class PlacementRuleIsValid implements ConfigValidator<ServiceSpec> {
+    @Override
+    public Collection<ConfigValidationError> validate(Optional<ServiceSpec> oldConfig, ServiceSpec newConfig) {
+        List<PodSpec> podSpecs = newConfig.getPods().stream()
+                .filter(podSpec -> podSpec.getPlacementRule().isPresent())
+                .collect(Collectors.toList());
+
+        List<ConfigValidationError> errors = new ArrayList<>();
+        for (final PodSpec podSpec : podSpecs) {
+            PlacementRule placementRule = podSpec.getPlacementRule().get();
+
+            if (!isValid(placementRule)) {
+                String errMsg = String.format(
+                        "The PlacementRule for PodSpec '%s' had invalid constraints",
+                        podSpec.getType());
+                errors.add(ConfigValidationError.valueError("PlacementRule", placementRule.toString(), errMsg));
+            }
+        }
+
+        return errors;
+    }
+
+    /**
+     * A placement rule is valid none of its children are invalid and it is valid.
+     */
+    private boolean isValid(final PlacementRule rule) {
+        if (rule instanceof OrRule) {
+            return ((OrRule) rule).getRules().stream().allMatch(r -> isValid(r));
+        } else if (rule instanceof AndRule) {
+            return ((AndRule) rule).getRules().stream().allMatch(r -> isValid(r));
+        } else {
+            return !(rule instanceof InvalidPlacementRule);
+        }
+    }
+
+}
