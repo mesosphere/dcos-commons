9c741f7de90e28b5cdfb45b248929de26483e612
Ben Wood <ben@benclarkwood.com>
2018-02-12 23:46:07 -0800

    [INFINITY-3111] Gracefully handle invalid placement constraints (#2221) (#2254)
    This PR adds better validation support for placement constraints. Invalid constraints are wrapped in a placement rule and a validator is added to check for this.
    
    This means that:
    * If the initial deployment has an invalid placement constraint, the scheduler does not crash and one can successfully uninstall it.
    * If a config update introduces an invalid constraint, deployment plan has an error as with any other invalid config change.
    
    Example output of a failed update:
    ```
    deploy (serial strategy) (ERROR)
    ├─ hello (serial strategy) (COMPLETE)
    │  └─ hello-0:[server] (COMPLETE)
    └─ world (serial strategy) (COMPLETE)
       ├─ world-0:[server] (COMPLETE)
       └─ world-1:[server] (COMPLETE)
    
    Errors:
    - Field: 'PlacementRule'; Value: 'AndRule{rules=[IsLocalRegionRule, InvalidPlacementRule{constraints=[[\"hostname\", "UNIQUE"]], exception=Invalid number of entries in rule. Expected 2 or 3, got 1: [[[\"hostname\"]}]}'; Message: 'The PlacementRule for PodSpec 'hello' had invalid constraints'
    ```

---
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/offer/evaluate/placement/InvalidPlacementRule.java
@@ -0,0 +1,66 @@
+package com.mesosphere.sdk.offer.evaluate.placement;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mesosphere.sdk.offer.evaluate.EvaluationOutcome;
+import com.mesosphere.sdk.specification.PodInstance;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.mesos.Protos.Offer;
+import org.apache.mesos.Protos.TaskInfo;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * An implementation of a {@PlacementRule} that is ALWAYS invalid.
+ */
+public class InvalidPlacementRule implements PlacementRule {
+    private final String constraints;
+    private final String exception;
+
+    @JsonCreator
+    public InvalidPlacementRule(
+            @JsonProperty("constraints ") String constraints,
+            @JsonProperty("exception") String exception) {
+        this.constraints = constraints;
+        this.exception = exception;
+    }
+
+    @Override
+    public EvaluationOutcome filter(Offer offer, PodInstance podInstance, Collection<TaskInfo> tasks) {
+        return EvaluationOutcome.fail(this,
+                String.format("Invalid placement constraints for %s: %s", podInstance.getName(), constraints)).build();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("InvalidPlacementRule{constraints=%s, exception=%s}", constraints, exception);
+    }
+
+    @Override
+    public Collection<PlacementField> getPlacementFields() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    @JsonProperty("constraints")
+    public String getConstraints() {
+        return constraints;
+    }
+
+    @JsonProperty("exception")
+    public String getException() {
+        return exception;
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+}
