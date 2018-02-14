d20edb64a5e3648142c2901623fab163323c586e
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2018-02-13 18:31:33 -0800

    Add fixture to mount volumes

--- frameworks/helloworld/tests/test_mount_volumes.py
+++ frameworks/helloworld/tests/test_mount_volumes.py
@@ -1,6 +1,8 @@
+import json
 import logging
+import os
+import time
 
-import os.path
 import pytest
 
 import sdk_cmd
@@ -8,10 +10,56 @@ import sdk_hosts
 import sdk_install
 import sdk_plan
 import sdk_tasks
-import sdk_utils
 from tests import config
 
+import dcos_launch
+import dcos_test_utils
+
 log = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope='session')
+def mount_volumes():
+    script = """
+#!/bin/bash
+sudo systemctl stop dcos-mesos-slave.service
+sudo rm -f /var/lib/dcos/mesos-resources
+sudo rm -f /var/lib/mesos/slave/meta/slaves/latest
+"""
+    for i in range(2):
+        script += """
+sudo mkdir -p /dcos/volume{idx}
+sudo dd if=/dev/zero of=/root/volume{idx}.img bs=1M count={size}
+sudo losetup /dev/loop{idx} /root/volume{idx}.img
+sudo mkfs -t ext4 /dev/loop{idx}
+sudo losetup -d /dev/loop{idx}
+echo "/root/volume{idx}.img /dcos/volume{idx} auto loop 0 2" | sudo tee -a /etc/fstab
+sudo mount /dcos/volume{idx}
+""".format(idx=i, size=200)
+
+    script += """
+sudo systemctl restart dcos-mesos-slave.service
+"""
+
+    cluster_info_path = os.getenv('CLUSTER_INFO_PATH', 'cluster_info.json')
+    if not os.path.exists(cluster_info_path):
+        assert False, 'No cluster info to work with!!'
+    cluster_info_json = json.load(open(cluster_info_path))
+    launcher = dcos_launch.get_launcher(cluster_info_json)
+    description = launcher.describe()
+    ssh_client = launcher.get_ssh_client()
+    with ssh_client.tunnel(description['masters'][0]['public_ip']) as t:
+        t.copy_file(dcos_test_utils.helpers.session_tempfile(ssh_client.key), 'ssh_key')
+        t.copy_file(dcos_test_utils.helpers.session_tempfile(script), 'volume_script.sh')
+        t.command(['chmod', '600', 'ssh_key'])
+        ssh_command = ['ssh', '-i', 'ssh_key'] + dcos_test_utils.ssh_client.SHARED_SSH_OPTS
+        scp_command = ['scp', '-i', 'ssh_key'] + dcos_test_utils.ssh_client.SHARED_SSH_OPTS
+        for private_agent in description['private_agents']:
+            target = '{}@{}'.format(ssh_client.user, private_agent['private_ip'])
+            t.command(scp_command + ['volume_script.sh', target + ':~/volume_script.sh'])
+            t.command(ssh_command + [target, 'bash', 'volume_script.sh'])
+        # nasty hack until we add a better post-flight
+        time.sleep(60)
 
 
 @pytest.fixture(scope='module', autouse=True)
@@ -32,7 +80,7 @@ def configure_package(configure_security):
 
 
 @pytest.mark.sanity
-def test_kill_node():
+def test_kill_node(mount_volumes):
     '''kill the node task, verify that the node task is relaunched against the same executor as before'''
     verify_shared_executor('hello-0')
 
@@ -54,7 +102,7 @@ def test_kill_node():
 
 
 @pytest.mark.sanity
-def test_kill_agent():
+def test_kill_agent(mount_volumes):
     '''kill the agent task, verify that the agent task is relaunched against the same executor as before'''
     verify_shared_executor('hello-0')
 
