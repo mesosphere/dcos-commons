7a3f36bdb515fd5a6c9b98d18a129a76a6540d76
Evan Lezar <elezar@mesosphere.com>
2018-02-20 17:52:53 +0100

    Update test-runner.sh from dse-private

--- test-runner.sh
+++ test-runner.sh
@@ -1,15 +1,5 @@
-#!/usr/bin/env bash
-
-# Build a framework, package, upload it, and then run its integration tests.
-# (Or all frameworks depending on arguments.) Expected to be called by test.sh
-
 # Exit immediately on errors
 set -e -x
-
-# Remove the DC/OS cluster
-cleanup() {
-    dcos-launch delete
-}
 
 # Export the required environment variables:
 export DCOS_ENTERPRISE
@@ -19,7 +9,14 @@ export SECURITY
 
 REPO_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ "$FRAMEWORK" = "all" ]; then
+# Determine the list of frameworks if it is not specified
+if [ -z "${FRAMEWORK}" -o x"${AUTO_DETECT_FRAMEWORKS}" == x"True" ]; then
+    if [ -d "$REPO_ROOT_DIR/frameworks" ]; then
+        FRAMEWORK_LIST=$(ls $REPO_ROOT_DIR/frameworks)
+    else
+        FRAMEWORK_LIST=$(basename ${REPO_ROOT_DIR})
+    fi
+elif [ "$FRAMEWORK" = "all" ]; then
     if [ -n "$STUB_UNIVERSE_URL" ]; then
         echo "Cannot set \$STUB_UNIVERSE_URL when building all frameworks"
         exit 1
@@ -30,7 +27,70 @@ else
     FRAMEWORK_LIST=$FRAMEWORK
 fi
 
-echo "Beginning integration tests at "`date`
+# First we need to build the framework(s)
+echo "Using FRAMEWORK_LIST: ${FRAMEWORK_LIST}"
+
+if [ -n "$STUB_UNIVERSE_URL" ]; then
+    echo "Using provided STUB_UNIVERSE_URL: $STUB_UNIVERSE_URL"
+else
+    for framework in $FRAMEWORK_LIST; do
+        echo "STARTING: $framework"
+        FRAMEWORK_DIR=$REPO_ROOT_DIR/frameworks/${framework}
+
+        if [ ! -d ${FRAMEWORK_DIR} -a "${FRAMEWORK}" != "all" ]; then
+            echo "FRAMEWORK_DIR=${FRAMEWORK_DIR} does not exist."
+            echo "Assuming single framework in ${REPO_ROOT}."
+            FRAMEWORK_DIR=${REPO_ROOT_DIR}
+        fi
+
+        echo "Starting build for $framework at "`date`
+        export UNIVERSE_URL_PATH=${FRAMEWORK_DIR}/${framework}-universe-url
+        ${FRAMEWORK_DIR}/build.sh aws
+        if [ ! -f "$UNIVERSE_URL_PATH" ]; then
+            echo "Missing universe URL file: $UNIVERSE_URL_PATH"
+            exit 1
+        fi
+        if [ -z ${STUB_UNIVERSE_LIST} ]; then
+            STUB_UNIVERSE_LIST=$(cat ${UNIVERSE_URL_PATH})
+        else
+            STUB_UNIVERSE_LIST="${STUB_UNIVERSE_LIST},$(cat ${UNIVERSE_URL_PATH})"
+        fi
+        echo "Finished build for $framework at "`date`
+    done
+    export STUB_UNIVERSE_URL=${STUB_UNIVERSE_LIST}
+    echo "Using STUB_UNIVERSE_URL: $STUB_UNIVERSE_URL"
+fi
+
+# Ensure that the ssh-agent is running:
+eval "$(ssh-agent -s)"
+if [ -f /ssh/key ]; then
+    ssh-add /ssh/key
+fi
+
+# Now create a cluster if it doesn't exist.
+if [ -z "$CLUSTER_URL" ]; then
+    echo "No DC/OS cluster specified. Attempting to create one now"
+    # TODO(elezar) Find the best way to create the config.yaml
+    dcos-launch create -c /build/config.yaml
+    dcos-launch wait
+
+    export CLUSTER_URL=https://`dcos-launch describe | jq -r .masters[0].public_ip`
+    CLUSTER_WAS_CREATED=True
+fi
+
+echo "Configuring dcoscli for cluster: $CLUSTER_URL"
+echo "\tDCOS_ENTERPRISE=$DCOS_ENTERPRISE"
+/build/tools/dcos_login.py
+
+if [ -f cluster_info.json ]; then
+    if [ `cat cluster_info.json | jq .key_helper` == 'true' ]; then
+        cat cluster_info.json | jq -r .ssh_private_key > /root/.ssh/id_rsa
+        chmod 600 /root/.ssh/id_rsa
+        ssh-add /root/.ssh/id_rsa
+    fi
+fi
+
+# Now run the tests:
 
 # Strip the quotes from the -k and -m options to pytest
 PYTEST_K_FROM_PYTEST_ARGS=`echo "$PYTEST_ARGS" \
@@ -77,74 +137,35 @@ if [ -n "$PYTEST_ARGS" ]; then
     pytest_args+=($PYTEST_ARGS)
 fi
 
-if [ -f /ssh/key ]; then
-    eval "$(ssh-agent -s)"
-    ssh-add /ssh/key
+# First in the root.
+if [ -d ${REPO_ROOT_DIR}/tests ]; then
+    FRAMEWORK_TESTS_DIR=${REPO_ROOT_DIR}/tests
+    echo "Starting test for $FRAMEWORK_TESTS_DIR at "`date`
+    py.test -vv -s "${pytest_args[@]}" ${FRAMEWORK_TESTS_DIR}
+    exit_code=$?
+    echo "Finished test for $FRAMEWORK_TESTS_DIR at "`date`
 fi
 
 for framework in $FRAMEWORK_LIST; do
-    echo "STARTING: $framework"
+    echo "Checking framework ${framework}"
     FRAMEWORK_DIR=$REPO_ROOT_DIR/frameworks/${framework}
-
-    if [ ! -d ${FRAMEWORK_DIR} -a "${FRAMEWORK}" != "all" ]; then
-        echo "FRAMEWORK_DIR=${FRAMEWORK_DIR} does not exist."
-        echo "Assuming single framework in ${REPO_ROOT}."
-        FRAMEWORK_DIR=${REPO_ROOT_DIR}
-    fi
-
-    if [ -z "$STUB_UNIVERSE_URL" ]; then
-        echo "Starting build for $framework at "`date`
-        export UNIVERSE_URL_PATH=${FRAMEWORK_DIR}/${framework}-universe-url
-        ${FRAMEWORK_DIR}/build.sh aws
-        if [ ! -f "$UNIVERSE_URL_PATH" ]; then
-            echo "Missing universe URL file: $UNIVERSE_URL_PATH"
-            exit 1
-        fi
-        export STUB_UNIVERSE_URL=$(cat $UNIVERSE_URL_PATH)
-        echo "Finished build for $framework at "`date`
+    FRAMEWORK_TESTS_DIR=${FRAMEWORK_DIR}/tests
+    if [ ! -d ${FRAMEWORK_TESTS_DIR} ]; then
+        echo "No tests found for ${framework} at ${FRAMEWORK_TESTS_DIR}"
     else
-        echo "Using provided STUB_UNIVERSE_URL: $STUB_UNIVERSE_URL"
-    fi
-
-    if [ -z "$CLUSTER_URL" ]; then
-
-        echo "No DC/OS cluster specified. Attempting to create one now"
-        dcos-launch create -c /build/config.yaml
-
-        # enable the trap to ensure cleanup
-        trap cleanup ERR
-
-        dcos-launch wait
-
-        # configure the dcos-cli/shakedown backend
-        export CLUSTER_URL=https://`dcos-launch describe | jq -r .masters[0].public_ip`
-        CLUSTER_WAS_CREATED=True
-    fi
-
-    echo "Configuring dcoscli for cluster: $CLUSTER_URL"
-    echo "\tDCOS_ENTERPRISE=$DCOS_ENTERPRISE"
-    /build/tools/dcos_login.py
-
-    if [ -f cluster_info.json ]; then
-        if [ `cat cluster_info.json | jq .key_helper` == 'true' ]; then
-            cat cluster_info.json | jq -r .ssh_private_key > /root/.ssh/id_rsa
-            chmod 600 /root/.ssh/id_rsa
-        fi
-    fi
-
-    echo "Starting test for $framework at "`date`
-    py.test -vv -s "${pytest_args[@]}" ${FRAMEWORK_DIR}/tests
-    exit_code=$?
-    echo "Finished test for $framework at "`date`
-
-    set +e
-    if [ -n "$CLUSTER_WAS_CREATED" ]; then
-        echo "Deleting cluster $CLUSTER_URL"
-        dcos-launch delete
-        unset CLUSTER_URL
+        echo "Starting test for $FRAMEWORK_TESTS_DIR at "`date`
+        py.test -vv -s "${pytest_args[@]}" ${FRAMEWORK_TESTS_DIR}
+        exit_code=$?
+        echo "Finished test for $FRAMEWORK_TESTS_DIR at "`date`
     fi
 done
 
 echo "Finished integration tests at "`date`
+
+if [ -n "$CLUSTER_WAS_CREATED" ]; then
+    echo "The DC/OS cluster $CLUSTER_URL was created. Please run"
+    echo "\t\$ dcos-launch delete"
+    echo "to remove the cluster."
+fi
 
 exit $exit_code
