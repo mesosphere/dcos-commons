7417fb3a44da73a338835d7d631c36c6a45bdea3
benclarkwood <ben@benclarkwood.com>
2018-03-09 15:39:32 -0800

    Remove errant "ben" pytest mark

--- frameworks/helloworld/tests/test_multistep_plan.py
+++ frameworks/helloworld/tests/test_multistep_plan.py
@@ -29,7 +29,6 @@ def configure_package(configure_security):
 @pytest.mark.sanity
 @pytest.mark.smoke
 @pytest.mark.config_update
-@pytest.mark.ben
 def test_bump_hello_cpus():
     def close_enough(val0, val1):
         epsilon = 0.00001
