5cec881e34a9fb689f651f3fc3a2d8a1e64ae482
Michael Browning <mrbrowning@distopia.net>
2018-03-19 15:44:17 -0700

    set region awareness envvar to true when region is specified

--- frameworks/cassandra/src/test/java/com/mesosphere/sdk/cassandra/scheduler/ServiceTest.java
+++ frameworks/cassandra/src/test/java/com/mesosphere/sdk/cassandra/scheduler/ServiceTest.java
@@ -65,6 +65,7 @@ public class ServiceTest {
                 .setPodEnv("node", getDefaultNodeEnv())
                 .run();
         Assert.assertEquals(result.getSchedulerEnvironment().get("SERVICE_REGION"), "Europe");
+        Assert.assertEquals(result.getSchedulerEnvironment().get("ALLOW_REGION_AWARENESS"), "true");
     }
 
     private static Map<String, String> getDefaultNodeEnv() {
