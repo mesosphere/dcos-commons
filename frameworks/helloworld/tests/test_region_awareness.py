fb3ce632bb4935d92dcc6ec695729e1d50089635
Michael Browning <mrbrowning@distopia.net>
2018-03-21 04:24:23 -0700

    Ensure service does not remain installed after multi-region tests (#2396)
    * Ensure service does not remain installed after multi-region tests
    
    * Use pytest fixture
    
    * Wrap install in try/finally block in fixture
    
    * Add allow_region_awareness config var
    
    * Address comments
    
    * Wait for in-progress recovery

--- frameworks/helloworld/tests/test_region_awareness.py
+++ frameworks/helloworld/tests/test_region_awareness.py
@@ -12,87 +12,96 @@ from tests import config
 log = logging.getLogger(__name__)
 
 POD_NAMES = ['hello-0', 'world-0', 'world-1']
+LOCAL_REGION = 'USA'
+REMOTE_REGION = 'Europe'
+
+
+@pytest.fixture
+def local_service():
+    try:
+        sdk_install.install(
+            config.PACKAGE_NAME,
+            config.SERVICE_NAME,
+            3,
+            additional_options={
+                "service": {
+                    "scenario": "MULTI_REGION",
+                    "allow_region_awareness": True
+                }
+            })
+
+        yield
+    finally:
+        sdk_install.uninstall(config.PACKAGE_NAME, config.SERVICE_NAME)
+
+
+@pytest.fixture
+def remote_service():
+    try:
+        sdk_install.install(
+            config.PACKAGE_NAME,
+            config.SERVICE_NAME,
+            3,
+            additional_options={
+                "service": {
+                    "scenario": "MULTI_REGION", 
+                    "allow_region_awareness": "true",
+                    "region": REMOTE_REGION
+                }
+            })
+
+        yield
+    finally:
+        sdk_install.uninstall(config.PACKAGE_NAME, config.SERVICE_NAME)
 
 
 @pytest.mark.dcos_min_version('1.11')
 @pytest.mark.region_awareness
 @sdk_utils.dcos_ee_only
-def test_nodes_deploy_to_local_region_by_default():
-    sdk_install.install(
-        config.PACKAGE_NAME,
-        config.SERVICE_NAME,
-        3,
-        additional_options={"service": {"scenario": "MULTI_REGION"}})
-
-    sdk_plan.wait_for_completed_deployment(config.SERVICE_NAME)
-
+def test_nodes_deploy_to_local_region_by_default(local_service):
     for pod_name in POD_NAMES:
-        info = sdk_cmd.service_request(
-            'GET', config.SERVICE_NAME, '/v1/pod/{}/info'.format(pod_name)
-        ).json()[0]['info']
+        pod_region = get_pod_region(config.SERVICE_NAME, pod_name)
 
-        assert (
-            [l['value'] for l in info['labels']['labels'] if l['key'] == 'offer_region'][0] == 'USA'
-        )
-    
-    sdk_install.uninstall(config.PACKAGE_NAME, config.SERVICE_NAME)
+        assert pod_region == LOCAL_REGION
 
 
 @pytest.mark.dcos_min_version('1.11')
 @pytest.mark.region_awareness
 @sdk_utils.dcos_ee_only
-def test_nodes_can_deploy_to_remote_region():
-    sdk_install.install(
-        config.PACKAGE_NAME,
-        config.SERVICE_NAME,
-        3,
-        additional_options={"service": {"scenario": "MULTI_REGION", "region": "Europe"}})
-
-    sdk_plan.wait_for_completed_deployment(config.SERVICE_NAME)
-
+def test_nodes_can_deploy_to_remote_region(remote_service):
     for pod_name in POD_NAMES:
-        info = sdk_cmd.service_request(
-            'GET', config.SERVICE_NAME, '/v1/pod/{}/info'.format(pod_name)
-        ).json()[0]['info']
+        pod_region = get_pod_region(config.SERVICE_NAME, pod_name)
 
-        assert (
-            [l['value'] for l in info['labels']['labels'] if l['key'] == 'offer_region'][0] ==
-            'Europe'
-        )
-
-    sdk_install.uninstall(config.PACKAGE_NAME, config.SERVICE_NAME)
+        assert pod_region == REMOTE_REGION
 
 
 @pytest.mark.dcos_min_version('1.11')
 @pytest.mark.region_awareness
 @sdk_utils.dcos_ee_only
-def test_region_config_update_does_not_succeed():
-    sdk_install.install(
-        config.PACKAGE_NAME,
-        config.SERVICE_NAME,
-        3,
-        additional_options={"service": {"scenario": "MULTI_REGION"}})
-
+def test_region_config_update_does_not_succeed(local_service):
     sdk_plan.wait_for_completed_deployment(config.SERVICE_NAME)
-    change_region_config('Europe')
+    change_region_config(REMOTE_REGION)
     plan = sdk_plan.get_deployment_plan(config.SERVICE_NAME)
     assert plan.get('errors', [])
 
     sdk_cmd.svc_cli(config.PACKAGE_NAME, config.SERVICE_NAME, 'pod replace hello-0')
+    sdk_plan.wait_for_in_progress_recovery(config.SERVICE_NAME)
     sdk_plan.wait_for_completed_recovery(config.SERVICE_NAME)
 
-    info = sdk_cmd.service_request(
-        'GET', config.SERVICE_NAME, '/v1/pod/hello-0/info'
-    ).json()[0]['info']
+    pod_region = get_pod_region(config.SERVICE_NAME, 'hello-0')
 
-    assert (
-        [l['value'] for l in info['labels']['labels'] if l['key'] == 'offer_region'][0] == 'USA'
-    )
-
-    sdk_install.uninstall(config.PACKAGE_NAME, config.SERVICE_NAME)
+    assert pod_region == LOCAL_REGION
 
 
 def change_region_config(region_name):
     service_config = sdk_marathon.get_config(config.SERVICE_NAME)
     service_config['env']['SERVICE_REGION'] = region_name
     sdk_marathon.update_app(config.SERVICE_NAME, service_config, wait_for_completed_deployment=False)
+
+
+def get_pod_region(service_name, pod_name):
+    info = sdk_cmd.service_request(
+        'GET', service_name, '/v1/pod/{}/info'.format(pod_name)
+    ).json()[0]['info']
+
+    return [l['value'] for l in info['labels']['labels'] if l['key'] == 'offer_region'][0]
