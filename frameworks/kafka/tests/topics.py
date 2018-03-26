9ea5a4c19353b36eabbfb501ac0af000a7ea315f
Evan Lezar <elezar@mesosphere.com>
2018-03-26 16:28:36 +0200

    Refactor acls script to use get_bash_command.

--- frameworks/kafka/tests/topics.py
+++ frameworks/kafka/tests/topics.py
@@ -2,6 +2,7 @@ import logging
 
 import sdk_cmd
 
+from tests import auth
 
 LOG = logging.getLogger(__name__)
 
@@ -11,21 +12,21 @@ def add_acls(user: str, task: str, topic: str, zookeeper_endpoint: str, env_str=
     Add Porducer and Consumer ACLs for the specifed user and topic
     """
 
-    _add_role_acls("producer", user, task, topic, zookeeper_endpoint, env_str)
-    _add_role_acls("consumer --group=*", user, task, topic, zookeeper_endpoint, env_str)
+    _add_role_acls(["--producer", ], user, task, topic, zookeeper_endpoint, env_str)
+    _add_role_acls(["--consumer", "--group=*"], user, task, topic, zookeeper_endpoint, env_str)
 
 
-def _add_role_acls(role: str, user: str, task: str, topic: str, zookeeper_endpoint: str, env_str=None):
-    cmd = "bash -c \"{setup_env}kafka-acls \
-        --topic {topic_name} \
-        --authorizer-properties zookeeper.connect={zookeeper_endpoint} \
-        --add \
-        --allow-principal User:{user} \
-        --{role}\"".format(setup_env="{}  && ".format(env_str) if env_str else "",
-                                                     topic_name=topic,
-                                                     zookeeper_endpoint=zookeeper_endpoint,
-                                                     user=user,
-                                                     role=role)
+def _add_role_acls(roles: list, user: str, task: str, topic: str, zookeeper_endpoint: str, env_str: str=None):
+
+    cmd_list = ["kafka-acls",
+                "--topic", topic,
+                "--authorizer-properties", "zookeeper.connect={}".format(zookeeper_endpoint),
+                "--add",
+                "--allow-principal", "User:{}".format(user), ]
+    cmd_list.extend(roles)
+
+    cmd = auth.get_bash_command(" ".join(cmd_list), env_str)
+
     LOG.info("Running: %s", cmd)
     output = sdk_cmd.task_exec(task, cmd)
     LOG.info(output)
