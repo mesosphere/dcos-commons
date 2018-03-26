b0b6f89414f4b8f829a1a2587d4f26261c954f9f
Michael Browning <mrbrowning@distopia.net>
2018-03-26 16:16:47 -0700

    Spin on erroneous deployment and reinstate valid deployment

--- frameworks/helloworld/tests/test_region_awareness.py
+++ frameworks/helloworld/tests/test_region_awareness.py
@@ -11,6 +11,7 @@ import sdk_plan
 import sdk_tasks
 import sdk_utils
 from tests import config
+from tests.conftest import configure_universe
 
 log = logging.getLogger(__name__)
 
@@ -61,7 +62,7 @@ def remote_service():
 @pytest.mark.dcos_min_version('1.11')
 @pytest.mark.region_awareness
 @sdk_utils.dcos_ee_only
-def test_nodes_deploy_to_local_region_by_default(local_service):
+def test_nodes_deploy_to_local_region_by_default(configure_universe, local_service):
     for pod_name in POD_NAMES:
         pod_region = get_pod_region(config.SERVICE_NAME, pod_name)
 
@@ -71,7 +72,7 @@ def test_nodes_deploy_to_local_region_by_default(local_service):
 @pytest.mark.dcos_min_version('1.11')
 @pytest.mark.region_awareness
 @sdk_utils.dcos_ee_only
-def test_nodes_can_deploy_to_remote_region(remote_service):
+def test_nodes_can_deploy_to_remote_region(configure_universe, remote_service):
     for pod_name in POD_NAMES:
         pod_region = get_pod_region(config.SERVICE_NAME, pod_name)
 
@@ -81,32 +82,22 @@ def test_nodes_can_deploy_to_remote_region(remote_service):
 @pytest.mark.dcos_min_version('1.11')
 @pytest.mark.region_awareness
 @sdk_utils.dcos_ee_only
-def test_region_config_update_does_not_succeed(local_service):
-    sdk_plan.wait_for_completed_deployment(config.SERVICE_NAME)
+def test_region_config_update_does_not_succeed(configure_universe, local_service):
     change_region_config(REMOTE_REGION)
-    plan = sdk_plan.get_deployment_plan(config.SERVICE_NAME)
+    sdk_plan.wait_for_plan_status(config.SERVICE_NAME, 'deploy', 'ERROR', timeout_seconds=180)
 
-    assert plan.get('errors', [])
+    change_region_config(None)
+    sdk_plan.wait_for_completed_deployment(config.SERVICE_NAME, timeout_seconds=180)
 
 
 def change_region_config(region_name):
-    current_scheduler_id = get_scheduler_task_id()
-
     service_config = sdk_marathon.get_config(config.SERVICE_NAME)
-    service_config['env']['SERVICE_REGION'] = region_name
+    if region_name is None:
+        del service_config['env']['SERVICE_REGION']
+    else:
+        service_config['env']['SERVICE_REGION'] = region_name
+
     sdk_marathon.update_app(config.SERVICE_NAME, service_config, wait_for_completed_deployment=False)
-    sdk_tasks.check_task_relaunched(config.SERVICE_NAME, current_scheduler_id)
-
-
-def get_scheduler_task_id():
-    try:
-        tasks = [
-            t['id'] for t in shakedown.get_tasks(completed=True) if t['name'] == config.SERVICE_NAME
-        ]
-    except dcos.errors.DCOSHTTPException:
-        tasks = []
-
-    return tasks[0] if tasks else None
 
 
 def get_pod_region(service_name, pod_name):
