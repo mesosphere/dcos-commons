20b265e598a4526092d4a8de7981396cb40fd523
benclarkwood <ben@benclarkwood.com>
2018-04-03 17:56:01 -0700

    ERROR new deployments on a too long DNS name Do nothing to old deployments (but do warn)

---
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/config/validate/ServiceNameCannotBreakDNS.java
@@ -0,0 +1,47 @@
+package com.mesosphere.sdk.config.validate;
+
+import com.mesosphere.sdk.http.EndpointUtils;
+import com.mesosphere.sdk.offer.LoggingUtils;
+import com.mesosphere.sdk.specification.ServiceSpec;
+import org.slf4j.Logger;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+public class ServiceNameCannotBreakDNS implements ConfigValidator<ServiceSpec> {
+
+    public static final Logger LOGGER = LoggingUtils.getLogger(ServiceNameCannotBreakDNS.class);
+
+
+    private static final int MAX_SERVICE_NAME_LENGTH = 63;
+
+    @Override
+    public Collection<ConfigValidationError> validate(Optional<ServiceSpec> oldConfig, ServiceSpec newConfig) {
+        // Is there a prior config?
+        if (oldConfig.isPresent()) {
+            // There is. We can at most WARN that the name is going to cause truncated DNS.
+            if (!nameWithinSubDomainLimit(newConfig.getName())) {
+                LOGGER.warn("The service name (without slashes) exceeds the maximum size allowed in a DNS subdomain.");
+            }
+
+            return Collections.emptyList();
+        } else {
+            // This is a new deployment, we can fail it if the service name is too large.
+            if (!nameWithinSubDomainLimit(newConfig.getName())) {
+                return Collections.singletonList(ConfigValidationError.valueError(
+                        "service.name",
+                        newConfig.getName(),
+                        "Service name (without slashes) exceeds 63 characters. In order for service DNS " +
+                                "to work correctly, the service name (without slashes) must not exceed 63 characters"));
+            }
+
+            return Collections.emptyList();
+        }
+    }
+
+    private boolean nameWithinSubDomainLimit(String name) {
+        // Each subdomain of a DNS address must be less than 63 characters.
+        return EndpointUtils.removeSlashes(name).length() <= MAX_SERVICE_NAME_LENGTH;
+    }
+}
