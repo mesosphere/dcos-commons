728f672be75985a4c5ba54a1f64b984e7020e58e
Evan Lezar <elezar@mesosphere.com>
2018-05-03 17:07:53 +0200

    Skip ZooKeeper authnz tests in Open

--- frameworks/kafka/tests/test_zookeeper_auth.py
+++ frameworks/kafka/tests/test_zookeeper_auth.py
@@ -18,6 +18,12 @@ from tests import client
 from tests import config
 
 
+pytestmark = [pytest.mark.skipif(sdk_utils.is_open_dcos(),
+                                 reason="Feature only supported in DC/OS EE"),
+              pytest.mark.skipif(sdk_utils.dcos_version_less_than("1.10"),
+                                 reason="Kerberos tests require DC/OS 1.10 or higher")]
+
+
 log = logging.getLogger(__name__)
 
 
