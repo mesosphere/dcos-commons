2378cb61ed13096559dd92503705c7c092bd7aad
Murilo Pereira <murilo@murilopereira.com>
2018-06-05 11:04:00 +0200

    Names.

--- frameworks/kafka/tests/test_tls.py
+++ frameworks/kafka/tests/test_tls.py
@@ -141,7 +141,7 @@ def test_tls_ciphers(kafka_service_tls):
 
     for openssl_cipher in possible_openssl_ciphers:
         if sdk_security.is_cipher_enabled(config.SERVICE_NAME, task_name, openssl_cipher, endpoint):
-            enabled_ciphers.add(sdk_security.rfc_name(openssl_cipher))
+            enabled_ciphers.add(cipher_suites.rfc_name(openssl_cipher))
 
     log.info('%s ciphers enabled out of %s:', len(enabled_ciphers), len(possible_ciphers))
     log.info("\n".join(sdk_utils.sort(list(enabled_ciphers))))
