df4f5d4a2ebc270e13f104d2a2afe8e2ec036ee3
Evan Lezar <elezar@mesosphere.com>
2018-06-15 20:02:15 +0200

    Use a generalized script to run flake8

---
+++ tools/ci/checks/run_check_in_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DOCKER_TAG=${DOCKER_TAG:-latest}
+DOCKER_IMAGE=${DOCKER_IMAGE:-mesosphere/dcos-commons:${DOCKER_TAG}}
+
+
+TOOL=$1
+shift
+
+docker run --rm -ti \
+    -v $(pwd):/build:ro \
+    -w /build \
+        mesosphere/dcos-commons:elezar-dev \
+            ${TOOL} $*
--- tools/ci/checks/run_flake8_checks.sh
+++ tools/ci/checks/run_flake8_checks.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
+# Run flake8 in a docker container.
 
+TOOL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-DOCKER_TAG=${DOCKER_TAG:-latest}
-DOCKER_IMAGE=${DOCKER_IMAGE:-mesosphere/dcos-commons:${DOCKER_TAG}}
-
-docker run --rm -ti \
-    -v $(pwd):/build:ro \
-    -w /build \
-        mesosphere/dcos-commons:elezar-dev \
-            flake8 $*
+${TOOL_DIR}/run_check_in_docker.sh flake8 $*
