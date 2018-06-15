e10cab735fcbd3693df6451226edf1ad5d2d8cc4
Evan Lezar <elezar@mesosphere.com>
2018-06-15 21:03:11 +0200

    Add a script to check applicability of changes

---
+++ tools/ci/checks/are_changes_applicable.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if git rev-parse --verify HEAD >/dev/null 2>&1
+then
+    against=${COMPARE_TO:-HEAD}
+else
+    # Initial commit: diff against an empty tree object
+    against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
+fi
+
+patterns=(
+    "^cli"
+    "^clivendor"
+    "^conftest.py"
+    "^govendor"
+    "^sdk"
+    "^sdk"
+    "^test_requirements.txt"
+    "^testing"
+    "^tools"
+    "^tools"
+)
+
+if [[ -n $FRAMEWORK ]]; then
+    patterns+=("^frameworks/$FRAMEWORK")
+fi
+
+IGNORE="\.md\$"
+DIFF_FILES=$(git diff ${against} --name-only | grep -vE "${IGNORE}")
+
+matches=()
+for f in "${patterns[@]}"; do
+    matches+=($(echo "${DIFF_FILES}" | grep -E "$f" ))
+done
+
+
+if [ ${#matches[@]} -gt 0 ]; then
+    echo "Changes applicable"
+fi
+
+echo "${matches[*]}"
