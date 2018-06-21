ab880648f7aa505b5f54432f6d9a6a62fa151527
Sam Tran <stran@mesosphere.com>
2018-06-21 09:00:11 -0700

    Change back to Evan's updated image

--- frameworks/kafka/tests/client.py
+++ frameworks/kafka/tests/client.py
@@ -93,7 +93,7 @@ class KafkaClient:
             "container": {
                 "type": "MESOS",
                 "docker": {
-                    "image": "samvantran/kafka-client:4.1.0",
+                    "image": "elezar/kafka-client:deca3d0",
                     "forcePullImage": True
                 }
             },
