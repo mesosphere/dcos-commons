262c3edb4fd7cc117d138996ee8fb7dbaa348cf3
Evan Lezar <elezar@mesosphere.com>
2018-06-25 14:51:33 +0200

    Run docker directly in flake8 check

--- tools/ci/checks/run_flake8_checks.sh
+++ tools/ci/checks/run_flake8_checks.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Run flake8 in a docker container.
 
-TOOL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOCKER_TAG=${DOCKER_TAG:-latest}
+DOCKER_IMAGE=${DOCKER_IMAGE:-mesosphere/dcos-commons:${DOCKER_TAG}}
 
-${TOOL_DIR}/run_check_in_docker.sh flake8 $*
+docker run --rm -t \
+    -v $(pwd):/build:ro \
+    -w /build \
+        ${DOCKER_IMAGE} \
+            flake8 $@
