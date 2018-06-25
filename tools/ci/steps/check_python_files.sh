e3a827c78f6bd2b7f8e2a612dad2f451087296a3
Evan Lezar <elezar@mesosphere.com>
2018-06-25 18:10:17 +0200

    Allow base branch to be set

--- tools/ci/steps/check_python_files.sh
+++ tools/ci/steps/check_python_files.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # This script is used by the build system to check MODIFIED Python files in the repository.
+#
+# By default, the BASE_BRANCH is determined using the get_base_branch script, but this
+# can be overridden by setting the BASE_BRANCH environment variable before invoking this
+# script
 
 TOOL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../checks" && pwd )"
 
 # Determine the target branch for the diff calculation
-BASE_BRANCH=$( ${TOOL_DIR}/get_base_branch.sh )
+BASE_BRANCH=${BASE_BRANCH:-$( ${TOOL_DIR}/get_base_branch.sh )}
 
 # Get the list of changed .py files relative to the base branch
 CHANGESET=$( ${TOOL_DIR}/get_applicable_changes.py --extensions ".py" --from-git "${BASE_BRANCH}" )
