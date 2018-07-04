6e4388fede00de854465b6a92e27ad2420161059
Evan Lezar <elezar@mesosphere.com>
2018-07-04 09:47:35 +0200

    Add PYTHONPATH for pylint checks (#2562)

--- tools/ci/checks/run_pylint_checks.sh
+++ tools/ci/checks/run_pylint_checks.sh
@@ -4,6 +4,7 @@ DOCKER_TAG=${DOCKER_TAG:-latest}
 DOCKER_IMAGE=${DOCKER_IMAGE:-mesosphere/dcos-commons:${DOCKER_TAG}}
 
 docker run --rm -t \
+    -e PYTHONPATH="$PYTHONPATH" \
     -v $(pwd):/build:ro \
     -w /build \
         ${DOCKER_IMAGE} \
