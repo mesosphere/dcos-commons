9b5491d4d1bcf624571222af8e3793b45a224e0a
Evan Lezar <elezar@mesosphere.com>
2018-07-09 18:21:10 +0200

    [COPS-3222][DCOS-38688][TEST] Cassandra pod replace performs rolling update (#2576)
    * Cassandra pod replace performs rolling update

--- testing/sdk_recovery.py
+++ testing/sdk_recovery.py
@@ -1,4 +1,5 @@
 import logging
+import typing
 
 import sdk_cmd
 import sdk_plan
@@ -13,26 +14,44 @@ def check_permanent_recovery(
     service_name: str,
     pod_name: str,
     recovery_timeout_s: int,
+    pods_with_updated_tasks: typing.List[str] = None,
 ):
     """
-    Perform a replace operation on a specified pod and check that it is replaced
+    Perform a replace (permanent recovery) operation on the specified pod.
 
-    All other pods are checked to see if they remain consistent.
+    The specified pod AND any additional pods in `pods_with_updated_tasks` are
+    checked to ensure that their tasks have been restarted.
+
+    Any remaining pods are checked to ensure that their tasks are not changed.
+
+    For example, performing a pod replace kafka-0 on a Kafka framework should
+    result in ONLY the kafa-0-broker task being restarted. In this case,
+    pods_with_updated_tasks is specified as None.
+
+    When performing a pod replace operation on a Cassandra seed node (node-0),
+    a rolling restart of other nodes is triggered, and
+    pods_with_updated_tasks = ["node-0", "node-1", "node-2"]
+    (assuming a three node Cassandra ring)
     """
     LOG.info("Testing pod replace operation for %s:%s", service_name, pod_name)
 
     sdk_plan.wait_for_completed_deployment(service_name)
     sdk_plan.wait_for_completed_recovery(service_name)
 
-    pod_list = sdk_cmd.svc_cli(package_name, service_name, "pod list", json=True)
+    pod_list = set(sdk_cmd.svc_cli(package_name, service_name, "pod list", json=True))
 
-    tasks_to_replace = set(sdk_tasks.get_task_ids(service_name, pod_name))
+    pods_to_update = set(
+        pods_with_updated_tasks if pods_with_updated_tasks else [] + [pod_name]
+    )
+
+    tasks_to_replace = {}
+    for pod in pods_to_update:
+        tasks_to_replace[pod] = set(sdk_tasks.get_task_ids(service_name, pod_name))
+
     LOG.info("The following tasks will be replaced: %s", tasks_to_replace)
 
     tasks_in_other_pods = {}
-    for pod in pod_list:
-        if pod == pod_name:
-            continue
+    for pod in pod_list - pods_to_update:
         tasks_in_other_pods[pod] = set(sdk_tasks.get_task_ids(service_name, pod))
 
     LOG.info("Tasks in other pods should not be replaced: %s", tasks_in_other_pods)
@@ -43,7 +62,8 @@ def check_permanent_recovery(
     sdk_plan.wait_for_kicked_off_recovery(service_name, recovery_timeout_s)
     sdk_plan.wait_for_completed_recovery(service_name, recovery_timeout_s)
 
-    sdk_tasks.check_tasks_updated(service_name, pod_name, tasks_to_replace)
+    for pod, tasks in tasks_to_replace.items():
+        sdk_tasks.check_tasks_updated(service_name, pod, tasks)
 
     for pod, tasks in tasks_in_other_pods.items():
         sdk_tasks.check_tasks_not_updated(service_name, pod, tasks)
