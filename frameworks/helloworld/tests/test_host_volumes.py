18f3bc108b2e8d7633ccbd902d6bc915c9328432
Julien Eid <jeid@mesosphere.com>
2018-08-06 15:10:44 -0700

    Finally got codestyle working for python.

--- frameworks/helloworld/tests/test_host_volumes.py
+++ frameworks/helloworld/tests/test_host_volumes.py
@@ -9,15 +9,11 @@ from tests import config
 log = logging.getLogger(__name__)
 
 
-@pytest.fixture(scope='module', autouse=True)
+@pytest.fixture(scope="module", autouse=True)
 def configure_package(configure_security):
     try:
         sdk_install.uninstall(config.PACKAGE_NAME, config.SERVICE_NAME)
-        options = {
-            "service": {
-                "yaml": "host-volume"
-            }
-        }
+        options = {"service": {"yaml": "host-volume"}}
 
         sdk_install.install(config.PACKAGE_NAME, config.SERVICE_NAME, 2, additional_options=options)
 
@@ -30,18 +26,26 @@ def configure_package(configure_security):
 @pytest.mark.sanity
 @pytest.mark.smoke
 def test_check_host_volume_mounts():
-    '''world has no docker image defined, hello does. Check to make sure the host volume is mounted in the container'''
-    assert "host-volume-etc" in search_for_host_volume("hello-0-server", "bash -c 'mount'", "host-volume-etc")
-    assert "host-volume-etc" in search_for_host_volume("world-0-server", "bash -c 'mount'", "host-volume-etc")
-    assert "host-volume-var" in search_for_host_volume("world-0-server", "bash -c 'mount'", "host-volume-var")
+    """world has no docker image defined, hello does. Check to make sure the host volume is mounted in the container"""
+    assert "host-volume-etc" in search_for_host_volume(
+        "hello-0-server", "bash -c 'mount'", "host-volume-etc"
+    )
+    assert "host-volume-etc" in search_for_host_volume(
+        "world-0-server", "bash -c 'mount'", "host-volume-etc"
+    )
+    assert "host-volume-var" in search_for_host_volume(
+        "world-0-server", "bash -c 'mount'", "host-volume-var"
+    )
 
 
 @retrying.retry(wait_fixed=2000, stop_max_delay=5 * 60 * 1000)
 def search_for_host_volume(task_name, command, mount_name):
     _, output, _ = sdk_cmd.service_task_exec(config.SERVICE_NAME, task_name, command)
-    lines = [line.strip() for line in output.split('\n')]
-    log.info('Looking for %s...', mount_name)
+    lines = [line.strip() for line in output.split("\n")]
+    log.info("Looking for %s in task mounts.", mount_name)
     for line in lines:
         if mount_name in line:
             return line
-    raise Exception("Failed to read host volume mountpoint from {} with command '{}'".format(task_name, command))
+    raise Exception(
+        "Failed to read host volume mountpoint from {} with command '{}'".format(task_name, command)
+    )
