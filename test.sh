29967c3f5039689eae840aa6c73e67f50c6bfe59
Gauri Powale <gpowale@mesosphere.com>
2018-08-09 12:25:36 -0700

    Passing ssh-agent into docker.

--- test.sh
+++ test.sh
@@ -52,7 +52,7 @@ else
 fi
 gradle_cache="${REPO_ROOT_DIR}/.gradle_cache"
 ssh_path="${HOME}/.ssh/ccm.pem"
-ssh_user="core"
+ssh_agent="$SSH_AUTH_SOCK"
 aws_creds_path="${HOME}/.aws/credentials"
 enterprise="true"
 headless="false"
@@ -227,8 +227,8 @@ if [ -z "$CLUSTER_URL" -a x"$interactive" == x"true" ]; then
 fi
 
 # Configure SSH key for getting into the cluster during tests
-if [ -f "$ssh_path" ]; then
-    volume_args="$volume_args -v $ssh_path:/ssh/key" # pass provided key into docker env
+if [ -f "$ssh_agent" ]; then
+    volume_args="$volume_args -v $(dirname $SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK" # pass ssh-agent into docker env
 else
     if [ -n "$CLUSTER_URL" ]; then
         # If the user is providing us with a cluster, we require the SSH key for that cluster.
