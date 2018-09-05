833d0579538ea8c362975453d617f33a3e04f6c5
jeid64 <jeid64@gmail.com>
2018-09-05 13:31:10 -0400

    DCOS-38256 Backport Fixed bug where if Port env-key was defined, env based secrets were empty. (#2653)
    * DCOS-38256 Backport to 0.40 Fixed bug where if Port env-key was defined, env based secrets were empty. (#2650)
    
    * DCOS-38256 Fixed bug where if Port env-key was defined, env based
    secrets had an empty string value.
    
    * Missed a usecase in our executor of toMap.
    
    * Fix whitespace.

--- sdk/executor/src/main/java/com/mesosphere/sdk/offer/taskdata/ProcessBuilderUtils.java
+++ sdk/executor/src/main/java/com/mesosphere/sdk/offer/taskdata/ProcessBuilderUtils.java
@@ -1,6 +1,7 @@
 package com.mesosphere.sdk.offer.taskdata;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.mesos.Protos;
 
@@ -13,11 +14,24 @@ public class ProcessBuilderUtils {
     }
 
     /**
+     * Returns a Map representation of the provided {@link Protos.Environment}.
+     * In the event of duplicate labels, the last duplicate wins.
+     */
+    public static Map<String, String> toMap(Protos.Environment environment) {
+        // sort labels alphabetically for convenience in debugging/logging:
+        Map<String, String> map = new TreeMap<>();
+        for (Protos.Environment.Variable variable : environment.getVariablesList()) {
+            map.put(variable.getName(), variable.getValue());
+        }
+        return map;
+    }
+
+    /**
      * Returns a {@link ProcessBuilder} instance which has been initialized with the provided
      * {@link Protos.CommandInfo}'s command and environment.
      */
     public static ProcessBuilder buildProcess(Protos.CommandInfo cmd) {
-        return buildProcess(cmd.getValue(), EnvUtils.toMap(cmd.getEnvironment()));
+        return buildProcess(cmd.getValue(), toMap(cmd.getEnvironment()));
     }
 
     /**
