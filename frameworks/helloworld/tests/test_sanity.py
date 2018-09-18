22fc5eba3413cfd2373f8abaacd979de1983e402
Evan Lezar <elezar@mesosphere.com>
2018-09-18 09:59:50 +0200

    Correctly get scheduler task ID

--- frameworks/helloworld/tests/test_sanity.py
+++ frameworks/helloworld/tests/test_sanity.py
@@ -47,7 +47,8 @@ def test_install():
 @pytest.mark.smoke
 @pytest.mark.dcos_min_version("1.9")
 def test_metrics_cli_for_scheduler_metrics(helloworld_service):
-    scheduler_task_id = sdk_tasks.get_task_ids("marathon", helloworld_service).pop()
+    scheduler_task_prefix = sdk_marathon.get_scheduler_task_prefix(helloworld_service)
+    scheduler_task_id = sdk_tasks.get_task_ids("marathon", scheduler_task_prefix).pop()
     scheduler_metrics = sdk_metrics.wait_for_metrics_from_cli(scheduler_task_id, timeout_seconds=60)
 
     assert scheduler_metrics, "Expecting a non-empty set of metrics"
