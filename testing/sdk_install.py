c75edb7e3480977e1329d09300ca9175c96a375d
Jitendra Pawar <jitnedra.pawar@gmail.com>
2018-10-15 11:29:51 -0700

    Made changes as per review comments.

--- testing/sdk_install.py
+++ testing/sdk_install.py
@@ -150,12 +150,12 @@ def uninstall(
         service_account,
         zk)
 
-#Portworx specific cleanups are done here. 
+# Portworx specific cleanups are done here. 
 def _portworx_cleanup():
     client = mesos.DCOSClient()
     agents = client.get_state_summary()['slaves']
-    #The cassandra tests only unmount and detach the portworx volumes created during tests
-    #Find the cassandra tests specific portworx volumes and delete those.
+    # The cassandra tests only unmount and detach the portworx volumes created during tests
+    # Find the cassandra tests specific portworx volumes and delete those.
     log.info("PORTWORX: cleanup cassandra volumes")
     exit_status, _ = shakedown.run_command_on_agent(agents[0]['hostname'],
         'for vol in `pxctl v l | grep Cassandra | cut -f 2`; do pxctl v d -f $vol; done', 'vagrant','/ssh/key')
@@ -247,7 +247,7 @@ def _uninstall(
                 raise
         finally:
             sdk_utils.list_reserved_resources()
-    #Call portworks specific cleanup routine at the end.
+    # Call portworx specific cleanup routine at the end.
     _portworx_cleanup()
 
 def merge_dictionaries(dict1, dict2):
