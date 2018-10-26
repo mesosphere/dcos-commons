9e250771ea82564045b5a488b3f5c816be0a1ad7
Tarun Gupta Akirala <tarugupta.92@gmail.com>
2018-10-26 02:36:21 -0700

    Format sdk/scheduler/src/main/java/com/mesosphere/sdk/http

--- sdk/scheduler/src/main/java/com/mesosphere/sdk/http/types/EndpointProducer.java
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/http/types/EndpointProducer.java
@@ -5,21 +5,16 @@ package com.mesosphere.sdk.http.types;
  */
 public interface EndpointProducer {
 
-    /**
-     * Returns some endpoint content to be sent in response to an 'endpoints' lookup request. This
-     * could be a URL, or the content of a file, etc.
-     */
-    public String getEndpoint();
+  /**
+   * Creates and returns an {@link EndpointProducer} which only produces the provided constant value.
+   */
+  static EndpointProducer constant(final String value) {
+    return () -> value;
+  }
 
-    /**
-     * Creates and returns an {@link EndpointProducer} which only produces the provided constant value.
-     */
-    public static EndpointProducer constant(final String value) {
-        return new EndpointProducer() {
-            @Override
-            public String getEndpoint() {
-                return value;
-            }
-        };
-    }
+  /**
+   * Returns some endpoint content to be sent in response to an 'endpoints' lookup request. This
+   * could be a URL, or the content of a file, etc.
+   */
+  String getEndpoint();
 }
