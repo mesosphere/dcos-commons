878b712d4f7582ab40ec4bb472c5181bf8cd91e3
Kevin Klues <klueska@gmail.com>
2018-11-14 20:36:35 +0100

    Expose 'sdk_{name}' python modules for import from 'sdk.testing'

--- python/sdk/testing/__init__.py
+++ python/sdk/testing/__init__.py
@@ -1,3 +1,24 @@
-__all__ = ['security']
+__all__ = [
+    'sdk_agents',
+    'sdk_auth',
+    'sdk_cmd',
+    'sdk_diag',
+    'sdk_fault_domain',
+    'sdk_hosts',
+    'sdk_install',
+    'sdk_jobs',
+    'sdk_marathon',
+    'sdk_metrics',
+    'sdk_networks',
+    'sdk_package_registry',
+    'sdk_plan',
+    'sdk_recovery',
+    'sdk_repository',
+    'sdk_security',
+    'sdk_tasks',
+    'sdk_upgrade',
+    'sdk_utils',
+    'security',
+]
 
 from . import security
