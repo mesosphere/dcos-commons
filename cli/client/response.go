8b04346cd788d10b9310f5128181b2ba01dc71d4
Karan Vishwanathan <karanvishwanathan@Mesospheres-MacBook-Pro-176.local>
2018-11-14 19:34:37 -0800

    refactor anc kick off CI

--- cli/client/response.go
+++ cli/client/response.go
@@ -24,14 +24,14 @@ func SetCustomResponseCheck(check responseCheck) {
 func CheckHTTPResponse(response *http.Response, err error) ([]byte, error) {
 	// Check for anything to return from the query itself
 	switch err.(type) {
-	if response == nil {
-		return nil, fmt.Errorf("Encountered an empty response, with error: %s, retry the operation again later", err)
-	}
 	case *url.Error:
 		// extract wrapped error
 		err = err.(*url.Error).Err
 	}
 	if err != nil {
+		if response == nil {
+			return nil, fmt.Errorf("Encountered an empty response, with error: %s, retry the operation again later", err)
+		}
 		switch err.(type) {
 		case x509.UnknownAuthorityError:
 			// custom suggestions for a certificate error:
