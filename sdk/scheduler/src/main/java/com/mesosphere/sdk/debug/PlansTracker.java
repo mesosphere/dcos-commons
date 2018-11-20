621e56404c1b14351236520a1f5204d2bd0575d8
Kaiwalya Joshi <kjoshi@mesosphere.com>
2018-11-19 20:58:52 -0800

    WIP: Initial implementation of Plans endpoint.

--- sdk/scheduler/src/main/java/com/mesosphere/sdk/debug/PlansTracker.java
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/debug/PlansTracker.java
@@ -7,6 +7,10 @@ import org.json.JSONObject;
 
 import com.mesosphere.sdk.http.ResponseUtils;
 import com.mesosphere.sdk.scheduler.plan.PlanCoordinator;
+import com.mesosphere.sdk.scheduler.plan.Plan;
+import com.mesosphere.sdk.scheduler.plan.Phase;
+import com.mesosphere.sdk.scheduler.plan.Step;
+import com.mesosphere.sdk.scheduler.plan.PlanManager;
 
 public class PlansTracker implements DebugEndpoint {
 
@@ -24,12 +28,45 @@ public class PlansTracker implements DebugEndpoint {
 		return outcome;
 	}
 	
-	public Response getJson(@QueryParam("plan") String plan,
-							 @QueryParam("phase") String phase,
-							 @QueryParam("step") String step,
-							 @QueryParam("sync") boolean sync) {
+	public Response getJson(@QueryParam("plan") String u_plan,
+							 @QueryParam("phase") String u_phase,
+							 @QueryParam("step") String u_step,
+							 @QueryParam("sync") boolean u_sync) {
+	
+		//TODO (kjoshi) Implement Pruning by plan/step/phase.
+		JSONObject outcome = new JSONObject();
+		for(PlanManager planManager: planCoordinator.getPlanManagers())
+		{
+			Plan plan  = planManager.getPlan();
+			
+			JSONObject planObject = new JSONObject();
+			planObject.put("status", plan.getStatus());
+			planObject.put("strategy", plan.getStrategy().getName());
+			planObject.put("errors", plan.getErrors());
+	
+			//Iterate over phases.
+			for(Phase phase: plan.getChildren())
+			{
+				JSONObject phaseObject = new JSONObject();
+				phaseObject.put("status", phase.getStatus());
+				phaseObject.put("strategy", phase.getStrategy().getName());
+				phaseObject.put("errors", phase.getErrors());
+			
+				//Iterate over steps.
+				for(Step step : phase.getChildren())
+				{
+					JSONObject stepObject = new JSONObject();
+					stepObject.put("status", step.getStatus());
+					stepObject.put("errors", step.getErrors());
+					phaseObject.put("(step) "+step.getName(), stepObject);
+				}
 
-		//TODO(kjoshi) Remove dummy return;
-		return ResponseUtils.jsonOkResponse(dummyJSONObject());
+				planObject.put("(phase) "+phase.getName(), phaseObject);
+			}
+			
+			outcome.put("(plan) "+plan.getName(), planObject);
+		}
+	
+		return ResponseUtils.jsonOkResponse(outcome);
 	}
 }
