59d42a8f05e1a822e1beae2c42a2423076a0d1f6
Karan Vishwanathan <karanvishwanathan@MesosphesMBP176.attlocal.net>
2018-12-02 01:46:36 -0500

    refactor structure

--- sdk/scheduler/src/main/java/com/mesosphere/sdk/debug/TaskStatusesTracker.java
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/debug/TaskStatusesTracker.java
@@ -38,39 +38,48 @@ public class TaskStatusesTracker implements DebugEndpoint {
     this.stateStore = stateStore;
   }
 
+  //CHECKSTYLE:OFF NestedForDepth
 
   private JSONArray getTaskStatuses(String filterPlan, String filterPhase, String filterStep) {
     JSONArray planArray = new JSONArray();
     for (PlanManager planManager : planCoordinator.getPlanManagers()) {
-      if (filterPlan != null && !planManager.getPlan().getName().equalsIgnoreCase(filterPlan))
+      if (filterPlan != null && !planManager.getPlan().getName().equalsIgnoreCase(filterPlan)) {
         continue;
+      }
       Plan plan = planManager.getPlan();
       JSONObject planObject = new JSONObject();
       planObject.put("plan", plan.getName());
       for (Phase phase : plan.getChildren()) {
-        if (filterPhase != null && !phase.getName().equalsIgnoreCase(filterPhase))
+        if (filterPhase != null && !phase.getName().equalsIgnoreCase(filterPhase)) {
           continue;
+        }
         JSONObject phaseObject = new JSONObject();
         for (Step step : phase.getChildren()) {
-          if (filterStep != null && !step.getName().equalsIgnoreCase(filterStep))
+          JSONArray taskArray = new JSONArray();
+          if (filterStep != null && !step.getName().equalsIgnoreCase(filterStep)) {
             continue;
-          JSONObject taskStatus = new JSONObject();
+          }
           Collection<TaskSpec> tasksInStep = step
               .getPodInstanceRequirement()
               .get().getPodInstance().getPod().getTasks();
           for (TaskSpec taskSpec : tasksInStep) {
-            String taskInstanceName = TaskSpec.getInstanceName(
-                step.getPodInstanceRequirement().get().getPodInstance(),
-                taskSpec.getName()
-            );
-            Optional<Protos.TaskStatus> status = stateStore.fetchStatus(taskInstanceName);
-            taskStatus.put("taskName", taskInstanceName);
-            if (status.isPresent()) {
-              taskStatus.put("taskId", status.get().getTaskId());
-              taskStatus.put("latestTaskState", status.get().getState());
+            //filter tasks in pod belonging to this step
+            if (step.getName().contains(taskSpec.getName())) {
+              String taskInstanceName = TaskSpec.getInstanceName(
+                  step.getPodInstanceRequirement().get().getPodInstance(),
+                  taskSpec.getName()
+              );
+              JSONObject taskStatus = new JSONObject();
+              Optional<Protos.TaskStatus> status = stateStore.fetchStatus(taskInstanceName);
+              taskStatus.put("taskName", taskInstanceName);
+              if (status.isPresent()) {
+                taskStatus.put("taskId", status.get().getTaskId());
+                taskStatus.put("latestTaskState", status.get().getState());
+              }
+              taskArray.put(taskStatus);
             }
           }
-          phaseObject.put(step.getName(), taskStatus);
+          phaseObject.put(step.getName(), taskArray);
         }
         planObject.put(phase.getName(), phaseObject);
       }
@@ -78,6 +87,8 @@ public class TaskStatusesTracker implements DebugEndpoint {
     }
     return planArray;
   }
+
+  //CHECKSTYLE:ON NestedForDepth
 
   public Response getJson(@QueryParam("plan") String filterPlan,
                           @QueryParam("phase") String filterPhase,
