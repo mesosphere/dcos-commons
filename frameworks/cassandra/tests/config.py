3d90365e55fa8ea4b6ea4868028c2ab4745ac75b
Ubuntu <ubuntu@ip-172-31-31-150.ap-south-1.compute.internal>
2018-12-04 10:03:56 +0000

    Cassandra-3.11.1-upgrade-changes

--- frameworks/cassandra/tests/config.py
+++ frameworks/cassandra/tests/config.py
@@ -17,7 +17,7 @@ PACKAGE_NAME = "cassandra"
 SERVICE_NAME = os.environ.get("SOAK_SERVICE_NAME") or "cassandra"
 
 DEFAULT_TASK_COUNT = 3
-DEFAULT_CASSANDRA_TIMEOUT = 600
+DEFAULT_CASSANDRA_TIMEOUT = 800
 # Soak artifact scripts may override the service name to test
 
 DEFAULT_NODE_ADDRESS = os.getenv(
