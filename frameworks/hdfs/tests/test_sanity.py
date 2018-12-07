96f46638e7abc4986ac7d8c712d29c48ac6424c2
sgoswami <sgoswami@mesosphere.io>
2018-12-07 14:11:28 -0800

    Cleaned up checking of namenodes.

--- frameworks/hdfs/tests/test_sanity.py
+++ frameworks/hdfs/tests/test_sanity.py
@@ -411,5 +411,6 @@ def test_namenodes_acheive_quorum_after_journalnode_replace():
     journal_task = sdk_tasks.get_service_tasks(foldered_name, "journal-0")[0]
     sdk_cmd.kill_task_with_pattern("journalnode", "nobody", agent_host=journal_task.host)
 
-    sdk_recovery.check_permanent_recovery(config.PACKAGE_NAME, foldered_name, pod, recovery_timeout_s=25 * 60,
-                                          pods_with_updated_tasks=["name-0", "name-1"])
+    pod_list = ["name-0", "name-1"]
+    for pod in pod_list:
+        sdk_recovery.check_permanent_recovery(config.PACKAGE_NAME, foldered_name, pod, recovery_timeout_s=25 * 60)
