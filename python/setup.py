9aa812614f28529d6773cf86e62e963f84e91cfb
Kevin Klues <klueska@gmail.com>
2018-12-11 10:24:11 -0500

    Added setup.py directly in 'python' folder.
    This makes it more intuitive for how to build this python package
    without using the (overly) generalized setup.py from tools/pip.
    
    In the future, we should remove tools/pip entirely, in favor of local
    setup.py file for all python related packages.

---
+++ python/setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+from setuptools import find_packages, setup
+
+version = os.getenv("VERSION")
+if not version:
+    print("Missing VERSION envvar.", file=sys.stderr)
+    sys.exit(1)
+
+setup(
+    name='sdk',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version=version,
+
+    description='The DC/OS Commons SDK python module',
+
+    # The project's main homepage.
+    url='https://github.com/mesosphere/dcos-commons',
+
+    # Author details
+    author='Mesosphere, Inc.',
+    author_email='help@dcos.io',
+
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Topic :: Software Development :: User Interfaces',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: Apache Software License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    # What does your project relate to?
+    keywords='mesos apache marathon mesosphere command datacenter dcos',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        "requests==2.20.1",
+        "retrying==1.3.3"
+    ],
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={
+        'sdk': [
+            'testing/security/keytab-validator'
+        ],
+    }
+)
