74872003047ac0ee515d74a82987c90bf0e6ce07
Marcin Owsiany <marcin@owsiany.pl>
2019-01-09 16:11:56 +0100

    [DCOS-46788] Improve confluent-kafka support in diag bundle script (#2872)
    * Save broker list in KafkaBundle.
    
    * Use KafkaBundle for confluent-kafka as well.
    
    * Update tools/diagnostics/base_tech_bundle/kafka_bundle.py
    
    * Document file naming convention.
    
    * Add broker get commands.

--- tools/diagnostics/base_tech_bundle/kafka_bundle.py
+++ tools/diagnostics/base_tech_bundle/kafka_bundle.py
@@ -1,5 +1,9 @@
+import json
 import logging
 
+import sdk_cmd
+
+import config
 from base_tech_bundle import BaseTechBundle
 
 logger = logging.getLogger(__name__)
@@ -7,4 +11,35 @@ logger = logging.getLogger(__name__)
 
 class KafkaBundle(BaseTechBundle):
     def create(self):
-        logger.info("Creating Kafka bundle (noop)")
+        logger.info("Creating Kafka bundle")
+        brokers = self.create_broker_list_file()
+        if brokers:
+            for broker_id in brokers:
+                self.create_broker_get_file(broker_id)
+
+    @config.retry
+    def create_broker_list_file(self):
+        rc, stdout, stderr = sdk_cmd.svc_cli(
+            self.package_name, self.service_name, "broker list", print_output=False
+        )
+
+        if rc != 0 or stderr:
+            logger.error(
+                "Could not perform broker list\nstdout: '%s'\nstderr: '%s'", stdout, stderr
+            )
+        else:
+            self.write_file("service_broker_list.json", stdout)
+            return json.loads(stdout)
+
+    @config.retry
+    def create_broker_get_file(self, broker_id):
+        rc, stdout, stderr = sdk_cmd.svc_cli(
+            self.package_name, self.service_name, "broker get %s" % broker_id, print_output=False
+        )
+
+        if rc != 0 or stderr:
+            logger.error(
+                "Could not perform broker get %s\nstdout: '%s'\nstderr: '%s'", broker_id, stdout, stderr
+            )
+        else:
+            self.write_file("service_broker_get_%s.json" % broker_id, stdout)
