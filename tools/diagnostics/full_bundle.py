155a6dfbd6b5e6fdfcdcd31d030942be92f53f4c
Murilo Pereira <murilo@murilopereira.com>
2019-01-11 16:42:04 +0100

    [DIAGNOSTICS] Fix condition where scheduler task can't be found. (#2878)
    This happens due to a name mismatch. The DCOS_SERVICE_NAME label in SDK services and the service name provided through the CLI (i.e. dcos $package --name=$service_name ...) may or may not be prefixed by a slash, so comparing the two as-is sometimes doesn't work.
    
    This change makes sure to get the scheduler task with and without a slash prefix in the name.

--- tools/diagnostics/full_bundle.py
+++ tools/diagnostics/full_bundle.py
@@ -15,6 +15,9 @@ import config
 log = logging.getLogger(__name__)
 
 
+DCOS_SERVICES_JSON_FILE_NAME = "dcos_services.json"
+
+
 @config.retry
 def get_dcos_services() -> (bool, str):
     rc, stdout, stderr = sdk_cmd.run_cli(
@@ -30,14 +33,14 @@ def get_dcos_services() -> (bool, str):
         return (True, stdout)
 
 
-def is_service_named(service_name: str, service: dict) -> bool:
+def service_names_match(sdk_service_name: str, dcos_service_name: str) -> bool:
     """Handles a case where DC/OS service names sometimes don't contain the first slash.
     e.g.: |     SDK service name     |   DC/OS service name    |
           |--------------------------+-------------------------|
           | /data-services/cassandra | data-services/cassandra |
           | /production/cassandra    | /production/cassandra   |
     """
-    return service.get("name") in [service_name, service_name.lstrip("/")]
+    return dcos_service_name.lstrip("/") == sdk_service_name.lstrip("/")
 
 
 def is_service_active(service: dict) -> bool:
@@ -45,11 +48,15 @@ def is_service_active(service: dict) -> bool:
 
 
 def services_with_name(service_name: str, services: List[dict]) -> List[dict]:
-    return [s for s in services if is_service_named(service_name, s)]
+    return [s for s in services if service_names_match(service_name, s.get("name"))]
 
 
 def active_services_with_name(service_name: str, services: List[dict]) -> List[dict]:
-    return [s for s in services if is_service_named(service_name, s) and is_service_active(s)]
+    return [
+        s
+        for s in services
+        if service_names_match(service_name, s.get("name")) and is_service_active(s)
+    ]
 
 
 def is_service_scheduler_task(package_name: str, service_name: str, task: dict) -> bool:
@@ -60,7 +67,7 @@ def is_service_scheduler_task(package_name: str, service_name: str, task: dict) 
     dcos_service_name = next(
         iter([l.get("value") for l in labels if l.get("key") == "DCOS_SERVICE_NAME"]), ""
     )
-    return dcos_package_name == package_name and dcos_service_name == service_name
+    return dcos_package_name == package_name and service_names_match(service_name, dcos_service_name)
 
 
 def directory_date_string() -> str:
@@ -114,11 +121,11 @@ class FullBundle(Bundle):
 
         all_services = json.loads(all_services_or_error)
 
-        self.write_file("dcos_services.json", all_services, serialize_to_json=True)
+        self.write_file(DCOS_SERVICES_JSON_FILE_NAME, all_services, serialize_to_json=True)
 
         # An SDK service might have multiple DC/OS service entries. We expect that at most one is
         # "active".
-        services = [s for s in all_services if is_service_named(self.service_name, s)]
+        services = [s for s in all_services if service_names_match(self.service_name, s.get("name"))]
         # TODO: handle inactive services too.
         active_services = [s for s in services if is_service_active(s)]
 
@@ -131,7 +138,7 @@ class FullBundle(Bundle):
 
         active_service = active_services[0]
 
-        marathon_services = [s for s in all_services if is_service_named("marathon", s)]
+        marathon_services = [s for s in all_services if service_names_match("marathon", s.get("name"))]
         # TODO: handle the possibility of having no active Marathon services?
         if len(marathon_services) > 1:
             log.warn("More than one marathon services: %s", len(marathon_services))
@@ -146,6 +153,12 @@ class FullBundle(Bundle):
             for t in active_marathon_service.get("tasks", [])
             if is_service_scheduler_task(self.package_name, self.service_name, t)
         ]
+        if not scheduler_tasks:
+            log.warn(
+                "Could not find scheduler tasks for '%s' under the Marathon service ('\"name\": \"marathon\"') 'tasks' key in '%s'.",
+                self.service_name,
+                DCOS_SERVICES_JSON_FILE_NAME,
+            )
 
         ServiceBundle(
             self.package_name,
