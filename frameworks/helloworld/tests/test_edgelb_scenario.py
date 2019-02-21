92cc926a514e988db08a7406107e4f921bcf54b5
Tarun Gupta Akirala <tarugupta.92@gmail.com>
2019-02-21 15:30:30 -0500

    run test multiple times

--- frameworks/helloworld/tests/test_edgelb_scenario.py
+++ frameworks/helloworld/tests/test_edgelb_scenario.py
@@ -14,8 +14,9 @@ log = logging.getLogger(__name__)
 foldered_name = sdk_utils.get_foldered_name(config.SERVICE_NAME)
 
 
+@pytest.mark.parametrize('num_times', range(10))
 @pytest.mark.edgelb
-def test_edgelb():
+def test_edgelb(num_times):
     try:
         service_options = {"service": {"yaml": "edgelb"}}
         sdk_install.uninstall(config.PACKAGE_NAME, foldered_name)
@@ -25,12 +26,21 @@ def test_edgelb():
             0,
             additional_options=service_options,
             wait_for_deployment=False,
-            wait_for_all_conditions=False,
+            wait_for_all_conditions=False
         )
+        log.info("starting plans")
         # after above method returns, start reload plan right away.
-        sdk_plan.wait_for_kicked_off_plan(foldered_name, "deploy")
+        wait_for_plan(foldered_name, "deploy")
         sdk_plan.start_plan(foldered_name, "reload")
-        sdk_plan.wait_for_kicked_off_plan(foldered_name, "reload")
-        time.sleep(1000000)  # Ctrl-C to cancel
+        wait_for_plan(foldered_name, "reload")
+        sdk_plan.wait_for_completed_plan(foldered_name, "deploy")
+        sdk_plan.wait_for_completed_plan(foldered_name, "reload")
+        # time.sleep(1000000)  # Ctrl-C to cancel
     finally:
         sdk_install.uninstall(config.PACKAGE_NAME, foldered_name)
+
+
+def wait_for_plan(service_name, plan_name):
+    return sdk_plan.wait_for_plan_status(
+        service_name, plan_name, ["PENDING", "STARTING", "IN_PROGRESS", "COMPLETE"]
+    )
