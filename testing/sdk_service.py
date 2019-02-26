7bbbd842310f7618f9bbb2c3d849ef4912edbb35
Martin Hrabovcin <mhrabovcin.c@mesosphere.com>
2019-02-26 13:53:35 +0100

    testing/sdk_service: fix mypy

--- testing/sdk_service.py
+++ testing/sdk_service.py
@@ -4,6 +4,7 @@ sdk_service IN ANY OTHER PARTNER REPOS
 ****************************************************************************************************
 
 """
+from typing import Any, Dict
 
 import sdk_upgrade
 
@@ -12,19 +13,19 @@ DEFAULT_TIMEOUT_SECONDS = 10 * 60  # 10 minutes
 
 
 def update_configuration(
-    package_name,
-    service_name,
-    configuration,
-    expected_task_count,
-    wait_for_deployment=True,
-    timeout_seconds=DEFAULT_TIMEOUT_SECONDS,
-):
+    package_name: str,
+    service_name: str,
+    configuration: Dict[str, Any],
+    expected_task_count: int,
+    wait_for_deployment: bool=True,
+    timeout_seconds: int=DEFAULT_TIMEOUT_SECONDS,
+) -> None:
     sdk_upgrade.update_or_upgrade_or_downgrade(
-        package_name,
-        service_name,
-        None,
-        configuration,
-        expected_task_count,
+        package_name=package_name,
+        service_name=service_name,
+        to_package_version=None,
+        additional_options=configuration,
+        expected_running_tasks=expected_task_count,
         wait_for_deployment=wait_for_deployment,
         timeout_seconds=timeout_seconds,
     )
