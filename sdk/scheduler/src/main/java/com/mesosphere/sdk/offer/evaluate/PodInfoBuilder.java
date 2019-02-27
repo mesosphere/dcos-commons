0e248c3f64230a7158ccc198f746c451bee5e74c
Karan Vishwanathan <karanvishwanathan@Mesospheres-MacBook-Pro-4010.local>
2019-02-26 17:39:20 -0800

    dont add temp

--- sdk/scheduler/src/main/java/com/mesosphere/sdk/offer/evaluate/PodInfoBuilder.java
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/offer/evaluate/PodInfoBuilder.java
@@ -575,10 +575,12 @@ public class PodInfoBuilder {
 
     // Isolate the tmp directory of tasks
     //switch to SANDBOX SELF after dc/os 1.13
-    containerInfo.addVolumes(Protos.Volume.newBuilder()
-        .setContainerPath("/tmp")
-        .setHostPath("tmp")
-        .setMode(Protos.Volume.Mode.RW));
+    if (isTaskContainer) {
+      containerInfo.addVolumes(Protos.Volume.newBuilder()
+          .setContainerPath("/tmp")
+          .setHostPath("tmp")
+          .setMode(Protos.Volume.Mode.RW));
+    }
 
     for (Protos.Volume hostVolume : hostVolumes) {
       containerInfo.addVolumes(hostVolume);
