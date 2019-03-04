2c782363faca09488ad5d57d94d1d23abcc2d048
Adam Dangoor <adamdangoor@gmail.com>
2019-03-04 15:39:38 +0000

    Progress towards passing mypy across multiple files

--- tools/universe/test_package.py
+++ tools/universe/test_package.py
@@ -2,7 +2,7 @@ from .package import Package
 from .package import Version
 
 
-def test_version_comparison():
+def test_version_comparison() -> None:
     assert Version(0, "1.2.4") < Version(10, "1.2.4")
     assert Version(0, "1.2.5") < Version(10, "1.2.4")
     assert Version(0, "1.2.3") == Version(0, "1.2.4")
@@ -14,7 +14,7 @@ def test_version_comparison():
     assert Version(0, "1.2.4") == Version(0, "1.2.3")
 
 
-def test_package_from_json():
+def test_package_from_json() -> None:
     package_json = {"name": "package", "version": "1.2.3", "releaseVersion": 10}
     p = Package.from_json(package_json)
 
@@ -22,33 +22,33 @@ def test_package_from_json():
     assert p.get_version() == Version(10, "1.2.3")
 
 
-def test_package_starts_with_beta_is_beta():
+def test_package_starts_with_beta_is_beta() -> None:
     p = Package("beta-package", None)
 
     assert p.is_beta()
 
 
-def test_normal_package_is_not_beta():
+def test_normal_package_is_not_beta() -> None:
 
     p = Package("package", None)
 
     assert not p.is_beta()
 
 
-def test_non_beta_backage_beta_name_is_name():
+def test_non_beta_backage_beta_name_is_name() -> None:
 
     p = Package("package", None)
 
     assert p.get_name() == p.get_non_beta_name()
 
 
-def test_beta_package_beta_name():
+def test_beta_package_beta_name() -> None:
     p = Package("beta-package", None)
 
     assert p.get_non_beta_name() == "package"
 
 
-def test_elastic_ordering():
+def test_elastic_ordering() -> None:
     p7 = Package.from_json(
         {"name": "beta-elastic", "version": "1.0.16-5.5.1-beta", "releaseVersion": 7}
     )
