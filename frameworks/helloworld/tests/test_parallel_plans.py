e2bdd8a3718061fed5eef2df5a062d0c39bedb3c
Tarun Gupta Akirala <tarugupta.92@gmail.com>
2019-03-12 17:23:59 -0700

    Add custom plans with unique resource sets

--- frameworks/helloworld/tests/test_parallel_plans.py
+++ frameworks/helloworld/tests/test_parallel_plans.py
@@ -26,7 +26,7 @@ def configure_package(configure_security):
 
 @pytest.mark.sanity
 def test_all_tasks_are_launched():
-    service_options = {"service": {"yaml": "sidecar"}, "hello": {"count": 1}}
+    service_options = {"service": {"yaml": "plan"}}
     sdk_install.install(
         config.PACKAGE_NAME,
         foldered_name,
@@ -36,21 +36,24 @@ def test_all_tasks_are_launched():
         wait_for_all_conditions=True
     )
     # after above method returns, start all plans right away.
-    sdk_plan.start_plan(foldered_name, "sidecar")
-    sdk_plan.start_plan(foldered_name, "sidecar-parameterized", {"PLAN_PARAMETER": "parameterized"})
-    sdk_plan.wait_for_completed_plan(foldered_name, "sidecar")
-    sdk_plan.wait_for_completed_plan(foldered_name, "sidecar-parameterized")
-    # /pod/<pod-id>/info fetches data from SDK's persistence layer
-    pod_hello_0_info = sdk_cmd.service_request(
-        "GET", foldered_name, "/v1/pod/hello-0/info"
-    ).json()
-    for taskInfoAndStatus in pod_hello_0_info:
-        info = taskInfoAndStatus["info"]
-        status = taskInfoAndStatus["status"]
-        # While `info` object is always present, `status` may or may not be present based on whether the
-        # task was launched and we received an update from mesos (or not).
-        if status:
-            assert info["taskId"]["value"] == status["taskId"]["value"]
-            assert len(info["taskId"]["value"]) > 0
-        else:
-            assert len(info["taskId"]["value"]) == 0
+    plans = ["manual-plan-0", "manual-plan-1", "manual-plan-2"]
+    for plan in plans:
+        sdk_plan.start_plan(foldered_name, plan)
+    for plan in plans:
+        sdk_plan.wait_for_completed_plan(foldered_name, plan)
+    pods = ["custom-pod-A-0", "custom-pod-B-0", "custom-pod-C-0"]
+    for pod in pods:
+        # /pod/<pod-id>/info fetches data from SDK's persistence layer
+        pod_hello_0_info = sdk_cmd.service_request(
+            "GET", foldered_name, "/v1/pod/{}/info".format(pod)
+        ).json()
+        for taskInfoAndStatus in pod_hello_0_info:
+            info = taskInfoAndStatus["info"]
+            status = taskInfoAndStatus["status"]
+            # While `info` object is always present, `status` may or may not be present based
+            # on whether the task was launched and we received an update from mesos (or not).
+            if status:
+                assert info["taskId"]["value"] == status["taskId"]["value"]
+                assert len(info["taskId"]["value"]) > 0
+            else:
+                assert len(info["taskId"]["value"]) == 0
