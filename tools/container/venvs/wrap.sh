35fdf6c61fba4c5400b7c00766ffd5ef2a364e19
Marcin Owsiany <mowsiany@mesosphere.com>
2019-04-25 09:56:36 +0200

    improve documentation

--- tools/container/venvs/wrap.sh
+++ tools/container/venvs/wrap.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-# No functionality yet, will be added once TeamCity is updated to use this file.
+# TODO: this is just a pass-through fow now, actual invocation of pipenv will
+# be added after TeamCity configs are updated to use this file.
 set -eu
-venv="$1"
+function usage() {
+	echo "Usage: $0 <PIPENV_PROJECT> <COMMAND> [ ARGS ... ]" >&2
+	echo "Where:" >&2
+	echo "  PIPENV_PROJECT is the name of a directory under /venvs" >&2
+	echo "  COMMAND and ARGS will be run with pipenv using the project" >&2
+}
+pipenv_project="$1"
 shift
+if [[ $# == 0 ]]; then
+	usage
+fi
 exec "$@"
