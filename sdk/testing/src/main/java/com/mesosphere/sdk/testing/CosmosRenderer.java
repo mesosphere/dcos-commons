a83eb6c375f527d0137622b3cd7cd5f81187ceb3
Kaiwalya Joshi <kjoshi@mesosphere.com>
2019-05-10 16:41:35 +0200

    Update tests to reflect split of framework-jre and scheduler-jre

--- sdk/testing/src/main/java/com/mesosphere/sdk/testing/CosmosRenderer.java
+++ sdk/testing/src/main/java/com/mesosphere/sdk/testing/CosmosRenderer.java
@@ -40,7 +40,8 @@ public class CosmosRenderer {
   static {
     RESOURCE_TEMPLATE_PARAMS = new HashMap<>();
     RESOURCE_TEMPLATE_PARAMS.put("artifact-dir", "https://test-url/artifacts");
-    RESOURCE_TEMPLATE_PARAMS.put("jre-url", "https://test-url/jre.tgz");
+    RESOURCE_TEMPLATE_PARAMS.put("scheduler-jre-url", "https://test-url/jre.tgz");
+    RESOURCE_TEMPLATE_PARAMS.put("framework-jre-url", "https://test-url/jre.tgz");
     RESOURCE_TEMPLATE_PARAMS.put("libmesos-bundle-url", "https://test-url/libmesos-bundle.tgz");
     RESOURCE_TEMPLATE_PARAMS.put("dcos-sdk-version", "99.99.99-SNAPSHOT");
   }
