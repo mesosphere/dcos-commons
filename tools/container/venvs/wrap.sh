1168ec4f910ff7ea27d3b116d6d0f8be26bda421
Marcin Owsiany <mowsiany@mesosphere.com>
2019-05-30 09:45:18 +0200

    Touch file again
    Trigger another run.

--- tools/container/venvs/wrap.sh
+++ tools/container/venvs/wrap.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -eu
+
 function usage() {
 	echo "Usage: $0 <PIPENV_PROJECT> <COMMAND> [ ARGS ... ]" >&2
 	echo "Where:" >&2
 	echo "  PIPENV_PROJECT is the name of a directory under /venvs" >&2
 	echo "  COMMAND and ARGS will be run with pipenv using the project" >&2
 }
+
 pipenv_project="$1"
 shift
 export PIPENV_PIPFILE="/venvs/${pipenv_project}/Pipfile"
