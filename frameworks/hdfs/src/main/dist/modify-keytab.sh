e2e7e89785d7ac72f087858815d1b339c8350463
ishanchandrajoshi <ishan.joshi2@tcs.com>
2019-06-02 06:30:46 +0000

    changed JAVA_HOME in modify-keytab.sh

--- frameworks/hdfs/src/main/dist/modify-keytab.sh
+++ frameworks/hdfs/src/main/dist/modify-keytab.sh
@@ -1,3 +1,3 @@
-export JAVA_HOME=$(ls -d $MESOS_SANDBOX/jdk*/jre/)
+export JAVA_HOME=$(ls -d $MESOS_SANDBOX/jdk*/)
 $JAVA_HOME/bin/java -jar krbtest.jar secrets-hdfs.keytab 
 echo "Keytab file modified"
