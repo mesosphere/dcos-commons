69d7094390daea616ec1ac4bc9e7f724280fd0bd
ishanchandrajoshi <ishan.joshi2@tcs.com>
2019-06-13 06:50:13 +0000

    removed if block from hdfs upgrade test

--- frameworks/hdfs/tests/test_upgrade.py
+++ frameworks/hdfs/tests/test_upgrade.py
@@ -17,23 +17,13 @@ def test_hdfs_upgrade():
     try:
         sdk_install.uninstall(config.PACKAGE_NAME, foldered_name)
 
-        if sdk_utils.dcos_version_less_than("1.9"):
-            # HDFS upgrade in 1.8 is not supported.
-            sdk_install.install(
-                config.PACKAGE_NAME,
-                foldered_name,
-                config.DEFAULT_TASK_COUNT,
-                additional_options={"service": {"name": foldered_name}},
-                timeout_seconds=30 * 60,
-            )
-        else:
-            sdk_upgrade.test_upgrade(
-                config.PACKAGE_NAME,
-                foldered_name,
-                config.DEFAULT_TASK_COUNT,
-                from_options={"service": {"name": foldered_name}},
-                timeout_seconds=30 * 60,
-            )
+        sdk_upgrade.test_upgrade(
+            config.PACKAGE_NAME,
+            foldered_name,
+            config.DEFAULT_TASK_COUNT,
+            from_options={"service": {"name": foldered_name}},
+            timeout_seconds=30 * 60,
+        )
 
     finally:
         sdk_install.uninstall(config.PACKAGE_NAME, foldered_name)
