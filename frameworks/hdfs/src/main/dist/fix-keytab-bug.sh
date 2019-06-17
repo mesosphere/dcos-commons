5845101a3e9c0190c8e327d61235139d2c0f0c7c
ishanchandrajoshi <ishan.joshi2@tcs.com>
2019-06-17 11:47:28 +0000

    removed fix-keytab-bug.sh and included all commands in svc

--- frameworks/hdfs/src/main/dist/fix-keytab-bug.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -x
-export JAVA_HOME=$(ls -d $MESOS_SANDBOX/jdk*/)
-$JAVA_HOME/bin/java -jar krbtest.jar $1
-echo "Keytab file fixed due to bug in HDFS version 3.2.0. See HADOOP-16283 {{HDFS_BUG_ISSUE_URI}}"
