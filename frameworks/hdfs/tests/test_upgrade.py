0ac03d84c64746ef03e6f4fcc0bcffb9566f1f08
ishanchandrajoshi <39409909+ishanchandrajoshi@users.noreply.github.com>
2019-06-19 17:46:07 +0530

    Upgrade to Apache Hdfs 3.2.0 (#3033)
    * made changes to run the framework with Apache Hdfs and a temporary fix to modify the keytab file
    
    * corrected the calling of script modify-keytab.sh
    
    * modified the assert for test_verify_https_port since the curl output is changed for newer version
    
    * added a temporary fix to test test_sanity
    
    * created additional task for upgrading NN. Also used bootstrap task to start standby NN after upgarde
    
    * patch null ref in sdk
    
    * added logic for checking whether its a fresh install or a upgrade
    
    * Remove stderr check and ignore CLI warnings
    
    * modified update plan to reverse deployment of Primary and Standby NN.
    
    * modified NN upgrade approach
    
    * resolved some more conflicts
    
    * changed JAVA_HOME to previous value
    
    * reset JAVA_HOME
    
    * changed JAVA_HOME in modify-keytab.sh
    
    * modified NN readiness check to include finalize command for upgrade
    
    * removed extra echoes
    
    * added test for hdfs upgrade
    
    * removed unnecessary imports from test_upgrade.py
    
    * removed if block from hdfs upgrade test
    
    * added vars in upgrade script, removed if block in test_upgrade, changed name and content of fix-keytab-bug.sh
    
    * removed mobile link and substituted mustache variable
    
    * removed cat statement
    
    * removed fix-keytab-bug.sh and included all commands in svc
    
    * changed the name of krbtest.jar and added few comments

---
+++ frameworks/hdfs/tests/test_upgrade.py
@@ -0,0 +1,28 @@
+import logging
+
+import pytest
+import sdk_install
+import sdk_upgrade
+
+from tests import config
+
+log = logging.getLogger(__name__)
+
+foldered_name = config.FOLDERED_SERVICE_NAME
+
+
+@pytest.mark.sanity
+def test_hdfs_upgrade():
+    try:
+        sdk_install.uninstall(config.PACKAGE_NAME, foldered_name)
+
+        sdk_upgrade.test_upgrade(
+            config.PACKAGE_NAME,
+            foldered_name,
+            config.DEFAULT_TASK_COUNT,
+            from_options={"service": {"name": foldered_name}},
+            timeout_seconds=30 * 60,
+        )
+
+    finally:
+        sdk_install.uninstall(config.PACKAGE_NAME, foldered_name)
