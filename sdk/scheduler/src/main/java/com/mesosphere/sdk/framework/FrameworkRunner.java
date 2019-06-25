5a53ca91cda8b1d91b861b0a1a3ff381c7f97629
Kaiwalya Joshi <kjoshi@mesosphere.com>
2019-06-25 18:52:34 -0400

    Fix runtime errors, undo test changes.

--- sdk/scheduler/src/main/java/com/mesosphere/sdk/framework/FrameworkRunner.java
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/framework/FrameworkRunner.java
@@ -165,14 +165,15 @@ public class FrameworkRunner {
     // The framework ID is not available when we're being started for the first time.
     frameworkId.ifPresent(fwkInfoBuilder::setId);
 
-    // We set to MULTI_ROLE by default and add all the necessary roles.
-    fwkInfoBuilder.addCapabilitiesBuilder()
-          .setType(Protos.FrameworkInfo.Capability.Type.MULTI_ROLE);
-
-    if (frameworkConfig.getPreReservedRoles().isEmpty()) {
-      fwkInfoBuilder.setRole(frameworkConfig.getRole());
-    } else {
+    if (!frameworkConfig.getPreReservedRoles().isEmpty() ||
+        !frameworkConfig.getRole().contentEquals(frameworkConfig.getNonNamespacedRole()))
+    {
+      // We set to MULTI_ROLE by default and add all the necessary roles.
+      fwkInfoBuilder.addCapabilitiesBuilder()
+        .setType(Protos.FrameworkInfo.Capability.Type.MULTI_ROLE);
       fwkInfoBuilder.addAllRoles(frameworkConfig.getAllResourceRoles());
+    } else {
+      fwkInfoBuilder.setRole(frameworkConfig.getRole());
     }
 
     if (!StringUtils.isEmpty(frameworkConfig.getWebUrl())) {
