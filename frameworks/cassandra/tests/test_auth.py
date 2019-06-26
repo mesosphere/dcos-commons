3bb21b10b312b4e5bf79ec680ba2295dbb897c4e
viivek46 <viivek46@gmail.com>
2019-06-26 17:24:25 +0530

    added backup/Restore Test while authn/z is enable

--- frameworks/cassandra/tests/test_auth.py
+++ frameworks/cassandra/tests/test_auth.py
@@ -1,3 +1,4 @@
+import os
 import retrying
 from typing import Any, Dict, Iterator, List
 import logging
@@ -10,6 +11,12 @@ import sdk_utils
 from tests import config
 
 log = logging.getLogger(__name__)
+
+
+no_strict_for_azure = pytest.mark.skipif(
+    os.environ.get("SECURITY") == "strict",
+    reason="backup/restore doesn't work in strict as user needs to be root",
+)
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -63,6 +70,61 @@ def test_unauthorized_users() -> None:
     tasks = sdk_tasks.get_service_tasks(config.SERVICE_NAME, "node-0")[0]
     _, stdout, stderr = sdk_cmd.run_cli("task exec {} bash -c 'export JAVA_HOME=$(ls -d $MESOS_SANDBOX/jdk*/) ;  export PATH=$MESOS_SANDBOX/python-dist/bin:$PATH ; export PATH=$(ls -d $MESOS_SANDBOX/apache-cassandra-*/bin):$PATH ; cqlsh -u dcossuperuser -p wrongpassword -e \"SHOW VERSION\" node-0-server.$FRAMEWORK_HOST $CASSANDRA_NATIVE_TRANSPORT_PORT' ".format(tasks.id))
     assert "Provided username dcossuperuser and/or password are incorrect" in stderr
+
+
+@pytest.mark.azure
+@no_strict_for_azure
+@pytest.mark.sanity
+def test_backup_and_restore_to_azure() -> None:
+    client_id = os.getenv("AZURE_CLIENT_ID")
+    if not client_id:
+        assert (
+            False
+        ), 'Azure credentials are required for this test. Disable test with e.g. TEST_TYPES="sanity and not azure"'
+    plan_parameters = {
+        "CLIENT_ID": client_id,
+        "CLIENT_SECRET": os.getenv("AZURE_CLIENT_SECRET"),
+        "TENANT_ID": os.getenv("AZURE_TENANT_ID"),
+        "AZURE_STORAGE_ACCOUNT": os.getenv("AZURE_STORAGE_ACCOUNT"),
+        "AZURE_STORAGE_KEY": os.getenv("AZURE_STORAGE_KEY"),
+        "CONTAINER_NAME": os.getenv("CONTAINER_NAME", "cassandra-test"),
+        "SNAPSHOT_NAME": str(uuid.uuid1()),
+        "CASSANDRA_KEYSPACES": '"testspace1 testspace2"',
+    }
+
+    config.run_backup_and_restore(
+        config.get_foldered_service_name(),
+        "backup-azure",
+        "restore-azure",
+        plan_parameters,
+        config.get_foldered_node_address(),
+    )
+
+
+@pytest.mark.aws
+@pytest.mark.sanity
+def test_backup_and_restore_to_s3() -> None:
+    key_id = os.getenv("AWS_ACCESS_KEY_ID")
+    if not key_id:
+        assert (
+            False
+        ), 'AWS credentials are required for this test. Disable test with e.g. TEST_TYPES="sanity and not aws"'
+    plan_parameters = {
+        "AWS_ACCESS_KEY_ID": key_id,
+        "AWS_SECRET_ACCESS_KEY": os.getenv("AWS_SECRET_ACCESS_KEY"),
+        "AWS_REGION": os.getenv("AWS_REGION", "us-west-2"),
+        "S3_BUCKET_NAME": os.getenv("AWS_BUCKET_NAME", "infinity-framework-test"),
+        "SNAPSHOT_NAME": str(uuid.uuid1()),
+        "CASSANDRA_KEYSPACES": '"testspace1 testspace2"',
+    }
+
+    config.run_backup_and_restore(
+        config.get_foldered_service_name(),
+        "backup-s3",
+        "restore-s3",
+        plan_parameters,
+        config.get_foldered_node_address(),
+    )
 
 
 def create_secret(secret_value: str, secret_path: str) -> None:
