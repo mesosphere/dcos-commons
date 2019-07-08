421004f567be7a010f9481a28c19b2d2a4545c54
viivek46 <viivek46@gmail.com>
2019-07-09 00:29:38 +0530

    wip

---
+++ frameworks/cassandra/src/main/dist/jmx-ssl-properties.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+JMX_SECURE_PROPERTIES_FILE=$MESOS_SANDBOX/.ssl/jmxremote.ssl.properties
+
+umask u=rw,g=,o=
+
+tee $JMX_SECURE_PROPERTIES_FILE <<EOF >/dev/null
+
+javax.net.ssl.keyStore=$KEY_STORE_PATH
+javax.net.ssl.keyStorePassword=$SECURE_JMX_KEY_STORE_KEY
+javax.net.ssl.trustStore=$TRUST_STORE_PATH
+javax.net.ssl.trustStorePassword=$SECURE_JMX_TRUST_STORE_KEY
+javax.net.ssl.enabled.protocols=TLSv1.2
+EOF
+
+JMX_PROPERTIES_FILE=$MESOS_SANDBOX/.ssl/jmx.properties
+
+tee $JMX_PROPERTIES_FILE <<EOF >/dev/null
+
+com.sun.management.jmxremote.host=$MESOS_CONTAINER_IP
+com.sun.management.jmxremote=true
+com.sun.management.jmxremote.authenticate=true
+com.sun.management.jmxremote.rmi.port=$SECURE_JMX_RMI_PORT
+com.sun.management.jmxremote.registry.ssl=true
+com.sun.management.jmxremote.ssl.need.client.auth=true
+com.sun.management.jmxremote.password.file=$MESOS_SANDBOX/.ssl/key_file
+com.sun.management.jmxremote.access.file=$MESOS_SANDBOX/jmx/access_file
+com.sun.management.jmxremote.ssl=true
+com.sun.management.jmxremote.ssl.config.file=$MESOS_SANDBOX/.ssl/jmxremote.ssl.properties
+com.sun.management.jmxremote.local.only=false
+
+
+EOF
