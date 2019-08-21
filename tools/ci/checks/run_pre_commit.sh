f54ac7a05fdc5f03874440909ed5057d01365d6e
Murilo Pereira <murilo@murilopereira.com>
2019-08-21 14:26:47 +0200

    [TOOLS] Fix run_pre_commit script for submodule-based projects.

--- tools/ci/checks/run_pre_commit.sh
+++ tools/ci/checks/run_pre_commit.sh
@@ -2,6 +2,7 @@
 
 set -euxo pipefail
 
+SCRIPT_FILE="$(basename "${BASH_SOURCE[@]}")"
 SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DCOS_COMMONS_DIRECTORY="$(cd "${SCRIPT_DIRECTORY}/../../.." && pwd)"
 
@@ -10,8 +11,13 @@ function was_run_from_submodule () {
 }
 
 if was_run_from_submodule; then
+  if [ -z "${FRAMEWORK}" ]; then
+    echo "${SCRIPT_FILE} depends on \$FRAMEWORK environment variable to be set if run in the context of a project"
+    echo "Please export \$FRAMEWORK"
+    exit 1
+  fi
   PROJECT_DIRECTORY="$(cd "${DCOS_COMMONS_DIRECTORY}/.." && pwd)"
-  PROJECT_ARGUMENTS="--project ${PROJECT_DIRECTORY}"
+  PROJECT_ARGUMENTS="${FRAMEWORK} --project ${PROJECT_DIRECTORY}"
 else
   PROJECT_DIRECTORY="${DCOS_COMMONS_DIRECTORY}"
   PROJECT_ARGUMENTS=""
