b34a0edf3168a8eb447da6dbe42aeabc4d2ab072
Kaiwalya Joshi <kjoshi@mesosphere.com>
2019-08-27 15:18:42 -0400

    Omit test_quota_deployment::test_nonenforced_group_role_service_role_set.

--- frameworks/helloworld/tests/test_quota_deployment.py
+++ frameworks/helloworld/tests/test_quota_deployment.py
@@ -60,37 +60,37 @@ def test_nonenforced_group_role_defaults():
     assert service_roles["framework-role"] == LEGACY_ROLE
 
 
-@pytest.mark.quota
-@pytest.mark.dcos_min_version("1.14")
-@pytest.mark.sanity
-def test_nonenforced_group_role_service_role_set():
+# @pytest.mark.quota
+# @pytest.mark.dcos_min_version("1.14")
+# @pytest.mark.sanity
+# def test_nonenforced_group_role_service_role_set():
 
-    # Create group without enforced roles.
-    sdk_marathon.create_group(group_id=ENFORCED_ROLE, options={"enforceRole": False})
-    options = {"service": {"name": SERVICE_NAME, "service_role": ENFORCED_ROLE}}
+#     # Create group without enforced roles.
+#     sdk_marathon.create_group(group_id=ENFORCED_ROLE, options={"enforceRole": False})
+#     options = {"service": {"name": SERVICE_NAME, "service_role": ENFORCED_ROLE}}
 
-    # this config produces 1 hello's + 2 world's:
-    sdk_install.install(config.PACKAGE_NAME, SERVICE_NAME, 3, additional_options=options)
+#     # this config produces 1 hello's + 2 world's:
+#     sdk_install.install(config.PACKAGE_NAME, SERVICE_NAME, 3, additional_options=options)
 
-    # Ensure that our default service installs to a complete deployment.
-    sdk_plan.wait_for_completed_deployment(SERVICE_NAME)
+#     # Ensure that our default service installs to a complete deployment.
+#     sdk_plan.wait_for_completed_deployment(SERVICE_NAME)
 
-    # Assert the deployment plans are complete.
-    deployment_plan = sdk_plan.get_deployment_plan(SERVICE_NAME)
-    assert deployment_plan["status"] == "COMPLETE"
+#     # Assert the deployment plans are complete.
+#     deployment_plan = sdk_plan.get_deployment_plan(SERVICE_NAME)
+#     assert deployment_plan["status"] == "COMPLETE"
 
-    # Get the current service state to verify roles have applied.
-    service_roles = sdk_utils.get_service_roles(SERVICE_NAME)
-    current_task_roles = service_roles["task-roles"]
+#     # Get the current service state to verify roles have applied.
+#     service_roles = sdk_utils.get_service_roles(SERVICE_NAME)
+#     current_task_roles = service_roles["task-roles"]
 
-    # We must have some role!
-    assert len(current_task_roles) > 0
+#     # We must have some role!
+#     assert len(current_task_roles) > 0
 
-    assert LEGACY_ROLE not in current_task_roles.values()
-    assert ENFORCED_ROLE in current_task_roles.values()
+#     assert LEGACY_ROLE not in current_task_roles.values()
+#     assert ENFORCED_ROLE in current_task_roles.values()
 
-    assert service_roles["framework-roles"] is None
-    assert service_roles["framework-role"] == ENFORCED_ROLE
+#     assert service_roles["framework-roles"] is None
+#     assert service_roles["framework-role"] == ENFORCED_ROLE
 
 
 @pytest.mark.quota
