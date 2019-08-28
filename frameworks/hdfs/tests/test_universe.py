63db5c3807ba6fd93530cce389266edd2fc2cf41
rathohit311356 <rathohit311356@gmail.com>
2019-08-28 17:23:38 +0530

    Fixing python syntax error

--- frameworks/hdfs/tests/test_universe.py
+++ frameworks/hdfs/tests/test_universe.py
@@ -8,8 +8,6 @@ import logging
 log = logging.getLogger(__name__)
 
 
-
-
 @pytest.mark.smoke
 @pytest.mark.sanity
 @pytest.mark.universe
@@ -25,4 +23,3 @@ def test_universe_file_formatting(file_base):
         if diff:
             print("\n" + ("\n".join(diff)))
             pytest.fail("%s is not formatted correctly, see diff above" % path)
-
