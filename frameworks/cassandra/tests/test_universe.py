891264d57ce3dc742b0ce607a0aa9c6e4ca9b82f
rathohit311356 <rathohit311356@gmail.com>
2019-08-29 12:30:33 +0530

    Fixing python code check for cassandra test_universe

--- frameworks/cassandra/tests/test_universe.py
+++ frameworks/cassandra/tests/test_universe.py
@@ -8,6 +8,7 @@ import logging
 log = logging.getLogger(__name__)
 
 
+@pytest.mark.smoke
 @pytest.mark.sanity
 @pytest.mark.universe
 @pytest.mark.parametrize("file_base", ["config", "package", "resource"])
@@ -16,9 +17,15 @@ def test_universe_file_formatting(file_base):
     path = os.path.join(framework_dir, "universe/%s.json" % file_base)
     with open(path, "r") as source:
         raw_data = [l.rstrip("\n") for l in source.readlines()]
-        formatted_data = [l for l in json.dumps(json.loads("".join(raw_data), object_pairs_hook=collections.OrderedDict), indent=2).split(
-            "\n")]
-        diff = list(difflib.unified_diff(raw_data, formatted_data, fromfile=path, tofile="formatted"))
+        formatted_data = [
+            l
+            for l in json.dumps(
+                json.loads("".join(raw_data), object_pairs_hook=collections.OrderedDict), indent=2
+            ).split("\n")
+        ]
+        diff = list(
+            difflib.unified_diff(raw_data, formatted_data, fromfile=path, tofile="formatted")
+        )
         if diff:
             print("\n" + ("\n".join(diff)))
             pytest.fail("%s is not formatted correctly, see diff above" % path)
