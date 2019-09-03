b503b2f699ec53bf6a61b3c3bcae1076d65c4523
Kaiwalya Joshi <kjoshi@mesosphere.com>
2019-09-03 14:51:47 -0400

    Revert "WIP DNM: Disable Package-Registry during sanity tests."
    This reverts commit d70e3b08fcb018e85d45af8b5555c1ab20c70ab5.

--- conftest.py
+++ conftest.py
@@ -67,7 +67,10 @@ INTEGRATION_TEST_LOG_COLLECTION = is_env_var_set(
 
 @pytest.fixture(scope="session", autouse=True)
 def configure_universe(tmpdir_factory):
-    yield from sdk_repository.universe_session()
+    if is_env_var_set("PACKAGE_REGISTRY_ENABLED", default=""):
+        yield from sdk_package_registry.package_registry_session(tmpdir_factory)
+    else:
+        yield from sdk_repository.universe_session()
 
 
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
