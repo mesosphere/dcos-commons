49a96ae6326ec6a1d25508573cac8bbfcbc9c745
rathohit311356 <rathohit311356@gmail.com>
2019-10-15 19:26:04 +0530

    Removing pytest stuff and making script to ensure it does without pytest

--- tools/ci/steps/check_json_files.py
+++ tools/ci/steps/check_json_files.py
@@ -1,42 +1,37 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import collections
 import difflib
 import os.path
-import pytest
 import json
-import logging
 import os
 import fnmatch
 
-log = logging.getLogger(__name__)
 
-
-@pytest.mark.smoke
-@pytest.mark.sanity
-@pytest.mark.universe
-@pytest.mark.parametrize("file_base", ["config", "package", "resource"])
-def test_universe_file_formatting(file_base):
-    framework_dir = os.path.dirname(os.path.dirname(__file__))
-    path_list = list()
-    for path, subdirs, files in os.walk(framework_dir):
-        for name in files:
-            filtered_path = fnmatch.filter([os.path.join(path, name)], "*universe*.json")
+cwd = os.getcwd()
+framework_dir = os.path.dirname(os.path.dirname(cwd+'/'))
+print(framework_dir)
+path_list = list()
+for path, subdirs, files in os.walk(framework_dir):
+    for name in files:
+        filtered_path = fnmatch.filter([os.path.join(path, name)], "*universe*.json")
         if len(filtered_path) > 0:
+            print(filtered_path)
             path_list.extend(filtered_path)
-    for path in path_list:
-        with open(path, "r") as source:
-            raw_data = [l.rstrip("\n") for l in source.readlines()]
-            formatted_data = [
-                l
-                for l in json.dumps(
-                    json.loads("".join(raw_data), object_pairs_hook=collections.OrderedDict),
-                    indent=2,
-                ).split("\n")
-            ]
-            diff = list(
-                difflib.unified_diff(raw_data, formatted_data, fromfile=path, tofile="formatted")
-            )
-            if diff:
-                print("\n" + ("\n".join(diff)))
-                pytest.fail("{} is not formatted correctly, see diff above".format(path))
+
+for path in path_list:
+    with open(path, "r") as source:
+        raw_data = [l.rstrip("\n") for l in source.readlines()]
+        formatted_data = [
+            l
+            for l in json.dumps(
+                json.loads("".join(raw_data), object_pairs_hook=collections.OrderedDict),
+                indent=2,
+            ).split("\n")
+        ]
+        diff = list(
+            difflib.unified_diff(raw_data, formatted_data, fromfile=path, tofile="formatted")
+        )
+        if diff:
+            print("\n" + ("\n".join(diff)))
+            print("{} is not formatted correctly, see diff above".format(path))
