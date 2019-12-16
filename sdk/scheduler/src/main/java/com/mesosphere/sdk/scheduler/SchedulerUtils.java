6c74536e2c457c68d4bd71330ed82f5b30d7ea68
Kaiwalya Joshi <kjoshi@mesosphere.com>
2019-12-16 15:21:49 -0800

    Move UninstallPlanFactory::getResourceIdsByAgentHost to SchedulerUtils::getResourceIdsByAgentHost to be used by other callers.

--- sdk/scheduler/src/main/java/com/mesosphere/sdk/scheduler/SchedulerUtils.java
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/scheduler/SchedulerUtils.java
@@ -1,6 +1,22 @@
 package com.mesosphere.sdk.scheduler;
 
+import com.mesosphere.sdk.offer.LoggingUtils;
+import com.mesosphere.sdk.offer.ResourceUtils;
+import com.mesosphere.sdk.offer.TaskException;
+import com.mesosphere.sdk.offer.taskdata.TaskLabelReader;
+import com.mesosphere.sdk.scheduler.recovery.FailureUtils;
+import com.mesosphere.sdk.state.StateStore;
 import com.mesosphere.sdk.storage.PersisterUtils;
+
+import org.apache.mesos.Protos;
+import org.slf4j.Logger;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 /**
  * This class provides utilities common to the construction and operation of Mesos Schedulers.
@@ -12,6 +28,8 @@ public final class SchedulerUtils {
    * confuse ZK with those.
    */
   public static final String SLASH_REPLACEMENT = "__";
+
+  private static final Logger LOGGER = LoggingUtils.getLogger(SchedulerUtils.class);
 
   private SchedulerUtils() {
   }
@@ -44,5 +62,55 @@ public final class SchedulerUtils {
 
     // Replace any other slashes (e.g. from folder support) with double underscores:
     return result.replace(PersisterUtils.PATH_DELIM_STR, SLASH_REPLACEMENT);
+  }
+
+
+  /**
+   * Returns a grouped mapping of agent hostname to resource ids present on that agent.
+   * <p>
+   * The resulting map will be sorted alphabetically by agent hostname, and the resource ids within each agent entry
+   * will also be sorted alphabetically.
+   */
+  public static Map<String, Set<String>> getResourceIdsByAgentHost(StateStore stateStore) {
+    Collection<Protos.TaskInfo> allTasks = stateStore.fetchTasks();
+    Set<String> taskIdsInErrorState = stateStore.fetchStatuses().stream()
+        .filter(taskStatus -> taskStatus.getState().equals(Protos.TaskState.TASK_ERROR))
+        .map(taskStatus -> taskStatus.getTaskId().getValue())
+        .collect(Collectors.toSet());
+
+    // Filter the tasks to those that have actually created resources. Tasks in an ERROR state which are also
+    // flagged as permanently failed are assumed to not have resources reserved on Mesos' end, despite our State
+    // Store still listing them with resources. This is because we log the planned reservation before it occurs.
+    Collection<Protos.TaskInfo> tasksWithExpectedReservations = allTasks.stream()
+        .filter(taskInfo -> !(FailureUtils.isPermanentlyFailed(taskInfo)
+            && taskIdsInErrorState.contains(taskInfo.getTaskId().getValue())))
+        .collect(Collectors.toList());
+
+    // The agent hostname mapping is sorted alphabetically. This doesn't affect functionality and is just for user
+    // experience when viewing the uninstall plan.
+    Map<String, Set<String>> resourceIdsByAgentHost = new TreeMap<>();
+    for (Protos.TaskInfo taskInfo : tasksWithExpectedReservations) {
+      String hostname;
+      try {
+        hostname = new TaskLabelReader(taskInfo).getHostname();
+      } catch (TaskException e) {
+        LOGGER.warn(
+            String.format("Failed to determine hostname of task %s", taskInfo.getName()),
+            e
+        );
+        hostname = "UNKNOWN_AGENT";
+      }
+
+      // Sort the resource ids alphabetically within each agent. This doesn't affect functionality and is just
+      // for user experience when viewing the uninstall plan.
+      resourceIdsByAgentHost
+          .computeIfAbsent(hostname, k -> new TreeSet<>())
+          .addAll(ResourceUtils.getResourceIds(ResourceUtils.getAllResources(taskInfo)));
+    }
+
+    LOGGER.info("Detected {}/{} tasks across {} agents",
+        tasksWithExpectedReservations.size(), allTasks.size(), resourceIdsByAgentHost.size());
+
+    return resourceIdsByAgentHost;
   }
 }
