291ecb074c12c1d40b32619a76209b4183fc41b6
Kaiwalya Joshi <kjoshi@mesosphere.com>
2019-12-16 17:52:24 -0800

    [TOOLING] Misc fixes for MWT runs. (#3210)
    * Prepend account names to created ceritifcate files when creating service accounts.
    
    * MWT Hello-World: Remove leading slashes from account-names.
    
    * Add config option --disable-diag-collect to prevent collection of all service logs due to a single test failure.
    
    * Add config option to specify package version for hello-world, defaults to latest catalog version.
    
    * Fix issues found in black, flake8 and pylint pre-commit-hooks.

--- frameworks/helloworld/tests/conftest.py
+++ frameworks/helloworld/tests/conftest.py
@@ -10,45 +10,77 @@ def configure_security(configure_universe):
 
 # pytest fixtures to run hello-world scale test located in frameworks/helloworld/tests/scale
 def pytest_addoption(parser):
-    parser.addoption('--count', action='store', default=1, type=int,
-                     help='Number of hello world services to deploy with a given scenario')
-    parser.addoption("--scenario", action='store', default='',
-                     help="hello world service yml to use")
-    parser.addoption("--service-name", action='store', default='hello-world',
-                     help="custom service name to be used instead of default 'hello-world'")
-    parser.addoption('--min', action='store', default=-1,
-                     help='min hello-world index to start from (default: -1).')
-    parser.addoption('--max', action='store', default=-1,
-                     help='max hello-world index to end at (default: -1).')
-    parser.addoption('--batch-size', action='store', default=1,
-                     help='batch size to deploy hello-world masters in (default: 1).')
+    parser.addoption(
+        "--count",
+        action="store",
+        default=1,
+        type=int,
+        help="Number of hello world services to deploy with a given scenario",
+    )
+    parser.addoption(
+        "--scenario", action="store", default="", help="hello world service yml to use"
+    )
+    parser.addoption(
+        "--service-name",
+        action="store",
+        default="hello-world",
+        help="custom service name to be used instead of default 'hello-world'",
+    )
+    parser.addoption(
+        "--min",
+        action="store",
+        default=-1,
+        help="min hello-world index to start from (default: -1).",
+    )
+    parser.addoption(
+        "--max", action="store", default=-1, help="max hello-world index to end at (default: -1)."
+    )
+    parser.addoption(
+        "--batch-size",
+        action="store",
+        default=1,
+        help="batch size to deploy hello-world masters in (default: 1).",
+    )
+    parser.addoption(
+        "--package-version",
+        action="store",
+        default="",
+        help="version of hello-world service to install. "
+        "specify exact version i.e '3.2.0-0.57.1', 'stub-universe', "
+        "or empty string ('') for the latest catalog version. (default: '')",
+    )
 
 
 @pytest.fixture
 def service_count(request) -> int:
-    return int(request.config.getoption('--count'))
+    return int(request.config.getoption("--count"))
 
 
 @pytest.fixture
 def scenario(request) -> str:
-    return str(request.config.getoption('--scenario'))
+    return str(request.config.getoption("--scenario"))
 
 
 @pytest.fixture
 def service_name(request) -> str:
-    return str(request.config.getoption('--service-name'))
+    return str(request.config.getoption("--service-name"))
 
 
 @pytest.fixture
 def min_index(request) -> int:
-    return int(request.config.getoption('--min'))
+    return int(request.config.getoption("--min"))
 
 
 @pytest.fixture
 def max_index(request) -> int:
-    return int(request.config.getoption('--max'))
+    return int(request.config.getoption("--max"))
 
 
 @pytest.fixture
 def batch_size(request) -> int:
-    return int(request.config.getoption('--batch-size'))
+    return int(request.config.getoption("--batch-size"))
+
+
+@pytest.fixture
+def package_version(request) -> str:
+    return str(request.config.getoption("--package-version"))
--- frameworks/helloworld/tests/scale/test_scale.py
+++ frameworks/helloworld/tests/scale/test_scale.py
@@ -13,8 +13,7 @@ JOB_RUN_TIMEOUT = 10 * 60  # 10 mins
 
 
 @pytest.mark.soak
-def test_soak_load(service_name,
-                   scenario) -> None:
+def test_soak_load(service_name, scenario, package_version) -> None:
     """Launch a soak test scenario. This does not verify the results
     of the test, but does ensure the instances were created.
 
@@ -23,16 +22,13 @@ def test_soak_load(service_name,
         scenario: yaml scenario to run helloworld with (normal, crashloop) are added for this case
     """
     soak_service_name = "{}-{}-soak".format(service_name, scenario)
-    _launch_load(soak_service_name, scenario)
+    _launch_load(soak_service_name, scenario, package_version)
 
 
 @pytest.mark.scale
-def test_scaling_load(service_name,
-                      scenario,
-                      service_count,
-                      min_index,
-                      max_index,
-                      batch_size) -> None:
+def test_scaling_load(
+    service_name, scenario, service_count, min_index, max_index, batch_size, package_version
+) -> None:
     """Launch a load test scenario in parallel if needed.
     This does not verify the results of the test, but does
     ensure the instances were created.
@@ -44,15 +40,20 @@ def test_scaling_load(service_name,
         min_index: minimum index to begin suffixes at
         max_index: maximum index to end suffixes at
         batch_size: batch size to deploy instances in
+        package_version: version of hello-world to deploy, defaults to catalog if unspecified.
     """
     scale_service_name = "{}-{}".format(service_name, scenario)
     deployment_list = []
     if min_index == -1 or max_index == -1:
-        deployment_list = ["{}-{}".format(scale_service_name, index) for index in range(0, int(service_count))]
+        deployment_list = [
+            "{}-{}".format(scale_service_name, index) for index in range(0, int(service_count))
+        ]
         min_index = 0
         max_index = service_count
     else:
-        deployment_list = ["{}-{}".format(scale_service_name, index) for index in range(min_index, max_index)]
+        deployment_list = [
+            "{}-{}".format(scale_service_name, index) for index in range(min_index, max_index)
+        ]
 
     sdk_security.install_enterprise_cli()
 
@@ -60,11 +61,14 @@ def test_scaling_load(service_name,
     end = max_index - min_index
     for current in range(0, end, batch_size):
         # Get current batch of schedulers to deploy.
-        batched_deployment_list = deployment_list[current:current + batch_size]
+        batched_deployment_list = deployment_list[current : current + batch_size]
         # Create threads with correct arguments.
-        deployment_threads = spawn_threads(batched_deployment_list,
-                                           _launch_load,
-                                           scenario=scenario)
+        deployment_threads = spawn_threads(
+            batched_deployment_list,
+            _launch_load,
+            scenario=scenario,
+            package_version=package_version,
+        )
         # Launch jobs.
         wait_and_get_failures(deployment_threads, timeout=JOB_RUN_TIMEOUT)
 
@@ -82,19 +86,22 @@ def test_scaling_cleanup(service_name, scenario, service_count, min_index, max_i
     scale_service_name = "{}-{}".format(service_name, scenario)
 
     if min_index == -1 or max_index == -1:
-        scale_cleanup_list = ["{}-{}".format(scale_service_name, index) for index in range(0, int(service_count))]
+        scale_cleanup_list = [
+            "{}-{}".format(scale_service_name, index) for index in range(0, int(service_count))
+        ]
     else:
-        scale_cleanup_list = ["{}-{}".format(scale_service_name, index) for index in range(min_index, max_index)]
+        scale_cleanup_list = [
+            "{}-{}".format(scale_service_name, index) for index in range(min_index, max_index)
+        ]
 
     sdk_security.install_enterprise_cli()
 
-    cleanup_threads = spawn_threads(scale_cleanup_list,
-                                    _uninstall_service)
+    cleanup_threads = spawn_threads(scale_cleanup_list, _uninstall_service)
     # Launch jobs.
     wait_and_get_failures(cleanup_threads, timeout=JOB_RUN_TIMEOUT)
 
 
-def _launch_load(service_name, scenario) -> None:
+def _launch_load(service_name, scenario, package_version) -> None:
     """Launch a load test scenario. This does not verify the results
     of the test, but does ensure the instances were created.
 
@@ -105,14 +112,12 @@ def _launch_load(service_name, scenario) -> None:
     # Note service-names *cannot* have underscores in them.
     launch_service_name = service_name.replace("_", "-")
     # service-names can have '/'s in them but service account names cannot, sanitize here.
-    launch_account_name = launch_service_name.replace("/", "__")
+    launch_account_name = launch_service_name.strip("/").replace("/", "__")
     security_info = _create_service_account(launch_account_name)
-    _install_service(launch_service_name,
-                     scenario,
-                     security_info)
+    _install_service(launch_service_name, scenario, security_info, package_version)
 
 
-def _install_service(service_name, scenario, security_info) -> None:
+def _install_service(service_name, scenario, security_info, package_version) -> None:
     # do not wait for deploy plan to complete, all tasks to launch or marathon app deployment
     # supports rapid deployments in scale test scenario
     options = {"service": {"name": service_name, "yaml": scenario}}
@@ -125,8 +130,9 @@ def _install_service(service_name, scenario, security_info) -> None:
         service_name,
         config.DEFAULT_TASK_COUNT,
         additional_options=options,
+        package_version=package_version,
         wait_for_deployment=False,
-        wait_for_all_conditions=False
+        wait_for_all_conditions=False,
     )
 
 
@@ -137,20 +143,18 @@ def _uninstall_service(service_name) -> None:
         service_name: Service name or Marathon ID
         role: The role for the service to use (default is no role)
     """
-    if service_name.startswith('/'):
+    if service_name.startswith("/"):
         service_name = service_name[1:]
     # Note service-names *cannot* have underscores in them.
     service_name = service_name.replace("_", "-")
     log.info("Uninstalling {}.".format(service_name))
-    sdk_install.uninstall(config.PACKAGE_NAME,
-                          service_name)
+    sdk_install.uninstall(config.PACKAGE_NAME, service_name)
 
 
 def _create_service_account(service_name) -> None:
     if sdk_utils.is_strict_mode():
         try:
-            log.info("Creating service accounts for '{}'"
-                     .format(service_name))
+            log.info("Creating service accounts for '{}'".format(service_name))
 
             sa_name = "{}-principal".format(service_name)
             sa_secret = "{}-secret".format(service_name)
