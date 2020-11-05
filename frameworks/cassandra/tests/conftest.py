298d2e0cc0eacc0d904cc5dafc123b714d20497e
Kaiwalya Joshi <kjoshi@mesosphere.com>
2020-11-05 00:39:11 -0800

    Cleanup Cassandra Portworx Options. Guard Pod-Replacement-Failure policy. Add basic integration tests.

--- frameworks/cassandra/tests/conftest.py
+++ frameworks/cassandra/tests/conftest.py
@@ -1,6 +1,7 @@
 from typing import Iterator
 
 import pytest
+import sdk_external_volumes
 import sdk_security
 from tests import config
 
@@ -8,3 +9,9 @@ from tests import config
 @pytest.fixture(scope="session")
 def configure_security(configure_universe: None) -> Iterator[None]:
     yield from sdk_security.security_session(config.SERVICE_NAME)
+
+
+@pytest.fixture(scope="session")
+def configure_external_volumes():
+    # Handle creation of external volumes.
+    yield from sdk_external_volumes.external_volumes_session()
